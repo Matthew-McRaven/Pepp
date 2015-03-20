900d0461cdd7f0a4808cbb3f3178518006d2e5b6
StanWarford <Stan.Warford@pepperdine.edu>
2015-03-19 22:35:07 -0700

    Fix bug that was not highlighting unit-pre and unit-post code.

--- pepcpu8highlighter.cpp
+++ pepcpu8highlighter.cpp
@@ -34,11 +34,16 @@ PepHighlighter::PepHighlighter(QTextDocument *parent)
     oprndFormat.setFontWeight(QFont::Bold);
     QStringList oprndPatterns;
     oprndPatterns << "\\bLoadCk\\b" << "\\bC\\b" << "\\bB\\b"
-            << "\\bA\\b" << "\\bMARCk\\b" << "\\bMDRCk\\b"
-            << "\\bAMux\\b" << "\\bMDRMux\\b" << "\\bCMux\\b"
-            << "\\bALU\\b" << "\\bCCk\\b" << "\\bVCk\\b"
-            << "\\bANDZ\\b" << "\\bZCk\\b" << "\\bNCk\\b"
-            << "\\bMemRead\\b" << "\\bMemWrite\\b" << "^(\\s)*UnitPre(?=:)\\b" << "^(\\s)*UnitPost(?=:)\\b";
+                  << "\\bA\\b" << "\\bMARCk\\b" << "\\bMDRCk\\b"
+                  << "\\bAMux\\b" << "\\bMDRMux\\b" << "\\bCMux\\b"
+                  << "\\bALU\\b" << "\\bCCk\\b" << "\\bVCk\\b"
+                  << "\\bAndZ\\b" << "\\bZCk\\b" << "\\bNCk\\b"
+                  << "\\bMemRead\\b" << "\\bMemWrite\\b" << "^(\\s)*UnitPre(?=:)\\b" << "^(\\s)*UnitPost(?=:)\\b"
+                     // pre/post symbols:
+                  << "\\bN\\b" << "\\bZ\\b" << "\\bV\\b"
+                  << "\\bX\\b" << "\\bSP\\b" << "\\bPC\\b" << "\\bIR\\b"
+                  << "\\bT1\\b" << "\\bT2\\b" << "\\bT3\\b" << "\\bT4\\b"
+                  << "\\bT5\\b" << "\\bT6\\b" << "\\bMem\\b";
     foreach (const QString &pattern, oprndPatterns) {
         rule.pattern = QRegExp(pattern);
         rule.format = oprndFormat;
