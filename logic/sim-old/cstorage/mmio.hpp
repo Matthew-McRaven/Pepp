d1898817f23f4d44edec00b5547080ea39d5e9d7
Matthew McRaven <mkm302@georgetown.edu>
2021-07-16 15:56:48 -0700

    feat(sim): begin implementing MM input
    Sketch out interface or MMIO.
    Committing now so that I can switch to a better dev machine.

--- logic/sim-old/cstorage/mmio.hpp
+++ logic/sim-old/cstorage/mmio.hpp
@@ -2,15 +2,42 @@
 
 #include "base.hpp"
 #include "helper.hpp"
+#include "pubsub.hpp"
 
 namespace components::storage 
 {
+namespace components::storage{
+
+// TODO: Refactor using a memory span.
 template<typename offset_t, typename val_size_t=uint8_t>
 	requires (components::storage::UnsignedIntegral<offset_t> && components::storage::Integral<val_size_t>)
-class input: public components::storage::storage_base<offset_t, val_size_t>
+class Input: public components::storage::Base<offset_t, val_size_t>
 {
+public:
+	// TODO: Rule of 5.
+	// TODO: Copy-swap.
+	Input(offset_t max_offset);
+    virtual ~Input() = default;
+	void clear(val_size_t fill_val=0) override;
+    // Read / Write functions that may generate signals or trap for IO.
+    val_size_t read(offset_t offset) const override;
+	val_size_t get(offset_t offset) const override;
+    void write(offset_t offset, val_size_t value) override;
+    void set(offset_t offset, val_size_t value) override;
 
+	val_size_t unread(offset_t offset) override;
+	val_size_t unwrite(offset_t offset) override;
+
+	// Number of bytes contained by this chip
+    // offset_t max_offset() const noexcept;
+    // Change the size of the chip at runtime, to avoid creating and deleting
+    // an excessive number of chip instances.
+    void resize(offset_t new_offset) noexcept override;
+private:
+	components::storage::Channel<offset_t, val_size_t> _storage;
+	components::storage::Channel<offset_t, val_size_t>::Endpoint _endpoint;
 };
 
-}
+}; // End namespace components::storage
+
 #include "mmio.tpp"
