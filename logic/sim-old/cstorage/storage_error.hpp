fee48f75079324b7442e6113cf9caec188a54cbc
Matthew McRaven <mkm302@georgetown.edu>
2021-07-18 14:24:21 -0700

    refactor(sim): convert to status_code
    error_codes couldn't be returned in an outcome.
    This made them useless.
    Therefore, we are going to use the new status-code.
    It is described here:
    https://github.com/ned14/status-code.
    
    It is integrated as part of my outcome.hpp library.

--- logic/sim-old/cstorage/storage_error.hpp
+++ logic/sim-old/cstorage/storage_error.hpp
@@ -1,10 +1,8 @@
-/*
- * Implementation derived from:
- * https://ned14.github.io/outcome/motivation/plug_error_code/
- */
-#include <iostream>
-#include <string>        // for string printing
-#include <system_error>  // bring in std::error_code et al
+#pragma once
+// Taken from:
+// https://github.com/ned14/status-code
+#include <initializer_list>
+#include <outcome.hpp>
 
 // This is the custom error code enum
 enum class StorageErrc
@@ -13,69 +11,40 @@ enum class StorageErrc
   NoMMInput, // Storage operation failed because there was no memory-mapped input available. This is recoverable.
   ResizeError, // Attempted to resize a component which may not be resized. This is recoverable by creating additional instances of the class.
   Unwritable, // Write failed because the device doesn't support writing. The value written was ignored. This is recoverable.
-  NoAvailableHistory, // Attempted to read a storage device whose history has been exhausted. This is recoverable.
-  HistoryDisabled // Attempted to read a storage device which has history disabled. This is recoverable.
+  NoAvailableDelta, // Attempted to read a storage device whose history has been exhausted. This is recoverable.
+  DeltaDisabled // Attempted to read a storage device which has history disabled. This is recoverable.
 };
 
-namespace std
+// To synthesise a custom status code domain for `StorageErrc`, inject the following
+// template specialisation:
+SYSTEM_ERROR2_NAMESPACE_BEGIN
+template <>
+struct quick_status_code_from_enum<StorageErrc>
+  : quick_status_code_from_enum_defaults<StorageErrc>
 {
-  // Tell the C++ 11 STL metaprogramming that enum StorageErrc
-  // is registered with the standard error code system
-  template <> struct is_error_code_enum<StorageErrc> : true_type
+  // Text name of the enum
+  static constexpr const auto domain_name = "Storage Error";
+
+  // Unique UUID for the enum. PLEASE use https://www.random.org/cgi-bin/randbyte?nbytes=16&format=h
+  // Check: I generated a unique UUID-v4 from the random.org website and then converted it to a hex string.
+  static constexpr const auto domain_uuid = "{40c3de8a-3448-4b5b-8f94-a380b813d223}";
+
+  // Map of each enum value to its text string, and list of semantically equivalent errc's
+  static const std::initializer_list<mapping> &value_mappings()
   {
-  };
-}
+    static const std::initializer_list<mapping> v = {
+    // Format is: { enum value, "string representation", { list of errc mappings ... } }
+    {StorageErrc::Success, "Storage access successful", {errc::success}},
+    {StorageErrc::NoMMInput, "No memory-mapped input available", {errc::io_error}},
+    {StorageErrc::ResizeError, "Storage resize failed", {errc::not_enough_memory}},
+    {StorageErrc::Unwritable, "Storage cannot be written to", {errc::not_supported}},
+    {StorageErrc::NoAvailableDelta, "No delta is stored in this class", {errc::no_message}},
+    {StorageErrc::DeltaDisabled, "Instance has deltas disabled", {errc::not_supported}},
+    };
+    return v;
+  }
+};
+SYSTEM_ERROR2_NAMESPACE_END
 
-namespace detail
-{
-  // Define a custom error code category derived from std::error_category
-  class StorageErrc_category : public std::error_category
-  {
-  public:
-    // Return a short descriptive name for the category
-    virtual const char *name() const noexcept override final { return "StorageError"; }
-    // Return what each enum means in text
-    virtual std::string message(int c) const override final
-    {
-      switch (static_cast<StorageErrc>(c))
-      {
-      case StorageErrc::Success:
-        return "Storage access successful";
-      case StorageErrc::NoMMInput:
-        return "No memory-mapped input available";
-      case StorageErrc::ResizeError:
-        return "Storage resize failed";
-      default:
-        return "unknown";
-      }
-    }
-    // OPTIONAL: Allow generic error conditions to be compared to me
-    virtual std::error_condition default_error_condition(int c) const noexcept override final
-    {
-      switch (static_cast<StorageErrc>(c))
-      {
-      case StorageErrc::NoMMInput:
-        return make_error_condition(std::errc::io_error);
-      case StorageErrc::ResizeError:
-        return make_error_condition(std::errc::not_enough_memory);
-      default:
-        // I have no mapping for this code
-        return std::error_condition(c, *this);
-      }
-    }
-  };
-}
-
-// Declare a global function returning a static instance of the custom category
-extern inline const detail::StorageErrc_category &StorageErrc_category()
-{
-  static detail::StorageErrc_category c;
-  return c;
-}
-
-// Overload the global make_error_code() free function with our
-// custom enum. It will be found via ADL by the compiler if needed.
-inline std::error_code make_error_code(StorageErrc e)
-{
-  return {static_cast<int>(e), StorageErrc_category()};
-}
+// Kind helper method to convert enum to type-erased system code.
+SYSTEM_ERROR2_NAMESPACE::system_code status_code(StorageErrc c) { return c; }
