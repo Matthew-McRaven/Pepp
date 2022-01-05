92e57488523451265c76ada556d2b2f9e25593d7
Matthew McRaven <mkm302@georgetown.edu>
2022-01-05 16:33:04 +0000

    fix(config): fix broken eslint rules
    JSX should be allowed in TSX files (line 33)
    Peer dependencies exist for a reason--stop warning about extraneous deps.
    TS forbids extensions, so ESLint needs to stop teeling me to add the,.

--- config/eslint/.eslintrc.js
+++ config/eslint/.eslintrc.js
@@ -4,9 +4,18 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -21,5 +30,8 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.[tj]sx', '**/*.spec.[tj]s?x', '**/*.setup.[tj]s?x'], peerDependencies: true }],
+    'import/extensions': ['error', { tsx: 'never', ts: 'never' }],
   },
 };
