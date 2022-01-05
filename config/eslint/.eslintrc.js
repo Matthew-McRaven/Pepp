1b0e10c41a6d4e902163540c669bb198e8d41b5c
Matthew McRaven <mkm302@georgetown.edu>
2022-01-05 02:10:25 +0000

    style(config): switch to AirBnb style guide

---
+++ config/eslint/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    './config/base.eslintrc.json',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
