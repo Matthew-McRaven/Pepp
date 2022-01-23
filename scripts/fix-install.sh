11333fd9478c648506f4ab2132e6b37d2c915dfa
Matthew McRaven <mkm302@georgetown.edu>
2022-01-23 18:38:36 +0000

    feat(script): add script to fix broken node-gyp packages
    Onigurama, nsfw are being evil.
    They have no install script
    Therefore, they break node-gyp prebuild, and consequently electron-builder.
    I fix this by appending an install script to their package.json

---
+++ scripts/fix-install.sh
@@ -0,0 +1,6 @@
+packages=( 'nsfw' 'oniguruma')
+for package in ${packages[@]}; do
+	cat node_modules/${package}/package.json > /tmp/package.json
+	rm node_modules/${package}/package.json
+	jq '. * {scripts:{"install":""}}' < /tmp/package.json >> node_modules/${package}/package.json
+done
