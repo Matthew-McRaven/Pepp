9669114342e66378d96d4cd8841fbe9e4b9c74ac
Matthew McRaven <mkm302@georgetown.edu>
2022-02-06 20:13:23 +0000

    test(ui.mem): add basic unit test to HexEditor

---
+++ ui/js-memory/packages/ui/memory/src/HexEditor/tests/Styled.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { StyledHexEditor } from '..';
+
+describe('<ObjectCodePane />', () => {
+  it('has been mounted', () => {
+    // `data` contains the bytes to show. It can also be `Uint8Array`!
+    const data = new Array(100).fill(0);
+    // If `data` is large, you probably want it to be mutable rather than cloning it over and over.
+    // `nonce` can be used to update the editor when `data` is reference that does not change.
+    let nonce = 0;
+    // The callback facilitates updates to the source data.
+    const handleSetValue = React.useCallback((offset, value) => {
+      data[offset] = value;
+      nonce += 1;
+    }, [data]);
+    const component = shallow(<StyledHexEditor
+      showAscii
+      columns={0x10}
+      data={data}
+      nonce={nonce}
+      onSetValue={handleSetValue}
+    />);
+    expect(component.length).toBe(1);
+  });
+});
