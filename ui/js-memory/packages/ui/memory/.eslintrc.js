47e10ac60bb8de9bc106c95ba3371844b772aa7f
Matthew McRaven <mkm302@georgetown.edu>
2022-02-06 19:43:56 +0000

    build(ui.mem): remove unused files in react-hex-editor

--- ui/js-memory/packages/ui/memory/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: 'react-app',
-  rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    // Rule defined in react-app but no longer valid
-    '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
-  },
-};
