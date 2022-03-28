77882e51de0d4d32ded7c3e98f93bb3eb8b66d75
Matthew McRaven <mkm302@georgetown.edu>
2022-03-28 14:40:11 +0000

    fix(ui.mem): expose HexEditorHandle, MemoryLike
    This is necessary for implementing the FlatMemoryView without reaching into the guts of the HexEditor.

--- ui/js-memory/packages/ui/memory/src/HexEditor/index.ts
+++ ui/js-memory/packages/ui/memory/src/HexEditor/index.ts
@@ -2,3 +2,5 @@ export { default as UnstyledBaseHexEditor } from './components/HexEditor';
 export { default as UnstyledHexEditor } from './components/AutoSizeHexEditor';
 export { default as BaseHexEditor } from './components/StyledHexEditor';
 export { default as StyledHexEditor } from './components/StyledAutoSizeHexEditor';
+export type { HexEditorHandle } from './types';
+export { MemoryLike } from './components/MemoryLike';
