484123c62ea7fe2a08aaf67461f3b07cb9982342
Matthew McRaven <mkm302@georgetown.edu>
2022-04-03 19:41:50 +0000

    test(ui.mem): add mounting test for FlatMemoryVIew

--- ui/js-memory/packages/ui/memory/src/FlatMemoryView/FlatMemoryView.test.tsx
+++ ui/js-memory/packages/ui/memory/src/FlatMemoryView/FlatMemoryView.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import FlatMemoryView from './FlatMemoryView';
+import * as HexEditor from '../HexEditor';
 
 describe('<FlatMemoryView />', () => {
   it('has been mounted', () => {
-    const component = shallow(<FlatMemoryView />);
+    // `data` contains the bytes to show. It can also be `Uint8Array`!
+    const data = new Uint8Array(100).fill(0);
+    const component = shallow(<FlatMemoryView
+      columns={0x10}
+      data={new HexEditor.MemoryLike(data)}
+    />);
     expect(component.length).toBe(1);
   });
 });
