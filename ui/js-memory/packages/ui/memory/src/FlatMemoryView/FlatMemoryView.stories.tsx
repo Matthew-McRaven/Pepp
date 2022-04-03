71702ee666610566b1ddcfbcc9de9e8dd1b8e31f
Matthew McRaven <mkm302@georgetown.edu>
2022-04-03 19:41:02 +0000

    refactor(ui.mem): move FlatMemoryView implementation out of story

--- ui/js-memory/packages/ui/memory/src/FlatMemoryView/FlatMemoryView.stories.tsx
+++ ui/js-memory/packages/ui/memory/src/FlatMemoryView/FlatMemoryView.stories.tsx
@@ -1,80 +1,23 @@
-import React, { useState, useCallback } from 'react';
+import React, { useMemo } from 'react';
 
 import * as HexEditor from '../HexEditor';
-import oneDarkPro from '../HexEditor/themes/oneDarkPro';
-import { IntegralConverter } from '../../../converters/src/IntegralConverter';
 
+// FlatMemoryViewNamespace
+import * as FMVNS from './FlatMemoryView';
+
+const FMV = FMVNS.default;
 export default {
   title: 'Memory/FlatMemoryView',
-  component: HexEditor.StyledHexEditor,
+  component: FMV,
   argTypes: {
   },
 };
 
-const StyledHexEditorTemplate = () => {
+const FlatMemoryViewTemplate = () => {
   // `data` contains the bytes to show. It can also be `Uint8Array`!
-  const data = React.useMemo<Uint8Array>(() => new Uint8Array(10000).fill(0), []);
+  const data = useMemo<Uint8Array>(() => new Uint8Array(0x10000).fill(0), []);
   const memHandle = new HexEditor.MemoryLike(data);
-  // If `data` is large, you probably want it to be mutable rather than cloning it over and over.
-  // `nonce` can be used to update the editor when `data` is reference that does not change.
-  const [nonce, setNonce] = useState(0);
-  // The callback facilitates updates to the source data.
-  const handleSetValue = React.useCallback((offset, value) => {
-    memHandle.set(offset, value);
-    setNonce((v: number) => (v + 1));
-  }, [data]);
-  const [width, setWidth] = React.useState(200);
-  const [ref, setRef] = React.useState<{ current?: HexEditor.HexEditorHandle }>({});
-
-  const measureRef = useCallback((node: HexEditor.HexEditorHandle) => {
-    setRef({ current: node });
-    if (node) setWidth(node.width);
-  }, []);
-
-  const it = (
-    <HexEditor.StyledHexEditor
-      showAscii
-      columns={0x10}
-      data={memHandle}
-      nonce={nonce}
-      onSetValue={handleSetValue}
-      theme={{ hexEditor: oneDarkPro }}
-      ref={measureRef}
-    />
-  );
-  const [value, setValue] = React.useState(0);
-
-  return (
-    <div style={{ maxWidth: width }}>
-      <div style={{ height: '500px' }}>
-        {it}
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex' }}>
-          Scroll to:
-          <IntegralConverter
-            base={16}
-            byteLength={2}
-            state={value}
-            setState={(newState: number) => {
-              ref.current?.scrollTo(newState);
-              setValue(newState);
-            }}
-            error={() => { }}
-          />
-        </div>
-        <button type="button" onClick={() => { ref.current?.scrollTo(100); }}>
-          SP
-        </button>
-        <button type="button" onClick={() => { ref.current?.scrollTo(100); }}>
-          PC
-        </button>
-        <button type="button" onClick={() => { ref.current?.scrollTo(100); }}>
-          T
-        </button>
-      </div>
-    </div>
-  );
+  return <FMV data={memHandle} columns={0x10} />;
 };
 
-export const FlatMemoryView = StyledHexEditorTemplate.bind({});
+export const FlatMemoryView = FlatMemoryViewTemplate.bind({});
--- ui/js-memory/packages/ui/memory/src/FlatMemoryView/FlatMemoryView.tsx
+++ ui/js-memory/packages/ui/memory/src/FlatMemoryView/FlatMemoryView.tsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './FlatMemoryView.scss';
 
+import * as HexEditor from '../HexEditor';
+import oneDarkPro from '../HexEditor/themes/oneDarkPro';
+import { IntegralConverter } from '../../../converters/src/IntegralConverter';
+
 export interface FlatMemoryViewProps {
-  heading: string;
-  content: React.ReactNode;
+  data: HexEditor.MemoryLike
+  columns: number
 }
 
-const FlatMemoryView = () => <div className="FlatMemoryView" data-testid="FlatMemoryView" />;
+const FlatMemoryView = (props: FlatMemoryViewProps) => {
+  const { data, columns } = props;
+  // If `data` is large, you probably want it to be mutable rather than cloning it over and over.
+  // `nonce` can be used to update the editor when `data` is reference that does not change.
+  const [nonce, setNonce] = useState(0);
+  // The callback facilitates updates to the source data.
+  const handleSetValue = React.useCallback((offset, value) => {
+    data.set(offset, value);
+    setNonce((v: number) => (v + 1));
+  }, [data]);
+
+  const [width, setWidth] = React.useState(200);
+  const [ref, setRef] = React.useState<{ current?: HexEditor.HexEditorHandle }>({});
+
+  const measureRef = useCallback((node: HexEditor.HexEditorHandle) => {
+    setRef({ current: node });
+    if (node) setWidth(node.width);
+  }, []);
+
+  const it = (
+    <HexEditor.StyledHexEditor
+      showRowLabels
+      showAscii
+      columns={columns}
+      data={data}
+      nonce={nonce}
+      onSetValue={handleSetValue}
+      theme={{ hexEditor: oneDarkPro }}
+      ref={measureRef}
+    />
+  );
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <div style={{ maxWidth: width }}>
+      <div style={{ height: '500px' }}>
+        {it}
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex' }}>
+          Scroll to:
+          <IntegralConverter
+            base={16}
+            byteLength={2}
+            state={value}
+            setState={(newState: number) => {
+              ref.current?.scrollToItem(Math.floor(newState / columns), 'start');
+              setValue(newState);
+            }}
+            error={() => { }}
+          />
+        </div>
+        <button type="button" onClick={() => { ref.current?.scrollTo(100); }}>
+          SP
+        </button>
+        <button type="button" onClick={() => { ref.current?.scrollTo(100); }}>
+          PC
+        </button>
+        <button type="button" onClick={() => { ref.current?.scrollTo(100); }}>
+          T
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default FlatMemoryView;
