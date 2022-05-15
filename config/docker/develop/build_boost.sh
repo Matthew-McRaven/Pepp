c29be42d2b9d3a246bf4c920e454b850995610c1
Matthew McRaven <matthew.mcraven@gmail.com>
2022-05-15 14:02:54 -0700

    build(docker): purge unused boost config files

--- config/docker/develop/build_boost.sh
+++
@@ -1,5 +0,0 @@
-
-#cp user-config.jam boost/
-echo "using python : 3.8 : ../../build/python3/usr/local/bin/python3 : ../../build/python3/usr/local/include/python3.8 : ../../build/python3/usr/local/lib/python3.8 : ;" > boost/user-config.jam
-cd boost && ./bootstrap.sh --prefix=../../build/boost --with-python-version=3.9 && \
-./b2 cxxflags="-fPIC -std=c++17" --prefix=../../build/boost --user-config=./user-config.jam address-model=64 link=static runtime-link=static -j 20 threading=multi install
--- config/docker/develop/download_boost.sh
+++
@@ -1,3 +0,0 @@
-wget https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.bz2
-mkdir boost
-tar -xf boost_1_75_0.tar.bz2 -C ./boost --strip-components 1
