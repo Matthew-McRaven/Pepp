b298ada24da66da91e1891b2f875d2729099343d
Matthew McRaven <mkm302@georgetown.edu>
2022-09-16 17:07:20 -0700

    chore(config): update eslint configs
    Ported from some other projects.
    This should make it interact with TS more nicely.

--- config/eslint/.eslintrc.js
+++ config/eslint/.eslintrc.js
@@ -34,12 +34,16 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
     'import/no-extraneous-dependencies':
-      ['error', { devDependencies: ['**/*.test.tsx', '**/*.setup.ts'], peerDependencies: true }],
+            ['error', { devDependencies: ['**/*.test.tsx', '**/*.setup.ts'], peerDependencies: true }],
     'import/extensions': ['error', { tsx: 'never', ts: 'never' }],
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
     'max-len': ['warn', { code: 120, ignoreComments: true }],
     'eol-last': ['error', 'always'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
 
   },
 };
