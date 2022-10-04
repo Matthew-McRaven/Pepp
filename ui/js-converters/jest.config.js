f34eb5c220c114a3c335a938773e427f505dc013
Matthew McRaven <mkm302@georgetown.edu>
2022-10-03 17:10:25 -0700

    build(ui): change package type to  module
    Downstream projects crash when importing converters.
    Downstream projects `import` this module in their sources.
    This causes the *.es.js variant to be selected, which itself contains imports.
    
    Prior to this commit, this packages was CommonJS.
    This means that the *.js are interpreted as CommonJS.
    
    Therefore, our *.es.js variant (with imports) is treated as CommonJS.
    Additionally, main+module package fields seems to be messing with exports.
    
    To resolve this we must make the package a module.
    We must also fix our jest config to be module compliant.

--- ui/js-converters/jest.config.js
+++ ui/js-converters/jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   testEnvironment: 'jsdom',
   roots: ['src'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
@@ -11,7 +11,7 @@ module.exports = {
   moduleNameMapper: {
     // Mocks out all these file formats when tests are run.
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      'identity-obj-proxy',
+            'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
