f67c7603312c6795a9eb357f9242ba87edf8fa96
Matthew McRaven <mkm302@georgetown.edu>
2022-10-25 08:03:14 -0700

    refactor(target.p10): extend BigInt conversion to native bindings

--- logic/targets/pep10/src/bind/isa/index.ts
+++ logic/targets/pep10/src/bind/isa/index.ts
@@ -7,18 +7,18 @@ export type unaryMnemonicToOpcode = (mnemonic: string) => Uint8Array | undefined
 export type nonunaryMnemonicToOpcode = (mnemonic: string, addrMode:string) => Uint8Array | undefined;
 
 // Undefined symbols take on the value 0, as they can be patched later by the linker.
-export type symbolLookup = (name:string)=> number;
+export type symbolLookup = (name:string)=> bigint;
 
-const splitIntoBytes = (arg:number, count: number):Uint8Array => {
+const splitIntoBytes = (arg:bigint, count: number):Uint8Array => {
   const ret = new Uint8Array(count);
   let it = 0;
   let temp = arg;
   while (it < count) {
-    ret[count - it - 1] = temp % 256;
+    ret[count - it - 1] = Number(temp % 256n);
     it += 1;
     // TODO: Handle 64 bit ints
     // eslint-disable-next-line no-bitwise
-    temp >>= 8;
+    temp >>= 8n;
   }
   return ret;
 };
@@ -34,7 +34,7 @@ export const argToBytes = (arg:Arg, lut:symbolLookup) => {
     case 'char': throw new Error('Cannot yet handle');
     case 'string': throw new Error('Cannot yet handle');
     default:
-      return splitIntoBytes(0, 2);
+      return splitIntoBytes(0n, 2);
   }
 };
 
@@ -47,6 +47,6 @@ export const argtoNumber = (arg: Arg, lut:symbolLookup) => {
     case 'char': throw new Error('Cannot yet handle');
     case 'string': throw new Error('Cannot yet handle');
     default:
-      return 0;
+      return 0n;
   }
 };
