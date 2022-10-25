7eebd5bfe39baf6fd105177cd58ddc0e39a3fe6e
Matthew McRaven <mkm302@georgetown.edu>
2022-10-24 21:37:56 -0700

    chore(target.p10): remove useless print in tests [skip-ci]

--- logic/targets/pep10/test/asm/visitors/hex_code/macroless.test.ts
+++ logic/targets/pep10/test/asm/visitors/hex_code/macroless.test.ts
@@ -4,7 +4,7 @@ describe('asm.visit.extractSymbols', () => {
   it('processes program stress test', () => {
     const tree = asm.peg.parseRoot('a:.WORD 5\nADDA x,d\nasra\nLDWA 1887,i');
     asm.visit.extractSymbols(tree);
-    console.log(asm.visit.treeToHex(tree));
+    // console.log(asm.visit.treeToHex(tree));
     expect(asm.visit.treeToHex(tree)).toEqual(Uint8Array.from([0, 5, 161, 0, 0, 22, 64, 7, 95]));
   });
 });
