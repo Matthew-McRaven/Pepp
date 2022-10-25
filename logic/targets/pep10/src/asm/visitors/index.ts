0318775b0ab47da46e7cde8b3da4c61f190a7c4b
Matthew McRaven <mkm302@georgetown.edu>
2022-10-24 21:45:59 -0700

    fix(target.p10): add missing visitor include

--- logic/targets/pep10/src/asm/visitors/index.ts
+++ logic/targets/pep10/src/asm/visitors/index.ts
@@ -4,3 +4,4 @@ export { extractSymbols } from './extract_symbols';
 export { treeToHex, nodeToHex } from './hex_code';
 export { treeSize, nodeSize } from './size';
 export { setTreeAddresses } from './address_assign';
+export { registerSystemCalls } from './register_system_calls'
