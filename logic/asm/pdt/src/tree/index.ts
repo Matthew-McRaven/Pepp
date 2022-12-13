bf5850b95b8aa8a4d9500fb549983d72ba70e2e4
Matthew McRaven <mkm302@georgetown.edu>
2022-12-13 13:24:53 -0800

    fix(asm.pdt): remove level of indirection from `Node` type

--- logic/asm/pdt/src/tree/index.ts
+++ logic/asm/pdt/src/tree/index.ts
@@ -3,5 +3,5 @@ export * as Clock from './clock';
 export * as Device from './device';
 export * as Logic from './logic';
 export * as Root from './root';
-export * as Node from './node';
+export { Node } from './node';
 export * as Socket from './socket';
