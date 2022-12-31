0292c147e47eee4d82443e6699df7d53fdb9129e
Matthew McRaven <matthew.mcraven@gmail.com>
2022-12-31 07:05:50 -0800

    feat(dev.iface): properly export types from package
    Also bump to v3.0.1.

--- logic/device/interface/src/index.ts
+++ logic/device/interface/src/index.ts
@@ -1,2 +1,11 @@
 export { DevicePOD, NonNativeDevice, NativeDevice } from './device';
+export { InterposeResult, NonNativeInterposer, NativeInterposer } from './interposer';
+export {
+  TickMode, Scheduler, SystemTarget, SystemClock, SystemInitiator, SystemClocked, System,
+} from './system';
 export { NonNativeTarget, NativeTarget } from './target';
+export { Operation } from './target_operation';
+export {
+  TraceBuffer, TraceCommitHook, TraceCommitHookFn, TraceBufferStatus,
+} from './trace';
+export * as Trace from './trace/index';
