99560b8214e57eeb78fb25edc5c13d91e47754b7
Matthew McRaven <matthew.mcraven@gmail.com>
2023-01-09 07:30:01 -0800

    fix(dev): update package exports
    For JMTM

---
+++ logic/device/jmtm/src/index.ts
@@ -0,0 +1,3 @@
+export { MTM0 as JMTM0, getRegistration as getMTM0Registration } from './mtm0';
+export { MTM1 as JMTM1, getRegistration as getMTM1Registration } from './mtm1';
+export * as Socket from './mtm_socket';
