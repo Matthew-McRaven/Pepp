8dffb0c39a8423c427c863d0b1b3e1efd433761e
Matthew McRaven <mkm302@georgetown.edu>
2023-01-20 08:22:33 -0800

    feat(sim.ztb): add minimal CZTb implementation
    Still missing tests, TS wrapper.

--- logic/sim/cztb/src/dummy/sample.cpp
+++
@@ -1,8 +0,0 @@
-#include "sample.hpp"
-#include <random>
-double Test::foo() {
-  std::random_device rd;
-  std::mt19937 e2(rd());
-  std::uniform_real_distribution<> dist(0, 10);
-  return dist(e2);
-}
