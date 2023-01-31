5e94504ce4d51d07a85f7543ee5506a43f10a10d
Matthew McRaven <mkm302@georgetown.edu>
2023-01-31 07:26:24 -0800

    feat(asm.bi): add Qt data classes for figures, macros, and books

---
+++ logic/asm/builtins2/src/builtins/book.cpp
@@ -0,0 +1,70 @@
+#include "book.hpp"
+#include "figure.hpp"
+builtins::Book::Book(QString name) : QObject(nullptr), _name(name) {}
+
+QString builtins::Book::name() const { return _name; }
+
+const QList<QSharedPointer<builtins::Figure>> builtins::Book::figures() const {
+  return _figures;
+}
+
+QSharedPointer<const builtins::Figure>
+builtins::Book::findFigure(QString chapter, QString figure) const {
+  QList<QSharedPointer<const builtins::Figure>> temp;
+  for (auto figurePtr : _figures) {
+    if (figurePtr->chapterName() == chapter &&
+        figurePtr->figureName() == figure) {
+      temp.push_back(figurePtr);
+    }
+  }
+  if (auto length = temp.length(); length == 0)
+    return nullptr;
+  else if (length == 1)
+    return temp.first();
+  else {
+    qDebug()
+        << (u"More than one copy of figure {}.{}"_qs).arg(chapter).arg(figure);
+    return nullptr;
+  }
+}
+
+bool builtins::Book::addFigure(QSharedPointer<Figure> figure) {
+  // TODO: Adding N figures will take N^2 time because of the calls to find.
+  // Will be necessary to speed this up for large N.
+  if (findFigure(figure->chapterName(), figure->figureName()) == nullptr) {
+    _figures.push_back(figure);
+    return true;
+  } else
+    return false;
+}
+
+const QList<QSharedPointer<builtins::Macro>> builtins::Book::macros() const {
+  return _macros;
+}
+
+QSharedPointer<const builtins::Macro>
+builtins::Book::findMacro(QString name) const {
+  QList<QSharedPointer<const builtins::Macro>> temp;
+  for (auto macroPtr : _macros) {
+    if (macroPtr->name == name)
+      temp.push_back(macroPtr);
+  }
+  if (auto length = temp.length(); length == 0)
+    return nullptr;
+  else if (length == 1)
+    return temp.first();
+  else {
+    qDebug() << (u"More than one copy of macro {}"_qs).arg(name);
+    return nullptr;
+  }
+}
+
+bool builtins::Book::addMacro(QSharedPointer<Macro> macro) {
+  // TODO: Adding N macros will take N^2 time because of the calls to find.
+  // Will be necessary to speed this up for large N.
+  if (findMacro(macro->name) == nullptr) {
+    _macros.push_back(macro);
+    return true;
+  } else
+    return false;
+}
---
+++ logic/asm/builtins2/src/builtins/figure.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "elements.hpp"
+#include <QtCore>
+namespace builtins {
+class Figure : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(builtins::Architecture arch READ arch CONSTANT);
+  Q_PROPERTY(QString chapterName READ chapterName CONSTANT);
+  Q_PROPERTY(QString figureName READ figureName CONSTANT);
+  Q_PROPERTY(bool isOS READ isOS WRITE setIsOS NOTIFY isOSChanged);
+  Q_PROPERTY(QSharedPointer<const Figure> defaultOS READ defaultOS WRITE
+                 setDefaultOS NOTIFY defaultOSChanged);
+  Q_PROPERTY(const QList<QSharedPointer<builtins::Test>> tests READ tests NOTIFY
+                 testsChanged);
+  Q_PROPERTY(const QMap<QString, QSharedPointer<builtins::Element>> elements
+                 READ elements NOTIFY elementsChanged);
+
+public:
+  Figure(Architecture arch, QString chapter, QString figure);
+
+  builtins::Architecture arch() const;
+
+  QString chapterName() const;
+
+  QString figureName() const;
+
+  bool isOS() const;
+  bool setIsOS(bool value);
+
+  QSharedPointer<const Figure> defaultOS() const;
+  bool setDefaultOS(QSharedPointer<const Figure>);
+
+  const QList<QSharedPointer<builtins::Test>> tests() const;
+  void addTest(QSharedPointer<builtins::Test> test);
+
+  const QMap<QString, QSharedPointer<builtins::Element>> elements() const;
+  bool addElement(QString name, QSharedPointer<builtins::Element> element);
+
+signals:
+  void isOSChanged();
+  void defaultOSChanged();
+  void testsChanged();
+  void elementsChanged();
+
+private:
+  const Architecture _arch;
+  const QString _chapterName, _figureName;
+  bool _isOS = false;
+  QSharedPointer<const Figure> _defaultOS = nullptr;
+  QList<QSharedPointer<builtins::Test>> _tests = {};
+  QMap<QString, QSharedPointer<builtins::Element>> _elements = {};
+};
+} // end namespace builtins
