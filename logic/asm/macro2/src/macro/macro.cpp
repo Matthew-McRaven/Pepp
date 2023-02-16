b31e495f0cbe3109261f60ecdd2626acb1c848dc
Matthew McRaven <mkm302@georgetown.edu>
2023-02-15 20:19:25 -0800

    feat(asm.macro)!: make architecture a required field of a parsed macro
    This will allow us to combine asm.bi's macro into this one.

--- logic/asm/macro2/src/macro/macro.cpp
+++ logic/asm/macro2/src/macro/macro.cpp
@@ -1,14 +1,17 @@
 #include "./macro.hpp"
 
 macro::Parsed::Parsed(QString name, quint8 argCount, QString body,
-                      QObject *parent)
-    : QObject(parent), _name(name), _body(body), _argCount(argCount) {}
+                      QString architecture, QObject *parent)
+    : QObject(parent), _name(name), _body(body), _architecture(architecture),
+      _argCount(argCount) {}
 
 QString macro::Parsed::name() const { return _name; }
 
 QString macro::Parsed::body() const { return _body; }
 
 quint8 macro::Parsed::argCount() const { return _argCount; }
+
+QString macro::Parsed::architecture() const { return _architecture; }
 
 macro::Registered::Registered(Type type, Parsed *contents, QObject *parent)
     : QObject(parent), _contents(contents), _type(type) {
