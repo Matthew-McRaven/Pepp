c189509da246f76d73780b83594c01be23fcc5cd
Matthew McRaven <mkm302@georgetown.edu>
2023-02-15 17:09:23 -0800

    feat(asm.macro): add macro registry
    The macro registry combines builtin macros, OS generated macros, and user macros with a searchable API.

---
+++ logic/asm/macro2/src/macro/registry.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QObject>
+
+#include "./types.hpp"
+
+namespace macro {
+
+class Parsed;
+class Registered;
+
+class Registry : public QObject {
+  Q_OBJECT
+public:
+  explicit Registry(QObject *parent = nullptr);
+  bool contains(QString name) const;
+  const Registered *findMacro(QString name) const;
+  // FIXME: Replace with an iterator so as not to force additional memory
+  // allocations.
+  QList<const Registered *> findMacrosByType(Type type) const;
+  void clear();
+  // Ownership of macro is always transfered to this.
+  // Returns nullptr if the macro already exists in the registry. In this case,
+  // registry will delete macro.
+  // Returned pointer is non-owning
+  const Registered *registerMacro(Type type, Parsed *macro);
+
+signals:
+  //! Emitted when a macro is successfully registered.
+  void macrosChanged();
+  //! Emitted when clear() is called.
+  void cleared();
+
+private:
+  //
+  QMap<QString, QSharedPointer<Registered>> _macros;
+};
+} // namespace macro
