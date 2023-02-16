1ebb61300d964574ed41c4c9a5c6232d620bdca4
Matthew McRaven <mkm302@georgetown.edu>
2023-02-15 17:07:41 -0800

    feat(asm.macro): add data classes to hold parsed & registered macros
    Parsed macros are the results of post-processing macro::analyze_macro_definition.
    Registered macros are parsed macros with a macro::Type assigned.

--- logic/asm/macro2/src/macro/macro.cpp
+++ logic/asm/macro2/src/macro/macro.cpp
@@ -1,6 +1,20 @@
-#include "macro.hpp"
+#include "./macro.hpp"
 
-Macro::Macro()
-{
+macro::Parsed::Parsed(QString name, quint8 argCount, QString body,
+                      QObject *parent)
+    : QObject(parent), _name(name), _body(body), _argCount(argCount) {}
 
+QString macro::Parsed::name() const { return _name; }
+
+QString macro::Parsed::body() const { return _body; }
+
+quint8 macro::Parsed::argCount() const { return _argCount; }
+
+macro::Registered::Registered(Type type, Parsed *contents, QObject *parent)
+    : QObject(parent), _contents(contents), _type(type) {
+  contents->setParent(this);
 }
+
+const macro::Parsed *macro::Registered::contents() const { return _contents; }
+
+macro::Type macro::Registered::type() const { return _type; }
--- logic/asm/macro2/src/macro/macro.hpp
+++ logic/asm/macro2/src/macro/macro.hpp
@@ -2,8 +2,37 @@
 
 #include <QObject>
 
-class Macro : public QObject {
+#include "./types.hpp"
+namespace macro {
+class Parsed : public QObject {
   Q_OBJECT
+  Q_PROPERTY(QString name READ name CONSTANT);
+  Q_PROPERTY(QString body READ body CONSTANT);
+  Q_PROPERTY(quint8 argCount READ argCount CONSTANT);
+
 public:
-  Macro();
+  Parsed(QString name, quint8 argCount, QString body,
+         QObject *parent = nullptr);
+  QString name() const;
+  QString body() const;
+  quint8 argCount() const;
+
+private:
+  QString _name, _body;
+  quint8 _argCount;
 };
+class Registered : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(const Parsed *contents READ contents CONSTANT)
+  Q_PROPERTY(Type type READ type CONSTANT)
+public:
+  // Takes ownership of contents and changes its parent to this
+  Registered(Type type, Parsed *contents, QObject *parent = nullptr);
+  const Parsed *contents() const;
+  Type type() const;
+
+private:
+  const Parsed *_contents;
+  Type _type;
+};
+} // namespace macro
