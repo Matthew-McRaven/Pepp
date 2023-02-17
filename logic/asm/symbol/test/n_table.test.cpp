1ba7a93deaf3a2c1e14c6df1d4885483967dcdd3
Matthew McRaven <mkm302@georgetown.edu>
2023-02-16 17:09:50 -0800

    test(asm.sym): migrate to Qt unit test framework
    Closes #6.

--- logic/asm/symbol/test/n_table.test.cpp
+++ logic/asm/symbol/test/n_table.test.cpp
@@ -1,19 +1,21 @@
-#include "catch.hpp"
 
 #include "symbol/table.hpp"
 #include "symbol/visit.hpp"
+#include <QTest>
 
-/*
- *  Test Tree structure
- *          Branch (Parent)
- *          |             |
- *          Branch2     Leaf1
- *          |     |
- *        Leaf2 Leaf3
- */
-TEST_CASE("Validate functionality for multiple symbol tables.") {
+class SymbolNTable : public QObject {
+  Q_OBJECT
+private slots:
+  /*
+   *  Test Tree structure
+   *          Branch (Parent)
+   *          |             |
+   *          Branch2     Leaf1
+   *          |     |
+   *        Leaf2 Leaf3
+   */
 
-  SECTION("Check that local references are independent.") {
+  void independentReferences() {
     auto b1 = QSharedPointer<symbol::Table>::create();
     auto b2 = b1->addChild();
     auto l1 = b1->addChild();
@@ -26,12 +28,11 @@ TEST_CASE("Validate functionality for multiple symbol tables.") {
     // CHECK(x.use_count() == 2);
     // CHECK(y.use_count() == 2);
     // CHECK(z.use_count() == 2);
-    CHECK(x != y);
-    CHECK(x != z);
-    CHECK(y != z);
+    QCOMPARE_NE(x, y);
+    QCOMPARE_NE(x, z);
+    QCOMPARE_NE(y, z);
   }
-
-  SECTION("Find by name.") {
+  void findByName() {
     auto b1 = QSharedPointer<symbol::Table>::create();
     auto b2 = b1->addChild();
     auto l1 = b1->addChild();
@@ -43,40 +44,38 @@ TEST_CASE("Validate functionality for multiple symbol tables.") {
     auto y2 = l2->reference("hello");
     auto z1 = l3->reference("hello");
     auto z2 = l3->reference("hello");
-    CHECK(x1 == x2);
-    CHECK(y1 == y2);
-    CHECK(z1 == z2);
+    QCOMPARE(x1, x2);
+    QCOMPARE(y1, y2);
+    QCOMPARE(z1, z2);
     // Check that reference doesn't leak over.
-    CHECK(x1 != y1);
-    CHECK(x2 != z2);
-    CHECK(y2 != z1);
+    QCOMPARE_NE(x1, y1);
+    QCOMPARE_NE(x2, z2);
+    QCOMPARE_NE(y2, z1);
   }
-
   //  Dave: Added get tests
-  SECTION("Get by name using reference.") {
+  void getByName() {
     auto b1 = QSharedPointer<symbol::Table>::create();
     auto b2 = b1->addChild();
     auto l1 = b1->addChild();
     auto l2 = b2->addChild();
     auto l3 = b2->addChild();
     auto x = l1->get("hello");
-    CHECK(x == std::nullopt);
+    QCOMPARE(x, std::nullopt);
     auto y = l2->get("hello");
-    CHECK(y == std::nullopt);
+    QCOMPARE(y, std::nullopt);
     auto z = l3->get("hello");
-    CHECK(z == std::nullopt);
+    QCOMPARE(z, std::nullopt);
     auto x1 = l1->reference("hello");
     auto x2 = l1->get("hello");
-    CHECK(x2 == x1);
+    QCOMPARE(x2, x1);
     auto y1 = l2->define("hello"); //  Uses define instead of reference
     auto y2 = l2->get("hello");
-    CHECK(y2 == y1);
+    QCOMPARE(y2, y1);
     auto z1 = l3->define("hello"); //  Uses define instead of reference
     auto z2 = l3->get("hello");
-    CHECK(z2 == z2);
+    QCOMPARE(z2, z2);
   }
-
-  SECTION("Symbol existence checks.") {
+  void existenceChecks() {
     auto b1 = QSharedPointer<symbol::Table>::create();
     auto b2 = b1->addChild();
     auto l1 = b1->addChild();
@@ -87,32 +86,31 @@ TEST_CASE("Validate functionality for multiple symbol tables.") {
     auto z = l3->reference("z");
 
     // Each leaf should find it's own children regardless of policy.
-    CHECK(symbol::exists(l1, "x"));
-    CHECK(symbol::exists(l2, "y", symbol::TraversalPolicy::kSiblings));
-    CHECK(symbol::exists(l3, "z", symbol::TraversalPolicy::kWholeTree));
+    QVERIFY(symbol::exists(l1, "x"));
+    QVERIFY(symbol::exists(l2, "y", symbol::TraversalPolicy::kSiblings));
+    QVERIFY(symbol::exists(l3, "z", symbol::TraversalPolicy::kWholeTree));
 
     // Check that traversal policy is respected.
     // Leaf 2 should not be able to see leaf 3's symbol with kChildren.
     // Parent leaf can see all others
-    CHECK_FALSE(symbol::exists(l2, "z"));
-    CHECK_FALSE(symbol::exists(l3, "y"));
-    CHECK_FALSE(symbol::exists(l1, "z"));
-    CHECK_FALSE(symbol::exists(l1, "y"));
+    QVERIFY(!symbol::exists(l2, "z"));
+    QVERIFY(!symbol::exists(l3, "y"));
+    QVERIFY(!symbol::exists(l1, "z"));
+    QVERIFY(!symbol::exists(l1, "y"));
 
     //  Lower leafs can see other siblings but not parent with kSiblings
-    CHECK(symbol::exists(l2, "z", symbol::TraversalPolicy::kSiblings));
-    CHECK(symbol::exists(l3, "y", symbol::TraversalPolicy::kSiblings));
-    CHECK_FALSE(symbol::exists(l2, "x", symbol::TraversalPolicy::kSiblings));
-    CHECK_FALSE(symbol::exists(l3, "x", symbol::TraversalPolicy::kSiblings));
+    QVERIFY(symbol::exists(l2, "z", symbol::TraversalPolicy::kSiblings));
+    QVERIFY(symbol::exists(l3, "y", symbol::TraversalPolicy::kSiblings));
+    QVERIFY(!symbol::exists(l2, "x", symbol::TraversalPolicy::kSiblings));
+    QVERIFY(!symbol::exists(l3, "x", symbol::TraversalPolicy::kSiblings));
 
     //  Lower leafs can see parent and siblings
-    CHECK(symbol::exists(l2, "x", symbol::TraversalPolicy::kWholeTree));
-    CHECK(symbol::exists(l3, "x", symbol::TraversalPolicy::kWholeTree));
-    CHECK(symbol::exists(l1, "z", symbol::TraversalPolicy::kWholeTree));
-    CHECK(symbol::exists(l1, "y", symbol::TraversalPolicy::kWholeTree));
+    QVERIFY(symbol::exists(l2, "x", symbol::TraversalPolicy::kWholeTree));
+    QVERIFY(symbol::exists(l3, "x", symbol::TraversalPolicy::kWholeTree));
+    QVERIFY(symbol::exists(l1, "z", symbol::TraversalPolicy::kWholeTree));
+    QVERIFY(symbol::exists(l1, "y", symbol::TraversalPolicy::kWholeTree));
   }
-
-  SECTION("define() a local in one table does not affect the other.") {
+  void localityOfDefines() {
     auto b1 = QSharedPointer<symbol::Table>::create();
     auto b2 = b1->addChild();
     auto l1 = b1->addChild();
@@ -122,22 +120,21 @@ TEST_CASE("Validate functionality for multiple symbol tables.") {
     auto y = l2->define("hello");
     auto z = l3->reference("hello");
 
-    CHECK(x->state == symbol::DefinitionState::kUndefined);
+    QCOMPARE(x->state, symbol::DefinitionState::kUndefined);
     l1->define(x->name);
-    CHECK(x->state == symbol::DefinitionState::kSingle);
+    QCOMPARE(x->state, symbol::DefinitionState::kSingle);
     l1->define(x->name);
-    CHECK(x->state == symbol::DefinitionState::kMultiple);
+    QCOMPARE(x->state, symbol::DefinitionState::kMultiple);
     // Y was created and defined in one step. Check state
-    CHECK(y->state == symbol::DefinitionState::kSingle);
+    QCOMPARE(y->state, symbol::DefinitionState::kSingle);
     l2->define(y->name);
-    CHECK(y->state == symbol::DefinitionState::kMultiple);
+    QCOMPARE(y->state, symbol::DefinitionState::kMultiple);
 
     // Defining a local symbol doesn't affect the state of a symbol in another
     // table.
-    CHECK(z->state == symbol::DefinitionState::kUndefined);
+    QCOMPARE(z->state, symbol::DefinitionState::kUndefined);
   }
-
-  SECTION("Export/import one global") {
+  void singleValidGlobal() {
     auto b1 = QSharedPointer<symbol::Table>::create();
     auto b2 = b1->addChild();
     auto l1 = b1->addChild();
@@ -148,28 +145,27 @@ TEST_CASE("Validate functionality for multiple symbol tables.") {
     auto z = l3->reference("hello");
 
     l1->markGlobal("hello");
-    CHECK(x->binding == symbol::Binding::kGlobal);
-    CHECK(y->binding == symbol::Binding::kImported);
-    CHECK(z->binding == symbol::Binding::kImported);
-    CHECK(x->state == symbol::DefinitionState::kUndefined);
-    CHECK(y->state == symbol::DefinitionState::kUndefined);
-    CHECK(z->state == symbol::DefinitionState::kUndefined);
+    QCOMPARE(x->binding, symbol::Binding::kGlobal);
+    QCOMPARE(y->binding, symbol::Binding::kImported);
+    QCOMPARE(z->binding, symbol::Binding::kImported);
+    QCOMPARE(x->state, symbol::DefinitionState::kUndefined);
+    QCOMPARE(y->state, symbol::DefinitionState::kUndefined);
+    QCOMPARE(z->state, symbol::DefinitionState::kUndefined);
     // Check that defining a global symbol also defines its imports.
     l1->define("hello");
-    CHECK(x->state == symbol::DefinitionState::kSingle);
-    CHECK(y->state == symbol::DefinitionState::kSingle);
-    CHECK(z->state == symbol::DefinitionState::kSingle);
+    QCOMPARE(x->state, symbol::DefinitionState::kSingle);
+    QCOMPARE(y->state, symbol::DefinitionState::kSingle);
+    QCOMPARE(z->state, symbol::DefinitionState::kSingle);
     l2->define("hello");
-    CHECK(x->state == symbol::DefinitionState::kSingle);
-    CHECK(y->state == symbol::DefinitionState::kExternalMultiple);
-    CHECK(z->state == symbol::DefinitionState::kSingle);
+    QCOMPARE(x->state, symbol::DefinitionState::kSingle);
+    QCOMPARE(y->state, symbol::DefinitionState::kExternalMultiple);
+    QCOMPARE(z->state, symbol::DefinitionState::kSingle);
     l3->define("hello");
-    CHECK(x->state == symbol::DefinitionState::kSingle);
-    CHECK(y->state == symbol::DefinitionState::kExternalMultiple);
-    CHECK(z->state == symbol::DefinitionState::kExternalMultiple);
+    QCOMPARE(x->state, symbol::DefinitionState::kSingle);
+    QCOMPARE(y->state, symbol::DefinitionState::kExternalMultiple);
+    QCOMPARE(z->state, symbol::DefinitionState::kExternalMultiple);
   }
-
-  SECTION("Multiple global definitions") {
+  void multipleGlobal() {
     auto b1 = QSharedPointer<symbol::Table>::create();
     auto b2 = b1->addChild();
     auto l1 = b1->addChild();
@@ -178,95 +174,20 @@ TEST_CASE("Validate functionality for multiple symbol tables.") {
     auto x = l1->reference("hello");
     auto y = l2->reference("hello");
     l1->markGlobal("hello");
-    CHECK(x->binding == symbol::Binding::kGlobal);
-    CHECK(y->binding == symbol::Binding::kImported);
+    QCOMPARE(x->binding, symbol::Binding::kGlobal);
+    QCOMPARE(y->binding, symbol::Binding::kImported);
     l2->markGlobal("hello");
     //  Test that reused name set to global gets treated as global too
     auto z = l3->reference("hello");
-    CHECK(x->binding == symbol::Binding::kGlobal);
-    CHECK(y->binding == symbol::Binding::kGlobal);
-    CHECK(z->binding == symbol::Binding::kGlobal);
-    CHECK(x->state == symbol::DefinitionState::kExternalMultiple);
-    CHECK(y->state == symbol::DefinitionState::kExternalMultiple);
-    CHECK(z->state == symbol::DefinitionState::kExternalMultiple);
+    QCOMPARE(x->binding, symbol::Binding::kGlobal);
+    QCOMPARE(y->binding, symbol::Binding::kGlobal);
+    QCOMPARE(z->binding, symbol::Binding::kGlobal);
+    QCOMPARE(x->state, symbol::DefinitionState::kExternalMultiple);
+    QCOMPARE(y->state, symbol::DefinitionState::kExternalMultiple);
+    QCOMPARE(z->state, symbol::DefinitionState::kExternalMultiple);
   }
+};
 
-  /*
-   *  Test 2 Tree structures
-   *          Branch1     Branch2
-   *             |        |     |
-   *           Leaf1    Leaf2 Leaf3
-   */
-  SECTION("Symbol existence checks with 2 independent trees.") {
-    auto b1 = QSharedPointer<symbol::Table>::create();
-    auto b2 = QSharedPointer<symbol::Table>::create();
-    auto l1 = b1->addChild();
-    auto l2 = b2->addChild();
-    auto l3 = b2->addChild();
-    auto x = l1->reference("x");
-    auto y = l2->reference("y");
-    auto z = l3->reference("z");
+#include "n_table.test.moc"
 
-    // Each leaf should find it's own children regardless of policy.
-    CHECK(symbol::exists(l1, "x"));
-    CHECK(symbol::exists(l2, "y", symbol::TraversalPolicy::kSiblings));
-    CHECK(symbol::exists(l3, "z", symbol::TraversalPolicy::kWholeTree));
-
-    // Check that traversal policy is respected.
-    // Leaf 2 should not be able to see leaf 3's symbol with kChildren.
-    // Leaf 1 cannot see leaf 2 or 3
-    CHECK_FALSE(symbol::exists(l2, "z"));
-    CHECK_FALSE(symbol::exists(l3, "y"));
-    CHECK_FALSE(symbol::exists(l1, "z"));
-    CHECK_FALSE(symbol::exists(l1, "y"));
-
-    //  Lower leafs can see other siblings but not another tree with kSiblings
-    CHECK(symbol::exists(l2, "z", symbol::TraversalPolicy::kSiblings));
-    CHECK(symbol::exists(l3, "y", symbol::TraversalPolicy::kSiblings));
-    CHECK_FALSE(symbol::exists(l2, "x"));
-    CHECK_FALSE(symbol::exists(l3, "x"));
-
-    //  Lower leafs cannot see other trees even with kWholeTree search
-    CHECK_FALSE(symbol::exists(l2, "x", symbol::TraversalPolicy::kWholeTree));
-    CHECK_FALSE(symbol::exists(l3, "x", symbol::TraversalPolicy::kWholeTree));
-    CHECK_FALSE(symbol::exists(l1, "z", symbol::TraversalPolicy::kWholeTree));
-    CHECK_FALSE(symbol::exists(l1, "y", symbol::TraversalPolicy::kWholeTree));
-  }
-
-  SECTION("Multiple global definitions with 2 independent trees.") {
-    auto b1 = QSharedPointer<symbol::Table>::create();
-    auto b2 = QSharedPointer<symbol::Table>::create();
-    auto l1 = b1->addChild();
-    auto l2 = b2->addChild();
-    auto l3 = b2->addChild();
-    auto x = l1->reference("hello");
-    auto y = l2->reference("hello");
-
-    //  Marking branch leaf 1 as global does not impact branch 2 leaves
-    l1->markGlobal("hello");
-    CHECK(x->binding == symbol::Binding::kGlobal);
-    CHECK(y->binding == symbol::Binding::kLocal);
-    CHECK(x->state == symbol::DefinitionState::kUndefined);
-    CHECK(y->state == symbol::DefinitionState::kUndefined);
-
-    //  Marking second branch as global does not trigger external multiple error
-    //  in other branch
-    l2->markGlobal("hello");
-    CHECK(x->binding == symbol::Binding::kGlobal);
-    CHECK(y->binding == symbol::Binding::kGlobal);
-    CHECK(x->state == symbol::DefinitionState::kUndefined);
-    CHECK(y->state == symbol::DefinitionState::kUndefined);
-
-    //  Trigger global clash in branch 2
-    auto z = l3->reference("hello");
-    CHECK(z->binding == symbol::Binding::kImported);
-    CHECK(z->state == symbol::DefinitionState::kUndefined);
-
-    //  Create global error with second leaf value marked as global
-    l3->markGlobal("hello");
-    CHECK(z->binding == symbol::Binding::kGlobal);
-    CHECK(x->state == symbol::DefinitionState::kUndefined);
-    CHECK(y->state == symbol::DefinitionState::kExternalMultiple);
-    CHECK(z->state == symbol::DefinitionState::kExternalMultiple);
-  }
-}
+QTEST_MAIN(SymbolNTable);
