b46a00173af7bb88fcb26518ff41f173cd632216
Matthew McRaven <mkm302@georgetown.edu>
2023-02-16 17:15:51 -0800

    build: add additional logging to failed tests
    Needed to debug failing CI.

--- config/cmake/create_targets.cmake
+++ config/cmake/create_targets.cmake
@@ -86,5 +86,5 @@ macro(make_qtest target_name file dep)
   add_executable(${target_name} ${file})
   target_link_libraries(${target_name} PRIVATE ${dep})
   # And run the test with the correct reporting options.
-  add_test(NAME ${target_name} COMMAND ${target_name})
+  add_test(NAME ${target_name} COMMAND ${target_name} "--rerun-failed" "--output-on-failure")
 endMacro()
