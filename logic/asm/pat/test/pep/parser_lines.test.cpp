8b131239683510b95019e678d418ea88a5661dee
Matthew McRaven <mkm302@georgetown.edu>
2023-02-20 09:12:21 -0800

    test(asm.pas): fix unit tests broken by refactor

--- logic/asm/pat/test/pep/parser_lines.test.cpp
+++ logic/asm/pat/test/pep/parser_lines.test.cpp
@@ -1,19 +1,19 @@
 #include <QObject>
 #include <QTest>
 
-#include "pat/pep/parse/lines.hpp"
+#include "pat/pep/parse/rules_lines.hpp"
 using namespace std::literals::string_literals;
 class PatPepParserLines : public QObject {
   Q_OBJECT
 private slots:
   void testBlank() {
     auto value = "  \t"s;
-    pat::pep::ast::Blank result;
+    pat::pep::parse::BlankType result;
     QVERIFY(parse(value.begin(), value.end(), pat::pep::parse::blank, result));
   }
   void testComment() {
     auto value = ";magic"s;
-    pat::pep::ast::Comment result;
+    pat::pep::parse::CommentType result;
     QVERIFY(
         parse(value.begin(), value.end(), pat::pep::parse::comment, result));
   }
@@ -22,7 +22,7 @@ private slots:
     auto values = QList<std::string>{"hi"s, "symbol:hi", "symbol:hi;comment",
                                      "a;comment"};
     for (const auto &value : values) {
-      pat::pep::ast::Unary result;
+      pat::pep::parse::UnaryType result;
       QVERIFY2(
           parse(value.begin(), value.end(), pat::pep::parse::unary, result),
           value.data());
@@ -37,14 +37,14 @@ private slots:
         QList<std::string>{"hi 10"s, "symbol:hi 10", "symbol:hi 10;comme nt",
                            "a 10;comment", "hi 10,s"};
     for (const auto &value : values) {
-      pat::pep::ast::NonUnary result;
+      pat::pep::parse::NonUnaryType result;
       QVERIFY2(
           parse(value.begin(), value.end(), pat::pep::parse::nonunary, result),
           value.data());
       qWarning()
           << QString::fromStdString(result.comment)
           << QString::fromStdString(result.identifier)
-          << (boost::get<pat::pep::ast::DecimalLiteral>(result.arg).value)
+          << (boost::get<pat::pep::parse::DecimalLiteral>(result.arg).value)
           << QString::fromStdString(result.addr)
           << QString::fromStdString(result.symbol);
     }
@@ -53,7 +53,7 @@ private slots:
     auto values =
         QList<std::string>{"sym:.hi", "sym: .hi", ".hi 10", ".hi 10, 20"};
     for (const auto &value : values) {
-      pat::pep::ast::Directive result;
+      pat::pep::parse::DirectiveType result;
       QVERIFY2(
           parse(value.begin(), value.end(), pat::pep::parse::directive, result),
           value.data());
@@ -66,7 +66,7 @@ private slots:
     auto values =
         QList<std::string>{"sym:@hi", "sym: @hi", "@hi 10", "@hi 10, 20"};
     for (const auto &value : values) {
-      pat::pep::ast::Macro result;
+      pat::pep::parse::MacroType result;
       QVERIFY2(
           parse(value.begin(), value.end(), pat::pep::parse::macro, result),
           value.data());
