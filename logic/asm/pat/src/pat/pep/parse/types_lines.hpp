843313bba6f0c12e607978f938ed992543d379ac
Matthew McRaven <mkm302@georgetown.edu>
2023-02-20 09:07:16 -0800

    refactor(asm.pas): move all parse files to same directory
    I plan on using AST dir for other purposes.

---
+++ logic/asm/pat/src/pat/pep/parse/types_lines.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "./types_values.hpp"
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/home/x3.hpp>
+#include <climits>
+#include <string>
+namespace pat::pep::parse {
+struct BlankType {};
+
+struct CommentType {
+  std::string comment;
+};
+
+struct UnaryType {
+  std::string symbol, identifier, comment;
+};
+
+struct NonUnaryType {
+  std::string symbol, identifier;
+  parse::Value arg;
+  std::string addr, comment;
+};
+
+struct DirectiveType {
+  std::string symbol, identifier;
+  std::vector<parse::Value> args;
+  std::string comment;
+};
+
+struct MacroType {
+  std::string symbol, identifier;
+  std::vector<parse::Value> args;
+  std::string comment;
+};
+
+typedef boost::variant<BlankType, CommentType, UnaryType, NonUnaryType,
+                       DirectiveType, MacroType>
+    LineType;
+} // namespace pat::pep::parse
+
+BOOST_FUSION_ADAPT_STRUCT(pat::pep::parse::BlankType);
+BOOST_FUSION_ADAPT_STRUCT(pat::pep::parse::CommentType, comment);
+BOOST_FUSION_ADAPT_STRUCT(pat::pep::parse::UnaryType, symbol, identifier,
+                          comment);
+BOOST_FUSION_ADAPT_STRUCT(pat::pep::parse::NonUnaryType, symbol, identifier,
+                          arg, addr, comment);
+BOOST_FUSION_ADAPT_STRUCT(pat::pep::parse::DirectiveType, symbol, identifier,
+                          args, comment);
+BOOST_FUSION_ADAPT_STRUCT(pat::pep::parse::MacroType, symbol, identifier, args,
+                          comment);
