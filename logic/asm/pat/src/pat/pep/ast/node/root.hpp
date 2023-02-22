e1fc9431846dcf175a213627b7c2af8f83f4cf11
Matthew McRaven <mkm302@georgetown.edu>
2023-02-21 20:25:22 -0800

    feat(asm.pas): add pepp root AST node

--- logic/asm/pat/src/pat/pep/ast/node/root.hpp
+++ logic/asm/pat/src/pat/pep/ast/node/root.hpp
@@ -24,10 +24,35 @@ public:
   // Value interface
   QSharedPointer<SectionGroup> clone() const;
   quint64 size() const;
-  QString string() const;
 
 private:
   QSharedPointer<symbol::Table> _symbolTable = nullptr;
   QList<QSharedPointer<ChildNode<ISA>>> _children = {};
+};
+
+template <typename ISA> class Root {
+public:
+  explicit Root();
+  explicit Root(QSharedPointer<symbol::Table> symbolTable);
+  Root(const Root &other);
+  Root(Root &&other) noexcept;
+  Root &operator=(Root other);
+  friend void swap(Root &first, Root &second) {
+    using std::swap;
+    swap(first._symbolTable, second._symbolTable);
+    swap(first._children, second._children);
+  }
+
+  QSharedPointer<symbol::Table> symbolTable();
+  QSharedPointer<const symbol::Table> symbolTable() const;
+  void setSymbolTable(QSharedPointer<symbol::Table> symbolTable);
+  void addChild(QSharedPointer<ChildNode<ISA>> node);
+
+  // Value interface
+  QSharedPointer<Root> clone() const;
+
+private:
+  QSharedPointer<symbol::Table> _symbolTable = nullptr;
+  QList<QSharedPointer<SectionGroup<ISA>>> _children = {};
 };
 }; // namespace pat::pep::ast::node
