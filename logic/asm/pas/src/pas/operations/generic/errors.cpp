09b5f9816f3db2f9861d56c76e2f465191c02415
Matthew McRaven <mkm302@georgetown.edu>
2023-02-26 08:45:24 -0800

    feat(asm.pas): add visitor to collect all errors
    Closes #25.

---
+++ logic/asm/pas/src/pas/operations/generic/errors.cpp
@@ -0,0 +1,17 @@
+#include "./errors.hpp"
+#include "pas/ast/generic/attr_error.hpp"
+#include "pas/ast/generic/attr_location.hpp"
+#include "pas/ast/node.hpp"
+void pas::ops::generic::collectErrors::operator()(const ast::Node &node) {
+  using Location = ast::generic::SourceLocation;
+  using Message = ast::generic::Message;
+  using Error = ast::generic::Error;
+  if (node.has<Error>()) {
+    Location location;
+    if (node.has<Location>())
+      location = node.get<Location>();
+    for (auto error : node.get<Error>().value) {
+      errors.push_back(QPair<Location, Message>{{}, error});
+    }
+  }
+}
---
+++ logic/asm/pas/src/pas/operations/generic/errors.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#pragma once
+#include "pas/ast/op.hpp"
+#include <QtCore>
+namespace pas::ast {
+class Node;
+namespace generic {
+class Message;
+class SourceLocation;
+} // namespace generic
+} // namespace pas::ast
+
+namespace pas::ops::generic {
+struct collectErrors : public pas::ops::ConstOp<void> {
+  QList<QPair<ast::generic::SourceLocation, ast::generic::Message>> errors;
+  void operator()(const ast::Node &node);
+};
+} // namespace pas::ops::generic
