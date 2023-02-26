e8423fba557dba9956c032dcbc692e3bfcf4b2e1
Matthew McRaven <mkm302@georgetown.edu>
2023-02-25 21:06:33 -0800

    refactor(asm.pas): remove unused error member variable from visitor

--- logic/asm/pas/src/pas/operations/pepp/arg_from_parse_tree.hpp
+++ logic/asm/pas/src/pas/operations/pepp/arg_from_parse_tree.hpp
@@ -11,9 +11,8 @@ namespace pas::operations::pepp {
 using namespace pas::parse::pepp;
 struct ParseToArg
     : public boost::static_visitor<QSharedPointer<pas::ast::value::Base>> {
-  bool preferIdent = false;                    // in
-  QSharedPointer<symbol::Table> symTab;        // in
-  std::optional<QString> error = std::nullopt; // out
+  bool preferIdent = false;             // in
+  QSharedPointer<symbol::Table> symTab; // in
   QSharedPointer<pas::ast::value::Base> operator()(const CharacterLiteral &);
   QSharedPointer<pas::ast::value::Base> operator()(const StringLiteral &);
   QSharedPointer<pas::ast::value::Base> operator()(const Identifier &);
