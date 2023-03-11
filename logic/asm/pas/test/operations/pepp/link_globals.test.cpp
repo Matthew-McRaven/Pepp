bac1d6d210be9a1e8285b572b6eb9ce72cd8fe43
Matthew McRaven <mkm302@georgetown.edu>
2023-03-11 09:27:43 -0800

    test(asm.pas): add inter-tree link globals tests
    Test that undefined symbols become defined.
    Test that defined symbols become multiply defined.
    
    Closes #77.

--- logic/asm/pas/test/operations/pepp/link_globals.test.cpp
+++ logic/asm/pas/test/operations/pepp/link_globals.test.cpp
@@ -2,6 +2,7 @@
 #include "pas/driver/common.hpp"
 #include "pas/driver/pepp.hpp"
 #include "pas/isa/pep10.hpp"
+#include "symbol/value.hpp"
 #include <QObject>
 #include <QTest>
 
@@ -18,6 +19,60 @@ private slots:
     QVERIFY(globals->contains("s"));
     auto sym = globals->get("s");
     QCOMPARE(sym->binding, symbol::Binding::kGlobal);
+  }
+
+  void linkInterTree() {
+    QString body = "LDWA s,i";
+    auto globals = QSharedPointer<pas::driver::Globals>::create();
+    auto otherTable = QSharedPointer<symbol::Table>::create();
+    otherTable->define("s");
+    otherTable->markGlobal("s");
+    globals->add(*otherTable->get("s"));
+
+    // Verify that global symbol looks correct
+    QVERIFY(globals->contains("s"));
+    QCOMPARE(globals->get("s")->binding, symbol::Binding::kGlobal);
+
+    auto ret = pas::driver::pepp::createParser<Pep10ISA>(false)(body, nullptr);
+    QVERIFY(!ret.hadError);
+    pas::ops::generic::linkGlobals(*ret.root, globals, {u"EXPORT"_qs});
+
+    // Verify that local symbol is correct
+    QVERIFY(ret.root->has<pas::ast::generic::SymbolTable>());
+    auto thisTable = ret.root->get<pas::ast::generic::SymbolTable>().value;
+    QVERIFY(thisTable->exists("s"));
+    auto thisSym = *thisTable->get("s");
+    QCOMPARE(thisSym->binding, symbol::Binding::kImported);
+    QCOMPARE(thisSym->state, symbol::DefinitionState::kSingle);
+    auto casted =
+        dynamic_cast<symbol::value::ExternalPointer *>(&*thisSym->value);
+    QCOMPARE_NE(casted, nullptr);
+    QCOMPARE(casted->symbol_pointer, *otherTable->get("s"));
+    QCOMPARE(casted->symbol_table, otherTable);
+  }
+  void multiDefine() {
+    QString body = "s:LDWA s,i";
+    auto globals = QSharedPointer<pas::driver::Globals>::create();
+    auto otherTable = QSharedPointer<symbol::Table>::create();
+    otherTable->define("s");
+    otherTable->markGlobal("s");
+    globals->add(*otherTable->get("s"));
+
+    // Verify that global symbol looks correct
+    QVERIFY(globals->contains("s"));
+    QCOMPARE(globals->get("s")->binding, symbol::Binding::kGlobal);
+
+    auto ret = pas::driver::pepp::createParser<Pep10ISA>(false)(body, nullptr);
+    QVERIFY(!ret.hadError);
+    pas::ops::generic::linkGlobals(*ret.root, globals, {u"EXPORT"_qs});
+
+    // Verify that local symbol is correct
+    QVERIFY(ret.root->has<pas::ast::generic::SymbolTable>());
+    auto thisTable = ret.root->get<pas::ast::generic::SymbolTable>().value;
+    QVERIFY(thisTable->exists("s"));
+    auto thisSym = *thisTable->get("s");
+    QCOMPARE(thisSym->binding, symbol::Binding::kImported);
+    QCOMPARE(thisSym->state, symbol::DefinitionState::kExternalMultiple);
   }
 };
 
