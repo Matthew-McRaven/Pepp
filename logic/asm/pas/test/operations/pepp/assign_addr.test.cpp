48bf19a27b25dd05a00d257d10e8916954c5b6ea
Matthew McRaven <mkm302@georgetown.edu>
2023-03-12 21:32:17 -0700

    test(asm.pas): add test case skeletons for Pep/10 address assignment

--- logic/asm/pas/test/operations/pepp/assign_addr.test.cpp
+++ logic/asm/pas/test/operations/pepp/assign_addr.test.cpp
@@ -4,7 +4,16 @@
 class PasOpsPepp_AssignAddress : public QObject {
   Q_OBJECT
 private slots:
-  void smoke() {}
+  void unary() {}
+  void nonUnary() {}
+  void size0Directives() {}
+  void size0Directives_data() {}
+  void ascii() {}
+  void ascii_data() {}
+  void align() {}
+  void align_data() {}
+  void macro() {}
+  void empty() {}
 };
 
 #include "assign_addr.test.moc"
