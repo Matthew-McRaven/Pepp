d37eb585dfed6e516966faf4fd5f5d7eeee8c5dd
Matthew McRaven <mkm302@georgetown.edu>
2023-03-13 18:38:58 -0700

    feat(asm.pas): add visitor to register system call macros
    Progress on #14.

---
+++ logic/asm/pas/src/pas/operations/pepp/register_system_calls.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "macro/registry.hpp"
+#include "pas/ast/node.hpp"
+#include "pas/ast/op.hpp"
+
+namespace pas::ops::pepp {
+// Mus satisfy pas::ops::generic::isDirective
+struct RegisterSystemCalls : public pas::ops::MutatingOp<bool> {
+  QSharedPointer<macro::Registry> registry;
+  bool addedError = false;
+  bool operator()(pas::ast::Node &node) override;
+};
+bool registerSystemCalls(pas::ast::Node &node,
+                         QSharedPointer<macro::Registry> registry);
+} // namespace pas::ops::pepp
