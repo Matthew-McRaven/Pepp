3e8a4a5a3c40664ce1f5d4c40de9978092c9d87b
Matthew McRaven <mkm302@georgetown.edu>
2023-03-13 18:03:15 -0700

    docs(asm.pas): add notes on pas::bits::copy

--- logic/asm/pas/src/pas/bits/operations.hpp
+++ logic/asm/pas/src/pas/bits/operations.hpp
@@ -2,6 +2,9 @@
 #include "./order.hpp"
 #include <QtCore>
 namespace pas::bits {
+
+// When src is longer than dest, truncates high-order bytes (like casting
+// u16->u8). When dest is longer than src, dest is 0-padded.
 bool copy(const quint8 *src, BitOrder srcOrder, quint16 srcLength, quint8 *dest,
           BitOrder destOrder, quint16 destLength);
-}
+} // namespace pas::bits
