49e262eb398b95ee2f28b599c19a0925886c8453
Matthew McRaven <matthew.mcraven@gmail.com>
2023-03-23 08:54:42 -0700

    fix(asm.pas): allow argument size overrides
    Needed to allow BYTE's argument to take only 1 byte.

--- logic/asm/pas/src/pas/parse/pepp/arg_from_parse_tree.hpp
+++ logic/asm/pas/src/pas/parse/pepp/arg_from_parse_tree.hpp
@@ -12,6 +12,7 @@ struct ParseToArg
     : public boost::static_visitor<QSharedPointer<pas::ast::value::Base>> {
   bool preferIdent = false;             // in
   QSharedPointer<symbol::Table> symTab; // in
+  std::optional<quint8> sizeOverride = std::nullopt;
   QSharedPointer<pas::ast::value::Base> operator()(const CharacterLiteral &);
   QSharedPointer<pas::ast::value::Base> operator()(const StringLiteral &);
   QSharedPointer<pas::ast::value::Base> operator()(const Identifier &);
