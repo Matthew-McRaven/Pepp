cfec85c187caa1d8f1ee8a82d45642573e029bd8
Matthew McRaven <mkm302@georgetown.edu>
2023-04-09 17:02:09 -0700

    test(asm.pas): add unified OS+user program pipeline tests for CS6E
    Closes #144.

--- logic/asm/pas/test/e2e/pep10.test.cpp
+++ logic/asm/pas/test/e2e/pep10.test.cpp
@@ -78,6 +78,75 @@ private slots:
           << fig->typesafeElements()["pep"]->contents << fig->isOS();
     }
   }
+
+  void unified() {
+    QFETCH(QString, userBody);
+    QFETCH(QString, osBody);
+
+    // Load macros on each iteration to prevent macros from migrating between
+    // tests.
+    auto registry = QSharedPointer<macro::Registry>::create();
+    loadBookMacros(registry);
+
+    auto pipeline = pas::driver::pep10::pipeline(
+        {{osBody, {.isOS = true, .ignoreUndefinedSymbols = false}},
+         {userBody, {.isOS = false, .ignoreUndefinedSymbols = false}}},
+        registry);
+    auto result = pipeline->assemble(pas::driver::pep10::Stage::End);
+
+    // for (auto &x : pipeline->globals->table.keys())
+    //   qCritical() << "SYMBOL: " << x;
+    QCOMPARE(pipeline->pipelines.size(), 2);
+
+    auto osTarget = pipeline->pipelines[0].first;
+    QVERIFY(osTarget->bodies.contains(pas::driver::repr::Nodes::name));
+    auto osRoot = osTarget->bodies[pas::driver::repr::Nodes::name]
+                      .value<pas::driver::repr::Nodes>()
+                      .value;
+
+    auto userTarget = pipeline->pipelines[1].first;
+    QVERIFY(userTarget->bodies.contains(pas::driver::repr::Nodes::name));
+    auto userRoot = userTarget->bodies[pas::driver::repr::Nodes::name]
+                        .value<pas::driver::repr::Nodes>()
+                        .value;
+    // print out error messages before failing -- enables debugging broken
+    // tests.
+    if (!result) {
+      auto lines = pas::ops::pepp::formatListing<pas::isa::Pep10ISA>(*userRoot);
+      for (auto &line : lines)
+        qCritical() << line;
+      for (auto &error : pas::ops::generic::collectErrors(*osRoot))
+        qCritical() << "OS:   " << error.second.message;
+      for (auto &error : pas::ops::generic::collectErrors(*userRoot))
+        qCritical() << "USER: " << error.second.message;
+    }
+    QVERIFY(result);
+  }
+
+  void unified_data() {
+    QTest::addColumn<QString>("userBody");
+    QTest::addColumn<QString>("osBody");
+    auto registry = builtins::Registry(nullptr);
+    this->book = registry.findBook("Computer Systems, 6th Edition");
+    QVERIFY(!book.isNull());
+
+    for (auto &fig : book->figures()) {
+      auto defaultOS = fig->defaultOS();
+      if (fig->isOS())
+        continue;
+      else if (!fig->typesafeElements().contains("pep"))
+        continue;
+      else if (defaultOS == nullptr)
+        continue;
+      else if (!defaultOS->typesafeElements().contains("pep"))
+        continue;
+      auto chName = fig->chapterName().toStdString();
+      auto figName = fig->figureName().toStdString();
+      QTest::addRow("Figure %s.%s with OS", chName.data(), figName.data())
+          << fig->typesafeElements()["pep"]->contents
+          << defaultOS->typesafeElements()["pep"]->contents;
+    }
+  }
 };
 
 #include "pep10.test.moc"
