85ab8f5bef43c868f27d90ad910d3b3a46b6ced7
Matthew McRaven <mkm302@georgetown.edu>
2023-04-11 19:28:46 -0700

    feat(asm.pas): add helper to determine if a node is addressable
    Needed for pushing down a macro symbol.
    Also needed to determine if ORG needs to split a section.

---
+++ logic/asm/pas/src/pas/operations/pepp/addressable.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "pas/ast/generic/attr_directive.hpp"
+#include "pas/ast/node.hpp"
+#include "pas/operations/generic/is.hpp"
+#include "pas/operations/pepp/is.hpp"
+
+namespace pas::ops::pepp {
+template <typename ISA> bool isAddressable(ast::Node &node);
+}
+
+template <typename ISA> bool pas::ops::pepp::isAddressable(ast::Node &node) {
+  static const auto addressableDirectives =
+      QSet<QString>{"ALIGN", "ASCII", "BLOCK", "BYTE", "WORD"};
+  if (generic::isDirective()(node) &&
+      addressableDirectives.contains(
+          node.get<ast::generic::Directive>().value.toUpper()))
+    return true;
+  return isUnary<ISA>()(node) || isNonUnary<ISA>()(node);
+}
