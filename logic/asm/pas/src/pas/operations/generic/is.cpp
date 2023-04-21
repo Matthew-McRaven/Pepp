794204f2f5b450ec3f65e0b81aa59b5b2b10175a
Matthew McRaven <mkm302@georgetown.edu>
2023-04-21 07:42:32 -0700

    fix(asm.pas): fix broken AST property access in isAlign

--- logic/asm/pas/src/pas/operations/generic/is.cpp
+++ logic/asm/pas/src/pas/operations/generic/is.cpp
@@ -19,8 +19,6 @@ bool pas::ops::generic::isComment::operator()(const ast::Node &node) {
 }
 
 bool pas::ops::generic::isAlign::operator()(const ast::Node &node) {
-  (void)(void *)(&node);
-  auto n = node.get<ast::generic::Directive>().value;
   return node.get<ast::generic::Type>().value ==
              ast::generic::Type::Directive &&
          node.has<ast::generic::Directive>() &&
