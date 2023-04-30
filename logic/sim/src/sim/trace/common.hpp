cece22bbabfd9cd8e05d82fc37be17a61a0e1114
Matthew McRaven <mkm302@georgetown.edu>
2023-04-30 12:53:38 -0700

    feat(sim): add some common trace payload types

---
+++ logic/sim/src/sim/trace/common.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "bits/operations/log2.hpp"
+#include "sim/api.hpp"
+namespace bits::trace {
+template <typename Data>
+concept Pow2 = std::has_single_bit(sizeof(Data));
+template <typename Address, Pow2 Data> struct AddressedPayload {
+  Address address;
+  Data data;
+};
+
+template <typename Address, Pow2 Data> sim::api::Packet::Flags flags() {
+  // Only have 3 bits to store pow^2, so we can only stor up to 64.
+  static_assert(sizeof(Data) <= 64);
+  sim::api::Packet::Flags flags;
+  flags.scope = 0;
+  flags.dyn = 0;
+  flags.kind = ((bits::ceil_log2((sizeof(Address)) + 1) & 0b111) << 3) |
+               ((bits::ceil_log2(sizeof(Data)) + 1) & 0b111);
+  return flags;
+}
+
+inline quint8 payload_size(sim::api::Packet::Flags flags) {
+  quint8 addrBits = (flags.kind >> 3) & 0b111;
+  quint8 dataBits = flags.kind & 0b111;
+  return (1 << (addrBits - 1)) + (1 << (dataBits - 1));
+};
+} // namespace bits::trace
