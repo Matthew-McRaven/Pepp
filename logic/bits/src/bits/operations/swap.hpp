08390ae22207df782353e7bb9d212ac7e3f09023
Matthew McRaven <mkm302@georgetown.edu>
2023-05-03 20:40:20 -0700

    refactor(bits): add byteswap to bits::* unconditionally

--- logic/bits/src/bits/operations/swap.hpp
+++ logic/bits/src/bits/operations/swap.hpp
@@ -25,21 +25,19 @@ bit_cast(const From &src) noexcept {
   return dst;
 }
 } // namespace detail
+
+#if __cpp_lib_bit_cast == 201806L
+using ::std::bit_cast;
+#else
+using detail::bit_cast;
+#endif
 // Sample code from: https://en.cppreference.com/w/cpp/numeric/byteswap
 // Waiting on my compiler to support byteswap...
 template <std::integral T> constexpr T byteswap(T value) noexcept {
   static_assert(std::has_unique_object_representations_v<T>,
                 "T may not have padding bits");
-#if __cpp_lib_bit_cast == 201806L
-  auto value_representation =
-      std::bit_cast<std::array<std::byte, sizeof(T)>>(value);
+  auto value_representation = bit_cast<std::array<std::byte, sizeof(T)>>(value);
   std::ranges::reverse(value_representation);
-  return std::bit_cast<T>(value_representation);
-#else
-  auto value_representation =
-      detail::bit_cast<std::array<std::byte, sizeof(T)>>(value);
-  std::ranges::reverse(value_representation);
-  return detail::bit_cast<T>(value_representation);
-#endif
+  return bit_cast<T>(value_representation);
 }
 } // namespace bits
