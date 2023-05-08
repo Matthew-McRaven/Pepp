89fb95f149cdb54df271241118d1d3cfa696314c
Matthew McRaven <mkm302@georgetown.edu>
2023-05-08 07:30:16 -0700

    chore: remove unused JS macro library

--- logic/asm/macro/src/index.ts
+++
@@ -1,7 +0,0 @@
-import parseMacroDeclaration from './parser';
-import Registry from './registry';
-
-export { MacroType } from './macro';
-export type { RegisteredMacro, ParsedMacro } from './macro';
-export { parseMacroDeclaration };
-export { Registry };
