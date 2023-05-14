24242bec98b7ca14e0d58cd02d37b329a10f52dd
Matthew McRaven <mkm302@georgetown.edu>
2023-05-14 13:56:37 -0700

    chore(bin): apply autopep8 to python code
    Closes #251.

--- bin/term/test/asmrun.py
+++ bin/term/test/asmrun.py
@@ -5,91 +5,143 @@ import tempfile
 import sys
 import os
 executable = ""
+
+
 class TestCase(unittest.TestCase):
     def test_baremetal(self):
         with tempfile.TemporaryDirectory() as cwd:
-                ret = subprocess.run([executable, "get", "--ch", "os", "--fig", "bm"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                try:
-                  with open(f"{cwd}/os.pep", "wb") as f:
-                          f.write(ret.stdout)
-                  with open(f"{cwd}/in.pep", "wb") as f:
-                          f.write(b"USCALL")
-                except IOError:
-                  self.fail("Writing contents should not fail")
+            ret = subprocess.run(
+                [executable, "get", "--ch", "os", "--fig", "bm"], cwd=cwd, capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            try:
+                with open(f"{cwd}/os.pep", "wb") as f:
+                    f.write(ret.stdout)
+                with open(f"{cwd}/in.pep", "wb") as f:
+                    f.write(b"USCALL")
+            except IOError:
+                self.fail("Writing contents should not fail")
 
-                ret = subprocess.run([executable, "asm", "-s", "in.pep", "--os", "os.pep",
-                "-o", "out.pepo", "--elf", "in.elf"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
+            ret = subprocess.run([executable,
+                                  "asm",
+                                  "-s",
+                                  "in.pep",
+                                  "--os",
+                                  "os.pep",
+                                  "-o",
+                                  "out.pepo",
+                                  "--elf",
+                                  "in.elf"],
+                                 cwd=cwd,
+                                 capture_output=True)
+            self.assertEqual(ret.returncode, 0)
 
-                ret = subprocess.run([executable, "run", "-s", "in.elf"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                self.assertEqual(ret.stdout, b"Cannot use system calls in bare metal mode\n")
+            ret = subprocess.run(
+                [executable, "run", "-s", "in.elf"], cwd=cwd, capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            self.assertEqual(
+                ret.stdout,
+                b"Cannot use system calls in bare metal mode\n")
 
     def test_0527_flags(self):
         with tempfile.TemporaryDirectory() as cwd:
-                ret = subprocess.run([executable, "get", "--ch", "05", "--fig", "27"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                self.assertNotEqual(len(ret.stdout), 0)
+            ret = subprocess.run(
+                [executable, "get", "--ch", "05", "--fig", "27"], cwd=cwd, capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            self.assertNotEqual(len(ret.stdout), 0)
 
-                try:
-                  with open(f"{cwd}/in.pep", "wb") as f:
+            try:
+                with open(f"{cwd}/in.pep", "wb") as f:
                     f.write(ret.stdout)
-                  with open(f"{cwd}/in.txt", "wt") as f:
+                with open(f"{cwd}/in.txt", "wt") as f:
                     f.write("10\n")
                     f.write("20\n")
-                except IOError:
-                  self.fail("Writing contents should not fail")
+            except IOError:
+                self.fail("Writing contents should not fail")
 
-                ret = subprocess.run([executable, "asm", "-s", "in.pep", "-o", "out.pepo", "--elf", "in.elf"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
+            ret = subprocess.run([executable,
+                                  "asm",
+                                  "-s",
+                                  "in.pep",
+                                  "-o",
+                                  "out.pepo",
+                                  "--elf",
+                                  "in.elf"],
+                                 cwd=cwd,
+                                 capture_output=True)
+            self.assertEqual(ret.returncode, 0)
 
-
-                ret = subprocess.run([executable, "run", "-s", "in.elf", "-i", "in.txt", "-o", "-"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                self.assertEqual(ret.stdout, b"score = 25\n\n")
+            ret = subprocess.run([executable,
+                                  "run",
+                                  "-s",
+                                  "in.elf",
+                                  "-i",
+                                  "in.txt",
+                                  "-o",
+                                  "-"],
+                                 cwd=cwd,
+                                 capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            self.assertEqual(ret.stdout, b"score = 25\n\n")
 
     def test_0527_positionals(self):
         with tempfile.TemporaryDirectory() as cwd:
-                ret = subprocess.run([executable, "get", "--ch", "05", "--fig", "27"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                self.assertNotEqual(len(ret.stdout), 0)
-                try:
-                  with open(f"{cwd}/in.pep", "wb") as f:
-                        f.write(ret.stdout)
-                  with open(f"{cwd}/in.txt", "wt") as f:
+            ret = subprocess.run(
+                [executable, "get", "--ch", "05", "--fig", "27"], cwd=cwd, capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            self.assertNotEqual(len(ret.stdout), 0)
+            try:
+                with open(f"{cwd}/in.pep", "wb") as f:
+                    f.write(ret.stdout)
+                with open(f"{cwd}/in.txt", "wt") as f:
                     f.write("10\n")
                     f.write("20\n")
-                except IOError:
-                  self.fail("Writing contents should not fail")
+            except IOError:
+                self.fail("Writing contents should not fail")
 
-                ret = subprocess.run([executable, "asm", "in.pep", "--elf", "in.elf"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
+            ret = subprocess.run(
+                [executable, "asm", "in.pep", "--elf", "in.elf"], cwd=cwd, capture_output=True)
+            self.assertEqual(ret.returncode, 0)
 
-                ret = subprocess.run([executable, "run", "in.elf", "-i", "in.txt", "-o", "-"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                self.assertEqual(ret.stdout, b"score = 25\n\n")
+            ret = subprocess.run([executable,
+                                  "run",
+                                  "in.elf",
+                                  "-i",
+                                  "in.txt",
+                                  "-o",
+                                  "-"],
+                                 cwd=cwd,
+                                 capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            self.assertEqual(ret.stdout, b"score = 25\n\n")
 
     def test_macro_dir_1(self):
         with tempfile.TemporaryDirectory() as cwd:
-              try:
+            try:
                 os.makedirs(f"{cwd}/macros")
                 with open(f"{cwd}/macros/mac.pepm", "wt") as f:
                     f.write("@MAC 0\nLDWA 0,i")
                 with open(f"{cwd}/in.pep", "wt") as f:
                     f.write("@MAC\n")
-                ret = subprocess.run([executable, "asm", "in.pep", "--macro-dir", "macros", "-e", "in.err.txt"], cwd=cwd, capture_output=True)
+                ret = subprocess.run([executable,
+                                      "asm",
+                                      "in.pep",
+                                      "--macro-dir",
+                                      "macros",
+                                      "-e",
+                                      "in.err.txt"],
+                                     cwd=cwd,
+                                     capture_output=True)
 
                 self.assertEqual(ret.returncode, 0)
                 with open(f"{cwd}/in.pepo", "rt") as f:
                     contents = "\n".join(f.readlines()).strip()
                     self.assertEqual(contents, "40 00 00 zz")
-              except IOError:
+            except IOError:
                 self.fail("Writing contents should not fail")
 
     def test_macro_dir_2(self):
         with tempfile.TemporaryDirectory() as cwd:
-              try:
+            try:
                 os.makedirs(f"{cwd}/macros1")
                 os.makedirs(f"{cwd}/macros2")
                 with open(f"{cwd}/macros1/mac.pepm", "wt") as f:
@@ -99,36 +151,63 @@ class TestCase(unittest.TestCase):
 
                 with open(f"{cwd}/in.pep", "wt") as f:
                     f.write("@MAC\n@CAM")
-                ret = subprocess.run([executable, "asm", "in.pep", "--macro-dir", "macros1", "--macro-dir", "macros2", "-e", "in.err.txt"], cwd=cwd, capture_output=True)
+                ret = subprocess.run([executable,
+                                      "asm",
+                                      "in.pep",
+                                      "--macro-dir",
+                                      "macros1",
+                                      "--macro-dir",
+                                      "macros2",
+                                      "-e",
+                                      "in.err.txt"],
+                                     cwd=cwd,
+                                     capture_output=True)
 
                 self.assertEqual(ret.returncode, 0)
                 with open(f"{cwd}/in.pepo", "rt") as f:
                     contents = "\n".join(f.readlines()).strip()
                     self.assertEqual(contents, "40 00 00 61 00 00 zz")
-              except IOError:
+            except IOError:
                 self.fail("Writing contents should not fail")
 
     def test_reg_override(self):
         with tempfile.TemporaryDirectory() as cwd:
-                try:
-                  with open(f"{cwd}/in.pep", "wt") as f:
+            try:
+                with open(f"{cwd}/in.pep", "wt") as f:
                     f.write("STBA charOut,d\nLDBA 1,i\nSTBA pwrOff,d")
-                except IOError:
-                  self.fail("Writing contents should not fail")
-                ret = subprocess.run([executable, "asm", "in.pep", "--elf", "in.elf"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
+            except IOError:
+                self.fail("Writing contents should not fail")
+            ret = subprocess.run(
+                [executable, "asm", "in.pep", "--elf", "in.elf"], cwd=cwd, capture_output=True)
+            self.assertEqual(ret.returncode, 0)
 
-                # Skip OS routines, start in user program with pre-loaded accumulator value.
-                ret = subprocess.run([executable, "run", "-s", "in.elf", "--skip-load", "--skip-dispatch", "--reg", "Pc", "0x0000", "--reg", "a", "67", "-o", "-"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                self.assertEqual(ret.stdout, b"C\n")
+            # Skip OS routines, start in user program with pre-loaded
+            # accumulator value.
+            ret = subprocess.run([executable,
+                                  "run",
+                                  "-s",
+                                  "in.elf",
+                                  "--skip-load",
+                                  "--skip-dispatch",
+                                  "--reg",
+                                  "Pc",
+                                  "0x0000",
+                                  "--reg",
+                                  "a",
+                                  "67",
+                                  "-o",
+                                  "-"],
+                                 cwd=cwd,
+                                 capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            self.assertEqual(ret.stdout, b"C\n")
 
 
 if __name__ == "__main__":
-        parser = argparse.ArgumentParser(prog="pepp-term-test")
-        parser.add_argument("executable")
-        args = parser.parse_args()
-        executable = args.executable
-        suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestCase)
-        result = unittest.TextTestRunner(verbosity=2).run(suite)
-        sys.exit(len(result.failures))
+    parser = argparse.ArgumentParser(prog="pepp-term-test")
+    parser.add_argument("executable")
+    args = parser.parse_args()
+    executable = args.executable
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestCase)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(len(result.failures))
--- bin/term/test/get.py
+++ bin/term/test/get.py
@@ -4,29 +4,46 @@ import subprocess
 import tempfile
 import sys
 executable = ""
+
+
 class TestCase(unittest.TestCase):
     def test_get_pep(self):
         with tempfile.TemporaryDirectory() as cwd:
-                ret = subprocess.run([executable, "get", "--ch", "05", "--fig", "27"], cwd=cwd, capture_output=True)
-                self.assertEqual(ret.returncode, 0)
-                self.assertNotEqual(len(ret.stdout), 0)
+            ret = subprocess.run(
+                [executable, "get", "--ch", "05", "--fig", "27"], cwd=cwd, capture_output=True)
+            self.assertEqual(ret.returncode, 0)
+            self.assertNotEqual(len(ret.stdout), 0)
 
     def test_get_not_exist(self):
         with tempfile.TemporaryDirectory() as cwd:
-                ret = subprocess.run([executable, "get", "--ch", "00", "--fig", "x0"], cwd=cwd, capture_output=True)
-                self.assertNotEqual(ret.returncode, 0)
-                self.assertEqual(ret.stderr, b"Figure 00.x0 does not exist.\n")
+            ret = subprocess.run(
+                [executable, "get", "--ch", "00", "--fig", "x0"], cwd=cwd, capture_output=True)
+            self.assertNotEqual(ret.returncode, 0)
+            self.assertEqual(ret.stderr, b"Figure 00.x0 does not exist.\n")
 
     def test_get_no_var(self):
         with tempfile.TemporaryDirectory() as cwd:
-                ret = subprocess.run([executable, "get", "--ch", "05", "--fig", "27", "--type", "not"], cwd=cwd, capture_output=True)
-                self.assertNotEqual(ret.returncode, 0)
-                self.assertEqual(ret.stderr, b"Figure 05.27 does not contain a \"not\" variant.\n")
+            ret = subprocess.run([executable,
+                                  "get",
+                                  "--ch",
+                                  "05",
+                                  "--fig",
+                                  "27",
+                                  "--type",
+                                  "not"],
+                                 cwd=cwd,
+                                 capture_output=True)
+            self.assertNotEqual(ret.returncode, 0)
+            self.assertEqual(
+                ret.stderr,
+                b"Figure 05.27 does not contain a \"not\" variant.\n")
+
+
 if __name__ == "__main__":
-        parser = argparse.ArgumentParser(prog="pepp-term-test")
-        parser.add_argument("executable")
-        args = parser.parse_args()
-        executable = args.executable
-        suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestCase)
-        result = unittest.TextTestRunner(verbosity=2).run(suite)
-        sys.exit(len(result.failures))
+    parser = argparse.ArgumentParser(prog="pepp-term-test")
+    parser.add_argument("executable")
+    args = parser.parse_args()
+    executable = args.executable
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestCase)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(len(result.failures))
