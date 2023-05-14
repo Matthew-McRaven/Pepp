14aca88a7ff1aecb5173d4fb6ff46b5f4228ac94
Matthew McRaven <mkm302@georgetown.edu>
2023-05-13 18:53:00 -0700

    test(bin): add basic end-to-end tests for pepp-term
    Uses both positional and flag syntax.
    
    Closes #231.

---
+++ bin/term/test/ls.py
@@ -0,0 +1,23 @@
+import argparse
+import unittest
+import subprocess
+import tempfile
+import sys
+executable = ""
+class TestCase(unittest.TestCase):
+    def test_ls(self):
+        with tempfile.TemporaryDirectory() as cwd:
+                ret = subprocess.run([executable, "ls"], cwd=cwd, capture_output=True)
+                self.assertEqual(ret.returncode, 0)
+                self.assertNotEqual(len(ret.stdout), 0)
+
+if __name__ == "__main__":
+        parser = argparse.ArgumentParser(prog="pepp-term-test")
+        parser.add_argument("executable")
+        args = parser.parse_args()
+        executable = args.executable
+        suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestCase)
+        result = unittest.TextTestRunner(verbosity=2).run(suite)
+        sys.exit(len(result.failures))
+
+
