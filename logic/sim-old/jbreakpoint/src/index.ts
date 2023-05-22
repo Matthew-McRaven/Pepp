cd94b3bc18d32fdbb1122fc6cea1c7e9faca5b64
Matthew McRaven <mkm302@georgetown.edu>
2023-05-21 22:25:38 -0700

    refactor: remove previous breakpoint interposer JS implementation

--- logic/sim-old/jbreakpoint/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { NonNativeAddressBreakpointInterposer as JBreakpointInterposer } from './address';
-export { NonNativeValueBreakpointInterposer as JConditionalBreakpointInterposer } from './value';
-export const NBreakpointInterposer = {};
--- logic/sim-old/jbreakpoint/test/dummy.test.ts
+++
@@ -1,7 +0,0 @@
-import { JBreakpointInterposer } from '../src';
-
-describe('placeholder', () => {
-  it('does nothing', () => undefined);
-  // eslint-disable-next-line no-void
-  it('does not crash from lack of coverage', () => { void new JBreakpointInterposer(); });
-});
