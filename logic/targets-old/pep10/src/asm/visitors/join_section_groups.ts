1d9490d9550d5fa1b8ce2452370b1e66b061c009
Matthew McRaven <mkm302@georgetown.edu>
2023-05-21 22:16:03 -0700

    refactor: remove previous Pep/10 JS implementation

--- logic/targets-old/pep10/src/asm/visitors/join_section_groups.ts
+++
@@ -1,9 +0,0 @@
-import { TypedNode, SectionGroup } from '../ast/nodes';
-
-export 
-export const joinSectionGroups = (tree: TypedNode) => {
-    const groups = new Map<string, SectionGroup>();
-  tree.walk((node:TypedNode) => {
-    if()
-  });
-};
