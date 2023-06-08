fa3a330a27833bf884a62aab1d48047df324862c
Matthew McRaven <mkm302@georgetown.edu>
2023-06-07 21:10:50 -0700

    feat(help.about): add Pepp `about` information
    Needed to access "sanely" for #339.

---
+++ logic/help/about/src/about/pepp.cpp
@@ -0,0 +1,52 @@
+#include "pepp.hpp"
+#include "read.hpp"
+
+QString about::projectRepoURL() {
+  auto text = about::detail::readFile(":/about/repo.url");
+  if (!text.has_value()) {
+    qWarning() << "Unable to read repo URL.\n";
+    return {};
+  }
+  return *text;
+}
+
+QList<about::Maintainer> about::maintainers() {
+  QList<Maintainer> ret;
+  auto text = detail::readFile(":/about/maintainers.csv");
+  // maintainers.csv has no headers
+  for (const auto &line : text->split("\n")) {
+    // Skip empty lines.
+    if (line.isEmpty())
+      continue;
+    auto parts = line.split(",");
+    if (parts.size() != 2) {
+      qWarning() << "Failed to parse maintainers row: " << line << "\n";
+      return {};
+    }
+    ret.push_back(about::Maintainer{.name = parts[0], .email = parts[1]});
+  }
+  return ret;
+}
+
+QList<QString> about::contributors() {
+  QList<QString> ret;
+  auto text = detail::readFile(":/about/contributors.csv");
+  // maintainers.csv has no headers
+  for (const auto &line : text->split("\n")) {
+    // Skip empty lines.
+    if (line.isEmpty())
+      continue;
+    ret.push_back(line);
+  }
+  return ret;
+}
+
+QString about::licenseFull() {
+  auto text = detail::readFile(":/about/LICENSE_FULL");
+  return text.value_or("");
+}
+
+QString about::licenseNotice() {
+  auto text = detail::readFile(":/about/LICENSE_NOTICE");
+  return text.value_or("");
+}
---
+++ logic/help/about/src/about/pepp.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <QtCore>
+
+namespace about {
+QString projectRepoURL();
+struct Maintainer {
+  QString name, email;
+};
+QList<Maintainer> maintainers();
+QList<QString> contributors();
+QString licenseFull();
+QString licenseNotice();
+} // namespace about
