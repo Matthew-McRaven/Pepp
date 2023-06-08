e5706eb61f6cd2258cc0c13d36458fe4a57cd5c3
Matthew McRaven <mkm302@georgetown.edu>
2023-06-07 21:24:29 -0700

    feat(bin): add basic `about` info to terminal
    Still missing licensing info.
    
    Closes #339.

--- bin/term/src/about.cpp
+++ bin/term/src/about.cpp
@@ -1,10 +1,21 @@
 #include "./about.hpp"
+#include "about/dependencies.hpp"
+#include "about/pepp.hpp"
 #include "about/version.hpp"
 #include <iostream>
 
 AboutTask::AboutTask(QObject *parent) : Task(parent) {}
 
 void AboutTask::run() {
-  std::cout << "placeholder\n";
+  std::cout << u"Pepp Terminal, Version %1\nBased on commit %2\n\n"_qs
+                   .arg(about::versionString())
+                   .arg(about::g_GIT_SHA1)
+                   .toStdString();
+  std::cout << "Report issues or check for updates:\n";
+  std::cout << "\t" << about::projectRepoURL().toStdString() << "\n\n";
+  std::cout << "Authors:\n";
+  for (const auto &maintainer : about::maintainers())
+    std::cout << "\t" << maintainer.name.toStdString() << " <"
+              << maintainer.email.toStdString() << ">\n";
   return emit finished(0);
 }
