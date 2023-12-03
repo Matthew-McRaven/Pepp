beac2afd1af50180c13ca05a3dee8efd54f58e98
Matthew McRaven <matthew.mcraven@gmail.com>
2023-12-03 11:49:24 -0800

    Refactor dictionary access
    I should be able to use these refactored methods in both python and
    FORTH.

--- p4.py
+++ p4.py
@@ -30,96 +30,110 @@ def read_u16(VM, address):
 	return int.from_bytes(VM.memory[address:address+2],"big")
 def read_i16(VM, address):
 	return int.from_bytes(VM.memory[address:address+2],"big", signed=True)
-
-class DictFlags(enum.IntEnum):
+	
+class DictAccessor:
 	IMMEDIATE = 0x80
 	HIDDEN = 0x20
 	LEN = 0x1f
-		
-def addDictEntry(VM, name, tokens, immediate=False, hidden=False):
-	newTail = VM.here
-	# u16 -- link
-	write_u16(VM, VM.here, VM.dictTail); VM.here+=2
-	VM.dictTail = newTail
-	# u8 -- flags
-	write_u8(VM, VM.here, (DictFlags.IMMEDIATE if immediate else 0)
-		| (DictFlags.HIDDEN if hidden else 0)
-		| len(tokens) & DictFlags.LEN); VM.here += 1
-	# ?? I would like to encode length of block so I can dump code!!
-	# u8 -- length of name.
-	write_u8(VM, VM.here, len(name)); VM.here += 1
-	# n*u8 string + 1/3? u8 of null
-	# old = VM.here
-	for letter in bytearray(name, "utf-8"):
-		write_u8(VM, VM.here, letter); VM.here += 1
-	#print(f"Defined {(10*' '+name)[-10:]}, from {(2*'0'+hex(old)[2:])[-2:]:2}..{(2*'0'+hex(VM.here)[2:])[-2:]:2}; strlen {len(name):2}, memlen is {VM.here-old:2}")
-	# Always null terminate strings, and place words on a 16b boundary
-	# So, pad evens with 2*null, odds with 1.
-	if VM.here % 2 == 0:
-		write_u8(VM, VM.here, 0); VM.here += 1
-	write_u8(VM, VM.here, 0); VM.here += 1
-	# n*u16code list
-	cwa = VM.here
-	for token in tokens: 
-		write_u16(VM, VM.here, token); VM.here += 2
-	return cwa
 	
-def getDictEntry(VM, dictTail):
-	ret = {}
-	ret["link"] = read_u16(VM, dictTail); dictTail += 2
-	ret["flag"] = read_u8(VM, dictTail); dictTail += 1
-	ret["len"] = read_u8(VM, dictTail) & DictFlags.LEN; dictTail += 1
-	ret["str"] = dictTail
-	# Add 2 and mask out low bit to get actual length of string
-	# EVEN: 2 => 4 => 4, which is right because we pad evens with 2 nulls
-	# ODD: 1 =>3 => 2, which is right because we pad odds with 1 null
-	ret["cwa"] = dictTail + ((ret["len"] + 2) & 0xFE)
-	return ret
-	
-def dumpDict(VM):
-	prev, prevprev = VM.dictTail, 0
-	while	prev != prevprev:
-		entry = getDictEntry(VM, prev)
-		cwa = entry["cwa"]
-		exec_token = read_i16(VM, cwa)
+	def __init__(self, VM):
+		self.VM = VM
 		
-		strs = []
-		for i in range(entry["flag"]): strs.append((4*"0" + hex(read_u16(VM, cwa+2*i))[2:])[-4:])
-		#print(' '.join(a+b for a,b in zip(s[::2], s[1::2])))
-		print(f"{hex(entry['link']):4} <= {hex(entry['str']-4):4} {entry['len']:2}{readStr(VM,entry['str']):10} *[{hex(cwa):4}]={' '.join(strs)}")
-		# Dump the dict entry in binary
-		#s=binascii.hexlify(VM.memory[entry['str']-4:cwa + 2*entry["flag"]]).decode("utf-8")
-		#print(' '.join(a+b for a,b in zip(s[::2], s[1::2])))
-		prevprev = prev
-		prev = entry["link"]
+	@staticmethod
+	def header(VM, name, immediate=False, hidden=False):
+		# u16 (link)
+		write_u16(VM, VM.herePP(2), VM.latest);
+		VM.latest = VM.here - 2
+		# u8 (number of token bytes)
+		write_u8(VM, VM.herePP(1), 0)
+		# u8 (flags | string length)
+		write_u8(VM, VM.herePP(1), 
+			(DictAccessor.IMMEDIATE if immediate else 0)
+			| (DictAccessor.HIDDEN if hidden else 0)
+			| len(name) & DictAccessor.LEN)
+		# n * u8 name string; plus 1 or 2 u8 of null
+		for letter in bytearray(name, "utf-8"): write_u8(VM, VM.herePP(1), letter)
+		# Always null terminate strings, and place next words on a 16b boundary
+		# So, pad evens with 2*null, odds with 1.
+		if VM.here % 2 == 0: write_u8(VM, VM.herePP(1), 0)
+		write_u8(VM, VM.herePP(1), 0)
+		# Helper to dump the bytes of the entry
+		#print(binascii.hexlify(VM.memory[VM.latest:VM.here]).decode("utf-8"))
+		
+	@staticmethod
+	def defcode(VM, name, tokens, immediate=False):
+		DictAccessor.header(VM, name, immediate=immediate)
+		# Needed to return head of code field
+		cwa = VM.here
+		# n*u16code list
+		for token in tokens: write_u16(VM, VM.herePP(2), token);
+		# Update code length field
+		write_u8(VM, VM.latest+2, 2*len(tokens))
+		#print(f"Defined {(10*' '+name)[-10:]}, from {(2*'0'+hex(old)[2:])[-2:]:2}..{(2*'0'+hex(VM.here)[2:])[-2:]:2}; strlen {len(name):2}, memlen is {VM.here-old:2}")
+		return cwa
+		
+	# Yields a pointer into the dict
+	def find(self, name): pass
+	def entry(self, address):
+		ret = {}
+		ret["link"] = read_u16(self.VM, address); address += 2
+		ret["codelen"] = read_u8(self.VM, address); address += 1
+		ret["strlen"] = read_u8(self.VM, address) & DictAccessor.LEN; address += 1
+		ret["str"] = address
+		# Add 2 and mask out low bit to get actual length of string
+		# EVEN: 2 => 4 => 4, which is right because we pad evens with 2 nulls
+		# ODD: 1 =>3 => 2, which is right because we pad odds with 1 null
+		ret["cwa"] = address + ((ret["strlen"] + 2) & 0xFE)
+		return ret
+		
+	def dump(self): 	
+		current, prev = self.VM.latest, 0
+		while	prev != current:
+			entry = self.entry(current)
+			cwa = entry["cwa"]
+			exec_token = read_i16(VM, cwa)
+		
+			strs = []
+			for i in range(entry["codelen"]): strs.append((4*"0" + hex(read_u16(VM, cwa+2*i))[2:])[-4:])
+			#print(' '.join(a+b for a,b in zip(s[::2], s[1::2])))
+			print(f"{hex(entry['link']):4} <= {hex(entry['str']-4):4} {entry['strlen']:2}{readStr(VM,entry['str']):10} *[{hex(cwa):4}]={' '.join(strs)}")
+			# Dump the dict entry in binary
+			#s=binascii.hexlify(VM.memory[entry['str']-4:cwa + 2*entry["flag"]]).decode("utf-8")
+			#print(' '.join(a+b for a,b in zip(s[::2], s[1::2])))
+			prev, current = current, entry["link"]
 		
 # Helper for formatting a 2 byte hex value from int		
 as_hex = lambda as_hex : f"{(4*'0' + hex(as_hex)[2:])[-4:]}"
 					
 class vm (object):
 	def __init__(self):
+		self.dict = DictAccessor(self)
 		self.memory = bytearray(256)
 		#0..128 is for dict and return stack, 128...224 is param stack, with lower addresses being PAD.
 		self.rStack = Stack(self, 200, lambda: self.here)
 		self.pStack = Stack(self, 240, lambda: self.rStack.sp)
 		self.words = []
 		# Address of "previous"  dictionary entry
-		self.dictTail = 0
+		self.latest = 0
 		# Address of next available byte in dictionary memory.
 		self.here = 0
 		# Pointer to "next" word to execute, pointer to interpreter.
 		self.currentWord, self.nextWord = 0, 0
 		# Should execute continue?
 		self.alive = True
-	
+		
+	def herePP(self, incr):
+		here, VM.here = VM.here, VM.here+incr
+		return here
+		
 	# Negative token numbers are native, positive token numbers are FORTH
 	def nativeWord(self, name, call):
 		token = -len(self.words)-1
 		self.words.append(call)
-		return addDictEntry(self, name, [token]), token
+		return DictAccessor.defcode(self, name, [token]), token
 		
 	def intWord(self, name, tokens):
-		return addDictEntry(self, name, tokens)
+		return DictAccessor.defcode(self, name, tokens)
 		
 	def step(self):
 		cwa_exec = read_u16(self, self.currentWord)
@@ -135,11 +149,11 @@ class vm (object):
 		print(str(binascii.hexlify(self.memory[lo:hi])))
 		
 	def addr_from_name(self, name):
-		prev = self.dictTail
-		while	prev != 0:
-			entry = getDictEntry(self, prev)
-			prev = entry["link"]
-			entryName = readStr(VM, entry["str"])
+		current = self.latest
+		while	current != 0:
+			entry = self.dict.entry(current)
+			current = entry["link"]
+			entryName = readStr(self, entry["str"])
 			if name == entryName: return entry
 		return None
 			
@@ -166,7 +180,7 @@ def plus_i16(VM):
 	next(VM)
 	
 def wd_tail(VM):
-	VM.pStack.push(VM.dictTail.to_bytes(2, "big"))
+	VM.pStack.push(VM.latest.to_bytes(2, "big"))
 	next(VM)
 	
 # ( n1 -- ) # Print the top value on the stack to stdout
@@ -213,6 +227,7 @@ def plus3(VM):
 def cr(VM):
 	print()
 	next(VM)
+	
 def literal(VM):
 	print(VM.currentWord, VM.nextWord)
 	number = read_i16(VM, VM.nextWord)
@@ -230,7 +245,7 @@ def bootstrap(VM):
 	(cwa_exit, _) = VM.nativeWord("EXIT", exit)
 	(cwa_dup, _) = VM.nativeWord("DUP", dup)
 	(cwa_printstr, _) = VM.nativeWord("prntstr", printstr)
-	(cwa_dictTail, _) = VM.nativeWord("wd.tail", wd_tail)
+	(cwa_latest, _) = VM.nativeWord("wd.tail", wd_tail)
 	(cwa_halt, _) = VM.nativeWord("HALT", halt)
 	(cwa_plus3, _) = VM.nativeWord("3+", plus3)
 	(cwa_literal, _) = VM.nativeWord("LITERAL", literal)
@@ -241,10 +256,12 @@ def bootstrap(VM):
 	#VM.intWord("wde.code", [""])
 	#VM.intWord("wde.dump", ["DUP", "wde.link", "DUP", "wde.name"])
 	VM.pStack.push([0x04, 0x00])
-	#tokens = [cwa_dictTail, cwa_dup, cwa_wdelink, cwa_dup, cwa_wdename, cwa_halt]
+	#tokens = [cwa_latest, cwa_dup, cwa_wdelink, cwa_dup, cwa_wdename, cwa_halt]
 	tokens = [cwa_literal, 0xFEED, cwa_dot, cwa_halt]
 	token_exc = VM.intWord("doAll", tokens)#token_q, token_dot, token_printstr, token_halt])
-	dumpDict(VM)
+	ac = DictAccessor(VM)
+	ac.dump()
+	#dumpDict(VM)
 	
 	VM.nextWord = VM.addr_from_name("doAll")["cwa"]; next(VM)
 	VM.run()
