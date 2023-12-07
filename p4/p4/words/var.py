4466f65f733e37ce44c87b29412ecf9df89712df
Matthew McRaven <matthew.mcraven@gmail.com>
2023-12-06 21:33:46 -0800

    Add ALLOT word
    ALLOT allocates space in the dictionary area, and pushes the address
    onto the stack.

--- p4/p4/words/var.py
+++ p4/p4/words/var.py
@@ -7,3 +7,13 @@ def literal(VM):
 	number = VM.memory.read_b16(VM.tcb.nextWord, signed=False)
 	VM.pStack.push_b16(number, signed=False)
 	VM.tcb.nextWord += 2
+
+# ( n -- addr ) Allocates N bytes of unmanaged, unreclaimable, global memory.
+# Address of first byte is pushed on stack
+@NAMED("ALLOT")
+@NEXT
+def allot(VM):
+	count = VM.pStack.pop_b16(signed=False)
+	VM.pStack.push_b16(VM.tcb.here, signed=False)
+	VM.tcb.here += count
+	
