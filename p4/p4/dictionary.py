6d24df272ca6cd47e86b688911749aaff2c100f7
Matthew McRaven <matthew.mcraven@gmail.com>
2023-12-06 18:14:08 -0800

    Add helper to compute CWA from address
    I will need this for find.

--- p4/p4/dictionary.py
+++ p4/p4/dictionary.py
@@ -2,29 +2,57 @@ import enum
 
 from .strings import readStr as _readStr
 
-# Bit masks for
+class Offsets(enum.IntEnum):
+	LINK = 0
+	CODELEN = 2
+	STRLEN = 3
+	STR = 4
+	
+# Bit masks for possible dictionary entry flags.
 class Flags(enum.IntEnum):
 	IMMEDIATE = 0x80
 	HIDDEN = 0x20
 	# LEN includes includes hidden bit.
 	# This way, a hidden entry will NEVER match against a string, since
 	# the reported length of the hidden word is longer than the max word size (31).
-	# Trick borrowed from JoensForth
-	LEN = 0x3f
+	# Trick borrowed from JoensForth.
+	LEN = 0x3f 
+	MAX_LEN = 0x1f# Actual max len is 0x1f
+
+# Helper to walk FORTH dictionary, returning a pointer to the head
+# of the first matching entry.
+def find(VM, name, matchHidden=False):
+	while	(current:=VM.tcb.latest) != 0:
+		# Use cascading conditionals to avoid nested ones.
+		# Each non-default conditional will prevent
+		# Do not match against a hidden entry if we respect the flag
+		if not matchHidden and (current+Offsets.STRLEN & Flags.Hidden): pass 
+		elif name != _readStr(VM, current+Offsets.STR): pass
+		else: return current
+		current = VM.memory.read_b16(current, signed=False)
+	return 0
+		
+# Assuming address points to the link field of a dictionary entry, return the address of the first code word.
+def cwa(VM, address):
+	# Mask out non-length bits of the strlen field.
+	strlen = VM.memory.read_b8(address + Offsets.STRLEN, signed=False) & Flags.MAX_LEN
+	# Add 1 and mask out low bit to get actual length of string
+	# Mask out other flags
+	# EVEN: 2 => 4 => 4, which is right because we pad evens with 2 nulls
+	# ODD: 1 =>3 => 2, which is right because we pad odds with 1 null
+	offset = ((strlen + 2) & 0xFE)
+	return address + Offsets.STR + offset
 	
 # Native accessors for FORTH dictionary
 
 # Given an address, attempt to parse it as a dictionary header.
 def entry(VM, address):
 	ret = {}
-	ret["link"] = VM.memory.read_b16(address, signed=False); address += 2
-	ret["codelen"] = VM.memory.read_b8(address, signed=False); address += 1
-	ret["strlen"] = VM.memory.read_b8(address, signed=False) & Flags.LEN; address += 1
-	ret["str"] = address
-	# Add 2 and mask out low bit to get actual length of string
-	# EVEN: 2 => 4 => 4, which is right because we pad evens with 2 nulls
-	# ODD: 1 =>3 => 2, which is right because we pad odds with 1 null
-	ret["cwa"] = address + ((ret["strlen"] + 2) & 0xFE)
+	ret["link"] = VM.memory.read_b16(address + Offsets.LINK, signed=False)
+	ret["codelen"] = VM.memory.read_b8(address + Offsets.CODELEN, signed=False)
+	ret["strlen"] = VM.memory.read_b8(address + Offsets.STRLEN, signed=False) & Flags.LEN
+	ret["str"] = address + Offsets.STR
+	ret["cwa"] = cwa(VM, address)
 	return ret
 	
 # Pretty print the entire contents of the FORTH dictionary.
@@ -33,8 +61,7 @@ def dump(VM):
 	while	current != 0:
 		header = entry(VM, current)
 		cwa = header["cwa"]
-		exec_token = VM.memory.read_b16(cwa, signed=True)
-		
+		exec_token = VM.memory.read_b16(cwa, signed=True)	
 		strs = []
 		for i in range(header["codelen"]//2): strs.append((4*"0" + hex(VM.memory.read_b16(cwa+2*i,signed=False))[2:])[-4:])
 		#print(' '.join(a+b for a,b in zip(s[::2], s[1::2])))
