82680f60d4dd0851a54f0172bf34022a1f6b721b
Matthew McRaven <matthew.mcraven@gmail.com>
2023-12-18 13:09:05 -0800

    Add | & = >0 words
    Drop 3+ because it was a dummy word.

--- p4/words/math.py
+++ p4/words/math.py
@@ -7,8 +7,30 @@ def plus_i16(VM):
 	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
 	VM.pStack.push_b16(lhs+rhs, signed=True)
 
-# (n1 -- n1+3)
-@NAMED("3+")
-@NEXT	
-def plus3_i16(VM):
-	VM.pStack.push_b16(VM.pStack.pop_b16(signed=True)+3, signed=True)
+# (n1 n2 -- n1+n2)
+@NAMED("OR")
+@NEXT
+def or_i16(VM):
+	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	VM.pStack.push_b16(lhs | rhs, signed=True)
+
+# (n1 n2 -- n1 && n2)
+@NAMED("AND")
+@NEXT
+def and_i16(VM):
+	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	VM.pStack.push_b16(lhs & rhs, signed=True)
+
+# (n1 n2 -- n1 == n2)
+@NAMED("=")
+@NEXT
+def equals_i16(VM):
+	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	VM.pStack.push_b16(lhs == rhs, signed=True)
+
+# (n1 -- n1 > 0)
+@NAMED(">0")
+@NEXT
+def gt0_i16(VM):
+	lhs, = VM.pStack.pop_b16(signed=True)
+	VM.pStack.push_b16(lhs > 0, signed=True)
