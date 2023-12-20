0d02d636a2686afff16a087609c4bcc1dfc024b3
Matthew McRaven <mkm302@georgetown.edu>
2023-12-20 13:53:25 -0800

    Fix ordering of math operators (again)
    3 + 4 - 5 in postfix should be 3 4 + 5 -, but the ordering of my math operands was backwards.

--- p4/words/math.py
+++ p4/words/math.py
@@ -3,34 +3,34 @@ from ..utils import NATIVE
 # (n1 n2 -- n1+n2)
 @NATIVE("+")
 def plus_i16(VM):
-	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	rhs, lhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
 	VM.pStack.push_b16(lhs+rhs, signed=True)
 	VM.next()
 # (n1 n2 -- n1-n2)
 @NATIVE("-")
 def sub_i16(VM):
-	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	rhs, lhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
 	VM.pStack.push_b16(lhs-rhs, signed=True)
 	VM.next()
 
 # (n1 n2 -- n1|n2)
 @NATIVE("OR")
 def or_i16(VM):
-	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	rhs, lhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
 	VM.pStack.push_b16(lhs | rhs, signed=True)
 	VM.next()
 
 # (n1 n2 -- n1 && n2)
 @NATIVE("AND")
 def and_i16(VM):
-	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	rhs, lhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
 	VM.pStack.push_b16(lhs & rhs, signed=True)
 	VM.next()
 
 # (n1 n2 -- n1 == n2)
 @NATIVE("=")
 def equals_i16(VM):
-	lhs, rhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
+	rhs, lhs = VM.pStack.pop_b16(signed=True), VM.pStack.pop_b16(signed=True)
 	VM.pStack.push_b16(lhs == rhs, signed=True)
 	VM.next()
 
