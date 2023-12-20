4b62d1799237ff1a652f4d8b593765161eeb49d6
Matthew McRaven <mkm302@georgetown.edu>
2023-12-19 22:20:59 -0800

    Add byte>cell and cell>byte words
    SE sign extends a byte, TRUNC chops the high order byte and keeps the sign.

--- p4/words/math.py
+++ p4/words/math.py
@@ -31,6 +31,19 @@ def equals_i16(VM):
 # (n1 -- n1 > 0)
 @NATIVE(">0")
 def gt0_i16(VM):
-	lhs, = VM.pStack.pop_b16(signed=True)
+	lhs = VM.pStack.pop_b16(signed=True)
 	VM.pStack.push_b16(lhs > 0, signed=True)
+	VM.next()
+
+# (byte -- sign-extended word)
+@NATIVE("se")
+def se(VM):
+	VM.pStack.push_b16(VM.pStack.pop_b8(signed=True), signed=True)
+	VM.next()
+
+# (cell -- byte) Truncate cell to byte, keeping sign (I hope)
+@NATIVE("TRUNC")
+def TRUNC(VM):
+	val = VM.pStack.pop_b16(signed=True) & 0xff
+	VM.pStack.push_b8(val, signed=False if val > 0 else True)
 	VM.next()
