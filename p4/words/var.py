2b624c02f6d3e27e1614c126bc2dd22e7918c82f
Matthew McRaven <mkm302@georgetown.edu>
2023-12-20 15:43:50 -0800

    Move ALLOT to be a true FORTH word

--- p4/words/var.py
+++ p4/words/var.py
@@ -7,13 +7,3 @@ def literal(VM):
 	VM.pStack.push_b16(number, signed=False)
 	VM.tcb.nextWord(VM.tcb.nextWord() + 2)
 	VM.next()
-
-# ( n -- addr ) Allocates N bytes of unmanaged, unreclaimable, global memory.
-# Address of first byte is pushed on stack
-@NATIVE("ALLOT")
-def allot(VM):
-	count = VM.pStack.pop_b16(signed=False)
-	VM.pStack.push_b16(VM.tcb.here(), signed=False)
-	VM.tcb.here(VM.tcb.here() + count)
-	VM.next()
-	
