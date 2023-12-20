233d2990236b8e9b7a3e5b19e2d3f667f6534e2f
Matthew McRaven <mkm302@georgetown.edu>
2023-12-19 22:47:41 -0800

    Add pure FORTH files; load them in our test environment

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from p4.words import extract, core, io, math, stack, var, dict as _dict, interpret
 import p4.bootstrap, p4.vm, p4.dictionary
+from p4.words.io import open_file
 
 words = [fn for item in [core, stack, io, var, math, _dict, interpret] for fn in extract(item)]
 VM = p4.vm.vm()
@@ -11,5 +12,8 @@ f = lambda s: e(s)["cwa"]
 
 p4.dictionary.defforth(VM, ("doAll", 0, ["'", ".", "CR", "EXIT"]))
 p4.dictionary.defforth(VM, ("testReword", 0, "LIT 1 COREINT BRANCH -4".split()))
+
+with open("p4/bootstrap/dummy.f") as x: open_file(" ".join(x.readlines()))
+
 VM.tcb.nextWord(f("testReword")); VM.next()
 VM.run()
