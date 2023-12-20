2fa60a7b1632138822158a1d9d8ad94d6b22b8ab
Matthew McRaven <mkm302@georgetown.edu>
2023-12-19 22:17:10 -0800

    Add character pointer read/write !c @c

--- p4/words/core.py
+++ p4/words/core.py
@@ -24,12 +24,27 @@ def fetch(VM):
 	VM.pStack.push_b16(VM.memory.read_b16(addr, False), signed=False)
 	VM.next()
 
+# ( addr -- value) # Dereference a pointer to a byte
+@NATIVE("@c")
+def fetchchar(VM):
+	addr = VM.pStack.pop_b16(signed=False)
+	VM.pStack.push_b8(VM.memory.read_b8(addr, False), signed=False)
+	VM.next()
+
 # ( addr value --) # Write to a pointer
 @NATIVE("!")
 def store(VM):
 	value = VM.pStack.pop_b16(signed=False)
 	addr = VM.pStack.pop_b16(signed=False)
 	VM.memory.write_b16(addr, value, False)
+	VM.next()
+
+# ( addr value --) # Write to a pointer, single byte
+@NATIVE("!c")
+def storechar(VM):
+	value = VM.pStack.pop_b8(signed=False)
+	addr = VM.pStack.pop_b16(signed=False)
+	VM.memory.write_b8(addr, value, False)
 	VM.next()
 
 # Stops further instructions from executing in VM
