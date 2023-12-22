1718015c8e5875cde364918019b66f84d4416ba6
Matthew McRaven <mkm302@georgetown.edu>
2023-12-21 23:09:38 -0800

    Add additional debugging words

--- p4/vocabs/debug/dict.py
+++ p4/vocabs/debug/dict.py
@@ -10,3 +10,12 @@ import p4.dictionary
 def debug_dump_dict(VM):
 	p4.dictionary.dump(VM)
 	VM.next()
+@NATIVE("^RS.")
+def debug_dump_ret_stack(VM):
+	VM.rStack.dump()
+	VM.next()
+
+@NATIVE("^DBG")
+def debug_enable_printing(VM):
+	VM.debug = False if VM.debug else True
+	VM.next()
