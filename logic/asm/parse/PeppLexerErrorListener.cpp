e5d6f4e2e6452349ae1d1711b410642fd39c0f7d
Matthew McRaven <matthew.mcraven@gmail.com>
2024-01-10 17:12:59 -0800

    feat(asm): add error reporter for Pepp lexer
    At this time, it simply indicates if an error occured.
    This will be useful for automated unit testing.

---
+++ logic/asm/parse/PeppLexerErrorListener.cpp
@@ -0,0 +1,26 @@
+#include "PeppLexerErrorListener.h"
+
+bool parse::PeppLexerErrorListener::hadError()
+{
+    return _hadError;
+}
+
+void parse::PeppLexerErrorListener::syntaxError(antlr4::Recognizer *recognizer, antlr4::Token *offendingSymbol, size_t line, size_t charPositionInLine, const std::string &msg, std::exception_ptr e)
+{
+    _hadError = true;
+}
+
+void parse::PeppLexerErrorListener::reportAmbiguity(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex, size_t stopIndex, bool exact, const antlrcpp::BitSet &ambigAlts, antlr4::atn::ATNConfigSet *configs)
+{
+
+}
+
+void parse::PeppLexerErrorListener::reportAttemptingFullContext(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex, size_t stopIndex, const antlrcpp::BitSet &conflictingAlts, antlr4::atn::ATNConfigSet *configs)
+{
+
+}
+
+void parse::PeppLexerErrorListener::reportContextSensitivity(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex, size_t stopIndex, size_t prediction, antlr4::atn::ATNConfigSet *configs)
+{
+
+}
