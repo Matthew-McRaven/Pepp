488d7e5abb699d029740c7a68f1ddd05e577d629
Matthew McRaven <matthew.mcraven@gmail.com>
2024-02-04 09:29:31 -0800

    test(sim): convert frame utils to use concept
    This will allow me to elide a per-packet-header type method on my functors.

--- logic/sim/trace2/frame_utils.h
+++ logic/sim/trace2/frame_utils.h
@@ -3,13 +3,27 @@
 #include "sim/api2.hpp"
 #include "bits/operations/copy.hpp"
 namespace sim::trace2 {
-class UpdateFrameLength {
+template<typename T>
+concept HasLength = requires(T t) {
+    {
+        t.length
+    } -> std::convertible_to<decltype(t.length)>;
+};
+
+class UpdateFrameLength
+{
 public:
     UpdateFrameLength(quint16 length, sim::api2::frame::Header &out)
         : _length(length)
         , _out(out){};
-    void operator()(const sim::api2::frame::header::Trace &header) const;
-    void operator()(const auto &header) const {};
+    template<HasLength Header>
+    void operator()(const Header &header)
+    {
+        Header copy = header;
+        copy.length = _length;
+        _out = copy;
+    }
+    void operator()(const auto &header){};
 
 private:
     quint16 _length = 0;
@@ -18,25 +32,47 @@ private:
 
 struct GetFrameLength
 {
-    quint16 operator()(const sim::api2::frame::header::Trace &header) const;
+    template<HasLength Header>
+    quint16 operator()(const Header &header) const
+    {
+        return header.length;
+    }
     quint16 operator()(const auto &header) const { return 0; };
 };
 
-class UpdateFrameBackOffset {
+template<typename T>
+concept HasBackOffset = requires(T t) {
+    {
+        t.back_offset
+    } -> std::convertible_to<decltype(t.back_offset)>;
+};
+class UpdateFrameBackOffset
+{
 public:
     UpdateFrameBackOffset(quint16 back_offset, sim::api2::frame::Header &out)
         : _back_offset(back_offset)
         , _out(out){};
-    void operator()(const sim::api2::frame::header::Trace &header) const;
-    void operator()(const auto &header) const {};
+    template<HasBackOffset Header>
+    void operator()(const Header &header)
+    {
+        Header copy = header;
+        copy.back_offset = _back_offset;
+        _out = copy;
+    }
+    void operator()(const auto &header){};
 
 private:
     quint16 _back_offset = 0;
     sim::api2::frame::Header &_out;
 };
+
 struct GetFrameBackOffset
 {
-    quint16 operator()(const sim::api2::frame::header::Trace &header) const;
+    template<HasBackOffset Header>
+    quint16 operator()(const Header &header) const
+    {
+        return header.back_offset;
+    }
     quint16 operator()(const auto &header) const { return 0; };
 };
-} // sime::trace2
+} // namespace sim::trace2
