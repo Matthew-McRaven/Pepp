0d1cf438ed3acaef454c4cc1b6b94b8d153dad80
Matthew McRaven <matthew.mcraven@gmail.com>
2024-02-04 10:16:18 -0800

    test(bin.term): translate `get` tests to CPP

--- bin/term/test/ls.cpp
+++ bin/term/test/ls.cpp
@@ -6,13 +6,6 @@ TEST_CASE("Terminal, ls", "[term][cli]")
     auto path = term_path();
     QProcess term;
     term.start(path, {"ls"});
-    REQUIRE(term.waitForStarted());
-    REQUIRE(term.waitForFinished());
-    CHECK(term.exitCode() == 0);
+    wait_return(term, 0);
     CHECK(term.readAllStandardOutput().length() > 0);
-}
-
-int main(int argc, char *argv[])
-{
-    return Catch::Session().run(argc, argv);
 }
