6faa0784cfdb9a2eed157387fbfa4ee2c0f3b074
Matthew McRaven <matthew.mcraven@gmail.com>
2024-02-11 11:40:10 -0800

    build: create amalgamated test targets
    test-lib-all includes all catch test sources, and links against every library.
    A test browser might link against this target to execute its tests.
    
    test-all is a target which executes all of the tests in test-lib-all.
    It mainly exists to ensure that there are no linker issues cause by source amalgamation.
    
    Progress on #425, pending a test browser UI.

--- config/cmake/create_targets.cmake
+++ config/cmake/create_targets.cmake
@@ -2,7 +2,55 @@ include_guard()
 # Allows us to not use __declspec(dll...) everywhere
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
 # We always want universal builds, so do not set on a per-target basis
+# NOTE: This prevents us from building statically!!
 SET(CMAKE_OSX_ARCHITECTURES "x86_64;arm64" CACHE INTERNAL "" FORCE)
+
+
+# test-lib-all will include the sources for all catch tests, and be dependent on all the tests' libaries.
+# Our test browser must depend on this target.
+qt6_add_library(test-lib-all INTERFACE)
+# test-all bundles all the tests into a single executable to prove that there are no linker errors.
+# do not add to ctest, otherwise every test runs twice.
+qt6_add_executable(test-all ${CMAKE_CURRENT_LIST_DIR}/main.cpp)
+set_target_properties(test-all PROPERTIES FOLDER "qtc_runnable")
+target_link_libraries(test-all PUBLIC test-lib-all catch)
+# Failure to copy DLLs on Windows causes tests to fail at runtime.
+if (WIN32)
+    add_custom_command(TARGET test-all POST_BUILD
+            COMMAND ${CMAKE_COMMAND}
+            ARGS -E copy $<TARGET_RUNTIME_DLLS:test-all> $<TARGET_FILE_DIR:test-all>
+            COMMAND_EXPAND_LISTS
+    )
+endif ()
+
+# Track all libraries that have been linked to in make_target.
+# At the end of all build steps we can copy all of the libraries exactly once.
+DEFINE_PROPERTY(GLOBAL PROPERTY ALL_LIBRARIES)
+set_property(GLOBAL PROPERTY ALL_LIBRARIES "")
+function(maybe_append_all_libraries library)
+    # message("Called with ${library}")
+    # Check if the custom property (LINKED_LIBRARIES) exists for the target.
+    get_property(is_defined GLOBAL PROPERTY ALL_LIBRARIES DEFINED)
+    if (is_defined)
+        # Property exists, check if the library is already in the list.
+        get_property(linked_libraries GLOBAL PROPERTY ALL_LIBRARIES)
+        # message("So far: ${linked_libraries}")
+        if (NOT "${library}" IN_LIST linked_libraries)
+            # message("So far ${linked_libraries}")
+            # Library not found in the list, need to link it.
+            target_link_libraries(test-lib-all INTERFACE ${library})
+            # Update the LINKED_LIBRARIES property.
+            list(APPEND linked_libraries ${library})
+            set_property(GLOBAL PROPERTY ALL_LIBRARIES ${linked_libraries})
+        endif ()
+    else ()
+        # Property does not exist, so link the library and create the property.
+        target_link_libraries(test-lib-all INTERFACE ${library})
+        set_property(GLOBAL PROPERTY ALL_LIBRARIES ${library})
+        # message("It is ${is_defined}")
+    endif ()
+endfunction()
+
 
 # Helper to find and enable clang-tidy
 macro(inject_clang_tidy)
@@ -10,6 +58,7 @@ macro(inject_clang_tidy)
     # find_program(CLANG_TIDY "clang-tidy")
     # set(CMAKE_CXX_CLANG_TIDY clang-tidy -checks=-*,clang-analyzer-*)
 endmacro()
+
 
 # Force our desired C++ standard.
 macro(inject_cxx_standard)
@@ -59,8 +108,17 @@ function(make_target)
         qt6_add_library(${MK_TARGET} ${MK_SOURCES})
     elseif (MK_TYPE STREQUAL EXEC)
         qt6_add_executable(${MK_TARGET} ${MK_SOURCES})
-        # Replaces old make_qtest macro whose only differebce was DLL copying and add_test
+        # Replaces old make_qtest macro whose only difference was DLL copying and add_test
     elseif (MK_TYPE STREQUAL TEST)
+        # Amalgamate all test sources into a single target, and link against all dependencies exactly once.
+        target_sources(test-lib-all INTERFACE ${MK_SOURCES})
+        foreach (depend ${MK_DEPENDS})
+            # Don't bring in another source of "main"
+            if (NOT ${depend} MATCHES "catch-main")
+                maybe_append_all_libraries(${depend})
+            endif ()
+        endforeach ()
+
         add_executable(${MK_TARGET} ${MK_SOURCES})
         # Failure to copy DLLs on Windows causes tests to fail at runtime.
         if (WIN32)
