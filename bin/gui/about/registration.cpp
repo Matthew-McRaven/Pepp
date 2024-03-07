315ae691ce9deae9671f73a6841a6c4a27e30cab
Matthew McRaven <matthew.mcraven@gmail.com>
2024-03-06 17:32:41 -0800

    feat(about): correct version number, maintainers
    Convert library code to functions returning values.
    It is less difficult to avoid linker errors that way.

--- bin/gui/about/registration.cpp
+++ bin/gui/about/registration.cpp
@@ -15,9 +15,68 @@
  */
 
 #include "registration.hpp"
-#include "book_item_model.hpp"
+#include "help/about/pepp.hpp"
+#include "help/about/version.hpp"
+
+Version::Version(QObject *parent) : QObject(parent) {}
+QString Version::git_sha() { return about::g_GIT_SHA1(); }
+QString Version::git_tag() { return about::g_GIT_TAG(); }
+bool Version::git_dirty() { return about::g_GIT_LOCAL_CHANGES(); }
+int Version::version_major() { return about::g_MAJOR_VERSION(); }
+int Version::version_minor() { return about::g_MINOR_VERSION(); }
+int Version::version_patch() { return about::g_PATCH_VERSION(); }
+QString Version::version_str_full() {
+  return u"%1.%2.%3"_qs.arg(version_major()).arg(version_minor()).arg(version_patch());
+}
+
+Maintainer::Maintainer(QString name, QString email, QObject *parent) : QObject(parent), _name(name), _email(email) {}
+QString Maintainer::name() { return _name; }
+QString Maintainer::email() { return _email; }
 
 namespace about {
-    void registerTypes(QQmlApplicationEngine &engine) {
+void registerTypes(QQmlApplicationEngine &engine) {
+  qmlRegisterSingletonType<Version>("edu.pepp", 1, 0, "Version",
+                                    [](QQmlEngine *, QJSEngine *) { return new Version(); });
+  qmlRegisterUncreatableType<Maintainer>("edu.pepp", 1, 0, "Maintainer", "Must be created from C++");
+  qmlRegisterSingletonType<QList<Maintainer *>>("edu.pepp", 1, 0, "Maintainers", [](QQmlEngine *, QJSEngine *) {
+    // Need global scope ::, or it picks up about::Maintainer
+    QList<::Maintainer *> maintainers{};
+    for (const auto &maintainer : about::maintainers()) {
+      auto *item = new ::Maintainer(maintainer.name, maintainer.email, nullptr);
+      maintainers.push_back(item);
     }
+    // Class assumes ownership of objects via modifying parent pointer.
+    auto owning = new MaintainerList(maintainers);
+    return owning;
+  });
+}
 } // namespace about
+MaintainerList::MaintainerList(QList<Maintainer *> list, QObject *parent) : QAbstractListModel(parent), _list(list) {
+  // Must re-parent to avoid memory leaks.
+  for (auto *item : list)
+    item->setParent(this);
+}
+
+int MaintainerList::rowCount(const QModelIndex &parent) const { return _list.size(); }
+
+QVariant MaintainerList::data(const QModelIndex &index, int role) const {
+  int row = index.row();
+  if (!index.isValid() || row < 0 || row >= _list.size())
+    return QVariant();
+  auto *item = _list.at(row);
+  switch (role) {
+  case NAME:
+    return item->name();
+  case EMAIL:
+    return item->email();
+  default:
+    return QVariant();
+  }
+}
+
+QHash<int, QByteArray> MaintainerList::roleNames() const {
+  QHash<int, QByteArray> roles;
+  roles[NAME] = "name";
+  roles[EMAIL] = "email";
+  return roles;
+}
--- bin/gui/about/registration.hpp
+++ bin/gui/about/registration.hpp
@@ -19,6 +19,52 @@
 #include <QtCore>
 #include "commands/gui.hpp"
 
+class Version : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(QString git_sha READ git_sha CONSTANT)
+  Q_PROPERTY(QString git_tag READ git_tag CONSTANT)
+  Q_PROPERTY(bool git_dirty READ git_dirty CONSTANT)
+  Q_PROPERTY(int version_major READ version_major CONSTANT)
+  Q_PROPERTY(int version_minor READ version_minor CONSTANT)
+  Q_PROPERTY(int version_patch READ version_patch CONSTANT)
+  Q_PROPERTY(QString version_str_full READ version_str_full CONSTANT)
+public:
+  explicit Version(QObject *parent = nullptr);
+  ~Version() override = default;
+  static QString git_sha();
+  static QString git_tag();
+  static bool git_dirty();
+  static int version_major();
+  static int version_minor();
+  static int version_patch();
+  static QString version_str_full();
+};
+class Maintainer : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(QString name READ name CONSTANT)
+  Q_PROPERTY(QString email READ email CONSTANT)
+public:
+  Maintainer(QString name, QString email, QObject *parent = nullptr);
+  ~Maintainer() override = default;
+  QString name();
+  QString email();
+
+private:
+  QString _name = {}, _email = {};
+};
+class MaintainerList : public QAbstractListModel {
+public:
+  enum { NAME = Qt::UserRole, EMAIL = Qt::UserRole + 1 };
+  explicit MaintainerList(QList<Maintainer *> list, QObject *parent = nullptr);
+  ~MaintainerList() override = default;
+  int rowCount(const QModelIndex &parent) const override;
+  QVariant data(const QModelIndex &index, int role) const override;
+  QHash<int, QByteArray> roleNames() const override;
+
+private:
+  QList<Maintainer *> _list;
+};
+
 namespace about {
-    void registerTypes(QQmlApplicationEngine &engine);
+void registerTypes(QQmlApplicationEngine &engine);
 }
