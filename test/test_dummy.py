1a2a394295d2d79ba027597e841c1c203b784bba
Matthew McRaven <matthew.mcraven@gmail.com>
2024-05-07 22:28:18 -0700

    Remove unused dummy tests

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -25,18 +25,3 @@ class TestStringMethods(unittest.TestCase):
 
         VM.tcb.nextWord(f("testReword")); VM.next()
         VM.run()
-
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
