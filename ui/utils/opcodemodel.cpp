89d1b16beca8901bdba877b4a07bd8642a5df0d2
Matthew McRaven <matthew.mcraven@gmail.com>
2024-05-12 12:16:54 -0700

    fix: store opcodes for byte convert in alphabetical order
    Current sort order requires a user already know the opcode value, defeating the purpose.
    Alphabetic search is probably more user-friendly.
    
    Instructions are sorted by the addressless mnemonic first, and then by opcode value for addressing modes.

--- ui/utils/opcodemodel.cpp
+++ ui/utils/opcodemodel.cpp
@@ -12,24 +12,38 @@ QVariant OpcodeModel::data(const QModelIndex &index, int role) const {
     return QVariant();
   switch (role) {
   case Qt::DisplayRole:
-    return _mnemonics[index.row()].first;
+    return _mnemonics[index.row()].mnemonic_addr;
   }
   return {};
 }
 
 qsizetype OpcodeModel::indexFromOpcode(quint8 opcode) const {
-
   auto result =
-      std::find_if(_mnemonics.begin(), _mnemonics.end(), [&](const auto &pair) { return pair.second == opcode; });
+      std::find_if(_mnemonics.begin(), _mnemonics.end(), [&](const auto &pair) { return pair.opcode == opcode; });
   if (result == _mnemonics.end())
     return 0;
-  return result.key();
+  return result - _mnemonics.begin();
 }
 
 quint8 OpcodeModel::opcodeFromIndex(qsizetype index) const {
-  if (_mnemonics.contains(index))
-    return _mnemonics[index].second;
+  if (index > 0 && index < _mnemonics.size())
+    return _mnemonics[index].opcode;
   return -1;
 }
 
-void OpcodeModel::appendRow(QString mnemonic, quint8 opcode) { _mnemonics[_mnemonics.size()] = {mnemonic, opcode}; }
+void OpcodeModel::appendRow(QString mnemonic, quint8 opcode) {
+  auto indexOfComma = mnemonic.indexOf(',');
+  Opcode toInsert{
+      .opcode = opcode,
+      .mnemonic_addr = mnemonic,
+      .mnemonic_only = indexOfComma == -1 ? QStringView(mnemonic) : QStringView(mnemonic).left(indexOfComma),
+  };
+  // Sort the vector alphabetically on mnemonic, and on opcode for addressing modes.
+  auto index =
+      std::lower_bound(_mnemonics.begin(), _mnemonics.end(), toInsert, [](const auto &pair, const auto &toInsert) {
+        if (pair.mnemonic_only == toInsert.mnemonic_only)
+          return pair.opcode < toInsert.opcode;
+        return pair.mnemonic_only < toInsert.mnemonic_only;
+      });
+  _mnemonics.insert(index, toInsert);
+}
--- ui/utils/opcodemodel.hpp
+++ ui/utils/opcodemodel.hpp
@@ -21,9 +21,16 @@ public:
   void appendRow(QString mnemonic, quint8 opcode);
 
 private:
-  // Store the opcode's mnemonic string and intege value.
-  using T = std::pair<QString, quint8>;
-  // Map index/rows to opcodes.
-  using MapT = QMap<qsizetype, T>;
-  MapT _mnemonics = {};
+  struct Opcode {
+    quint8 opcode;
+    QString mnemonic_addr;
+    QStringView mnemonic_only;
+  };
+  // Map index/rows to opcodes. Store the opcode's mnemonic string and integer value.
+  // Upon each insert, ensure that vector is sorted alphabetically by opcode name, then by opcode value for addressing
+  // modes. The opcode value sorting is required to prevent ADDA,d from occuring before ADDA,i.
+  // NOTE: I don't expect to insert more than 200 items, so the
+  // N^2 performance of repeated insertion should be acceptable. This assumption will likely be broken for RISC-V, but I
+  // will deal with that later.
+  std::vector<Opcode> _mnemonics = {};
 };
