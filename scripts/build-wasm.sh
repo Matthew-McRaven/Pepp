2efa2180aab3dfebd52d4fd716eafe7b7fd6dbd0
Matthew McRaven <35609985+Matthew-McRaven@users.noreply.github.com>
2024-06-23 20:21:50 +0000

    chore: fix TZ-related crash in sphinx

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,3 +1,5 @@
 source /emsdk/emsdk_env.sh
 cmake -B build -D CMAKE_TOOLCHAIN_FILE=/qt/6.7.2/wasm_singlethread/lib/cmake/Qt6/qt.toolchain.cmake  -D QT_HOST_PATH=/qt/6.7.2/gcc_64 .
+# Bug with sphinx, see https://github.com/nektos/act/issues/1853.
+export TZ=UTC
 cd build && cmake --build . -j16
