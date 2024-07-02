ba3477684fefa0bef04252fc6d686cf0ac066df2
Matthew McRaven <matthew.mcraven@gmail.com>
2024-07-01 18:47:36 -0700

    fix(text): preserve alpha value in editor colors
    Requires doing alpha blending ourselves, since scintilla destroys alpha values.
    See destruction of alpha in Editor.cxx `Editor::StyleSetMessage` & `Editor::StyleGetMessage`.

--- ui/text/editor/scintillaasmeditbase.cpp
+++ ui/text/editor/scintillaasmeditbase.cpp
@@ -129,32 +129,51 @@ void ScintillaAsmEditBase::setLineNumbersVisible(bool visible) {
 }
 
 sptr_t c2i(const QColor &color) { return ColourRGBAFromQColor(color).AsInteger(); }
+// Editor ignores alpha, so we need do do alpha compositing ourselves
+// See: https://en.wikipedia.org/wiki/Alpha_compositing
+sptr_t alphaBlend(const QColor &front, const QColor &back) {
+  // Must do alpha math in [0,1]
+  float fa = front.alphaF(), ba = back.alphaF(), fa1 = 1 - fa, bafa1 = ba * fa1;
+  float frA = fa + bafa1;
+  // Calculate colors using the over operator
+  // Clip to [0,255] for ease of conversion
+  auto rR = static_cast<quint8>((front.red() * fa + back.red() * bafa1) / frA);
+  auto rG = static_cast<quint8>((front.green() * fa + back.green() * bafa1) / frA);
+  auto rB = static_cast<quint8>((front.blue() * fa + back.blue() * bafa1) / frA);
+  // Scale up alpha to [0,255] so it is in the same range as color components
+  auto rA = static_cast<quint8>(frA * 255);
+  // Premultiply colors by alpha so that the editor can drop alpha channel w/o loss of info.
+  auto premult = QRgba64::fromArgb32(qRgba(rR, rG, rB, rA)).premultiplied();
+  return c2i(QColor::fromRgba64(premult));
+}
 void ScintillaAsmEditBase::applyStyles() {
+  // WARNING: If you anticipate a color having an alpha value, you will need to do the blending yourself!
   if (_theme == nullptr) return;
+  auto baseBack = _theme->base()->background();
   send(SCI_STYLESETFORE, STYLE_DEFAULT, c2i(_theme->base()->foreground()));
   send(SCI_SETCARETFORE, c2i(_theme->base()->foreground()));
   send(SCI_STYLESETBACK, STYLE_DEFAULT, c2i(_theme->base()->background()));
   send(SCI_STYLECLEARALL, 0, 0);
   send(SCI_STYLESETFORE, symbolStyle, c2i(_theme->symbol()->foreground()));
-  send(SCI_STYLESETBACK, symbolStyle, c2i(_theme->symbol()->background()));
+  send(SCI_STYLESETBACK, symbolStyle, alphaBlend(_theme->symbol()->background(), baseBack));
   send(SCI_STYLESETFORE, mnemonicStyle, c2i(_theme->mnemonic()->foreground()));
-  send(SCI_STYLESETBACK, mnemonicStyle, c2i(_theme->mnemonic()->background()));
+  send(SCI_STYLESETBACK, mnemonicStyle, alphaBlend(_theme->mnemonic()->background(), baseBack));
   send(SCI_STYLESETFORE, directiveStyle, c2i(_theme->directive()->foreground()));
-  send(SCI_STYLESETBACK, directiveStyle, c2i(_theme->directive()->background()));
+  send(SCI_STYLESETBACK, directiveStyle, alphaBlend(_theme->directive()->background(), baseBack));
   send(SCI_STYLESETFORE, macroStyle, c2i(_theme->macro()->foreground()));
-  send(SCI_STYLESETBACK, macroStyle, c2i(_theme->macro()->background()));
+  send(SCI_STYLESETBACK, macroStyle, alphaBlend(_theme->macro()->background(), baseBack));
   send(SCI_STYLESETFORE, charStyle, c2i(_theme->character()->foreground()));
-  send(SCI_STYLESETBACK, charStyle, c2i(_theme->character()->background()));
+  send(SCI_STYLESETBACK, charStyle, alphaBlend(_theme->character()->background(), baseBack));
   send(SCI_STYLESETFORE, stringStyle, c2i(_theme->string()->foreground()));
-  send(SCI_STYLESETBACK, stringStyle, c2i(_theme->string()->background()));
+  send(SCI_STYLESETBACK, stringStyle, alphaBlend(_theme->string()->background(), baseBack));
   send(SCI_STYLESETFORE, SCE_PEPASM_MACRO_START, c2i(_theme->comment()->foreground()));
-  send(SCI_STYLESETBACK, SCE_PEPASM_MACRO_START, c2i(_theme->comment()->background()));
+  send(SCI_STYLESETBACK, SCE_PEPASM_MACRO_START, alphaBlend(_theme->comment()->background(), baseBack));
   send(SCI_STYLESETFORE, SCE_PEPASM_MACRO_END, c2i(_theme->comment()->foreground()));
-  send(SCI_STYLESETBACK, SCE_PEPASM_MACRO_END, c2i(_theme->comment()->background()));
+  send(SCI_STYLESETBACK, SCE_PEPASM_MACRO_END, alphaBlend(_theme->comment()->background(), baseBack));
   send(SCI_STYLESETFORE, commentStyle, c2i(_theme->comment()->foreground()));
-  send(SCI_STYLESETBACK, commentStyle, c2i(_theme->comment()->background()));
+  send(SCI_STYLESETBACK, commentStyle, alphaBlend(_theme->comment()->background(), baseBack));
   send(SCI_STYLESETFORE, errorStyle, c2i(_theme->error()->foreground()));
-  send(SCI_STYLESETBACK, errorStyle, c2i(_theme->error()->background()));
+  send(SCI_STYLESETBACK, errorStyle, alphaBlend(_theme->error()->background(), baseBack));
   send(SCI_MARKERSETFORE, SC_MARK_CIRCLE, c2i(_theme->error()->background()));
   send(SCI_MARKERSETBACK, SC_MARK_CIRCLE, c2i(_theme->base()->background()));
 }
