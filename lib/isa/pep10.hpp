7a6a872b6e7c409d5ac81807066183447c8e8443
Matthew McRaven <matthew.mcraven@gmail.com>
2024-08-08 23:48:53 -0700

    refactor(isa): move logic target

--- lib/isa/pep10.hpp
+++ lib/isa/pep10.hpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2023 J. Stanley Warford, Matthew McRaven
- *
+ * Copyright (c) 2023-2024 J. Stanley Warford, Matthew McRaven
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -17,76 +16,69 @@
 
 #pragma once
 #include <QtCore>
-#include "isa_globals.hpp"
 
-namespace isa::detail::pep9 {
-Q_NAMESPACE_EXPORT(ISA_EXPORT)
+namespace isa::detail::pep10 {
+Q_NAMESPACE
 enum class Mnemonic {
-  STOP = 0x0,
   RET = 0x1,
-  RETTR = 0x2,
-  MOVSPA = 0x3,
-  MOVFLGA = 0x4,
-  MOVAFLG = 0x5,
+  SRET = 0x2,
+  MOVFLGA = 0x3,
+  MOVAFLG = 0x4,
+  MOVSPA = 0x5,
+  SWAPSPA = 0x6,
+  NOP = 0x7,
 
-  NOTA = 0x06,
-  NOTX = 0x07,
-  NEGA = 0x08,
-  NEGX = 0x09,
-  ASLA = 0x0A,
-  ASLX = 0x0B,
-  ASRA = 0x0C,
-  ASRX = 0x0D,
-  ROLA = 0x0E,
-  ROLX = 0x0F,
-  RORA = 0x10,
-  RORX = 0x11,
+  // FAULTS
+  UNIMPL,
 
-  BR = 0x12,
-  BRLE = 0x14,
-  BRLT = 0x16,
-  BREQ = 0x18,
-  BRNE = 0x1A,
-  BRGE = 0x1C,
-  BRGT = 0x1E,
-  BRV = 0x20,
-  BRC = 0x22,
-  CALL = 0x24,
-
-  // Trap opcodes
-  NOP0 = 0x26,
-  NOP1 = 0x27,
-  NOP = 0x28,
-  DECI = 0x30,
-  DECO = 0x38,
-  HEXO = 0x40,
-  STRO = 0x48,
-
-  ADDSP = 0x50,
-  SUBSP = 0x58,
-  ADDA = 0x60,
-  ADDX = 0x68,
-  SUBA = 0x70,
-  SUBX = 0x78,
-  ANDA = 0x80,
-  ANDX = 0x88,
-  ORA = 0x90,
-  ORX = 0x98,
-
-  CPWA = 0xA0,
-  CPWX = 0xA8,
-  CPBA = 0xB0,
-  CPBX = 0xB8,
-
-  LDWA = 0xC0,
-  LDWX = 0xC8,
-  LDBA = 0xD0,
-  LDBX = 0xD8,
-  STWA = 0xE0,
-  STWX = 0xE8,
-  STBA = 0xF0,
-  STBX = 0xF8,
-
+  NOTA = 0x18,
+  NOTX = 0x19,
+  NEGA = 0x1A,
+  NEGX = 0x1B,
+  ASLA = 0x1C,
+  ASLX = 0x1D,
+  ASRA = 0x1E,
+  ASRX = 0x1F,
+  ROLA = 0x20,
+  ROLX = 0x21,
+  RORA = 0x22,
+  RORX = 0x23,
+  // STOP,
+  BR = 0x24,
+  BRLE = 0x26,
+  BRLT = 0x28,
+  BREQ = 0x2A,
+  BRNE = 0x2C,
+  BRGE = 0x2E,
+  BRGT = 0x30,
+  BRV = 0x32,
+  BRC = 0x34,
+  CALL = 0x36,
+  SCALL = 0x38,
+  LDWA = 0x40,
+  LDWX = 0x48,
+  LDBA = 0x50,
+  LDBX = 0x58,
+  STWA = 0x60,
+  STWX = 0x68,
+  STBA = 0x70,
+  STBX = 0x78,
+  CPWA = 0x80,
+  CPWX = 0x88,
+  CPBA = 0x90,
+  CPBX = 0x98,
+  ADDA = 0xA0,
+  ADDX = 0xA8,
+  SUBA = 0xB0,
+  SUBX = 0xB8,
+  ANDA = 0xC0,
+  ANDX = 0xC8,
+  ORA = 0xD0,
+  ORX = 0xD8,
+  XORA = 0xE0,
+  XORX = 0xE8,
+  ADDSP = 0xF0,
+  SUBSP = 0xF8,
   INVALID,
 };
 Q_ENUM_NS(Mnemonic)
@@ -111,19 +103,17 @@ enum class InstructionType {
   U_none,   //?
   R_none,   //?
   A_ix,     //?
-  AAA_noi,  //?
-  AAA_stro, //? d, n, s, sf, x
   AAA_all,  //?
   AAA_i,    //?
   RAAA_all, //?
   RAAA_noi
 };
-struct ISA_EXPORT Instruction {
+struct Instruction {
   Mnemonic mnemon;
   InstructionType type;
   bool unary;
 };
-struct ISA_EXPORT Opcode {
+struct Opcode {
   Instruction instr;
   AddressingMode mode;
   bool valid;
@@ -140,7 +130,7 @@ constexpr std::array<Opcode, 256> initOpcodes() {
   };
   auto add_all = [&ret](Instruction i) {
     auto base = static_cast<quint8>(i.mnemon);
-    ret[base] = {.instr = i, .mode = AM::I, .valid = true};
+    ret[base] = {.instr = i, .mode = AM::I, .valid = i.type != T::RAAA_noi};
     ret[base + 1] = {.instr = i, .mode = AM::D, .valid = true};
     ret[base + 2] = {.instr = i, .mode = AM::N, .valid = true};
     ret[base + 3] = {.instr = i, .mode = AM::S, .valid = true};
@@ -150,17 +140,22 @@ constexpr std::array<Opcode, 256> initOpcodes() {
     ret[base + 7] = {.instr = i, .mode = AM::SFX, .valid = true};
   };
 
-  ret[(quint8)M::STOP] = {
-      .instr = {.mnemon = M::STOP, .type = T::U_none, .unary = true}, .mode = AM::NONE, .valid = true};
+  ret[0x00] = {.instr = {.mnemon = M::INVALID, .type = T::U_none, .unary = true}, .mode = AM::NONE, .valid = false};
   ret[(quint8)M::RET] = {.instr = {.mnemon = M::RET, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
-  ret[(quint8)M::RETTR] = {
-      .instr = {.mnemon = M::RETTR, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
-  ret[(quint8)M::MOVSPA] = {
-      .instr = {.mnemon = M::MOVSPA, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
+  ret[(quint8)M::SRET] = {.instr = {.mnemon = M::SRET, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
   ret[(quint8)M::MOVFLGA] = {
       .instr = {.mnemon = M::MOVFLGA, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
   ret[(quint8)M::MOVAFLG] = {
       .instr = {.mnemon = M::MOVAFLG, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
+  ret[(quint8)M::MOVSPA] = {
+      .instr = {.mnemon = M::MOVSPA, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
+  ret[(quint8)M::SWAPSPA] = {
+      .instr = {.mnemon = M::SWAPSPA, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
+  ret[(quint8)M::NOP] = {.instr = {.mnemon = M::NOP, .type = T::U_none, .unary = 1}, .mode = AM::NONE, .valid = true};
+
+  // Gap
+  for (int it = (int)M::NOP + 1; it < (int)M::NOTA; it++)
+    ret[it] = {.instr = {.mnemon = M::INVALID, .type = T::U_none, .unary = true}, .mode = AM::NONE, .valid = false};
 
   ret[(quint8)M::NOTA] = {.instr = {.mnemon = M::NOTA, .type = T::R_none, .unary = 1}, .mode = AM::NONE, .valid = true};
   ret[(quint8)M::NOTX] = {.instr = {.mnemon = M::NOTX, .type = T::R_none, .unary = 1}, .mode = AM::NONE, .valid = true};
@@ -185,36 +180,7 @@ constexpr std::array<Opcode, 256> initOpcodes() {
   add_ix({.mnemon = M::BRV, .type = T::A_ix, .unary = 0});
   add_ix({.mnemon = M::BRC, .type = T::A_ix, .unary = 0});
   add_ix({.mnemon = M::CALL, .type = T::A_ix, .unary = 0});
-  // Add unary traps
-  ret[(quint8)M::NOP0] = {.instr = {.mnemon = M::NOP0, .type = T::R_none, .unary = 1}, .mode = AM::NONE, .valid = true};
-  ret[(quint8)M::NOP1] = {.instr = {.mnemon = M::NOP1, .type = T::R_none, .unary = 1}, .mode = AM::NONE, .valid = true};
-  // Add non-unary traps, and patch up the non-unary traps' addressing modes.
-  add_all({.mnemon = M::NOP, .type = T::AAA_all, .unary = 1});
-  add_all({.mnemon = M::DECI, .type = T::AAA_noi, .unary = 1});
-  ret[(quint8)M::DECI].valid = false;
-  add_all({.mnemon = M::DECO, .type = T::AAA_all, .unary = 1});
-  add_all({.mnemon = M::HEXO, .type = T::AAA_all, .unary = 1});
-  add_all({.mnemon = M::STRO, .type = T::AAA_stro, .unary = 1});
-  ret[(quint8)M::DECI + 0].valid = false;
-  ret[(quint8)M::DECI + 6].valid = false;
-  ret[(quint8)M::DECI + 7].valid = false;
-
-  add_all({.mnemon = M::ADDSP, .type = T::AAA_all, .unary = 0});
-  add_all({.mnemon = M::SUBSP, .type = T::AAA_all, .unary = 0});
-
-  add_all({.mnemon = M::ADDA, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::ADDX, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::SUBA, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::SUBX, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::ANDA, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::ANDX, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::ORA, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::ORX, .type = T::RAAA_all, .unary = 0});
-
-  add_all({.mnemon = M::CPWA, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::CPWX, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::CPBA, .type = T::RAAA_all, .unary = 0});
-  add_all({.mnemon = M::CPBX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::SCALL, .type = T::AAA_all, .unary = 1});
 
   add_all({.mnemon = M::LDWA, .type = T::RAAA_all, .unary = 0});
   add_all({.mnemon = M::LDWX, .type = T::RAAA_all, .unary = 0});
@@ -224,7 +190,22 @@ constexpr std::array<Opcode, 256> initOpcodes() {
   add_all({.mnemon = M::STWX, .type = T::RAAA_noi, .unary = 0});
   add_all({.mnemon = M::STBA, .type = T::RAAA_noi, .unary = 0});
   add_all({.mnemon = M::STBX, .type = T::RAAA_noi, .unary = 0});
-
+  add_all({.mnemon = M::CPWA, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::CPWX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::CPBA, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::CPBX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::ADDA, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::ADDX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::SUBA, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::SUBX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::ANDA, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::ANDX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::ORA, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::ORX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::XORA, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::XORX, .type = T::RAAA_all, .unary = 0});
+  add_all({.mnemon = M::ADDSP, .type = T::AAA_all, .unary = 0});
+  add_all({.mnemon = M::SUBSP, .type = T::AAA_all, .unary = 0});
   return ret;
 };
 
@@ -241,28 +222,28 @@ Q_ENUM_NS(Register);
 enum class CSR : quint8 { N, Z, V, C };
 Q_ENUM_NS(CSR);
 
-// TODO: Fill in from CS5E
 enum class MemoryVectors : quint16 {
-  UserStackPtr = 0xDEAD,
-  SystemStackPtr = 0xDEAD,
-  CharIn = 0xDEAD,
-  CharOut = 0xDEAD,
-  LOADER = 0xDEAD,
-  TrapHandler = 0xDEAD,
+  TrapHandler = 0xFFF6,
+  Dispatcher = 0xFFF8,
+  SystemStackPtr = 0xFFFA,
+  DiskIn = 0xFFFC,
+  CharIn = 0xFFFD,
+  CharOut = 0xFFFE,
+  PwrOff = 0xFFFF,
 };
 Q_ENUM_NS(MemoryVectors)
-} // namespace isa::detail::pep9
+} // namespace isa::detail::pep10
 
 namespace isa {
-struct ISA_EXPORT Pep9 {
-  using Mnemonic = detail::pep9::Mnemonic;
-  using AddressingMode = detail::pep9::AddressingMode;
-  using InstructionType = detail::pep9::InstructionType;
-  using Instruction = detail::pep9::Instruction;
-  using Opcode = detail::pep9::Opcode;
-  using Register = detail::pep9::Register;
-  using CSR = detail::pep9::CSR;
-  using MemoryVectors = detail::pep9::MemoryVectors;
+struct Pep10 {
+  using Mnemonic = detail::pep10::Mnemonic;
+  using AddressingMode = detail::pep10::AddressingMode;
+  using InstructionType = detail::pep10::InstructionType;
+  using Instruction = detail::pep10::Instruction;
+  using Opcode = detail::pep10::Opcode;
+  using Register = detail::pep10::Register;
+  using CSR = detail::pep10::CSR;
+  using MemoryVectors = detail::pep10::MemoryVectors;
   static constexpr quint8 RegisterCount = 7;
   static constexpr quint8 CSRCount = 4;
 
@@ -282,6 +263,11 @@ struct ISA_EXPORT Pep9 {
   static bool isOpcodeUnary(quint8 opcode);
   static bool isStore(Mnemonic mnemonic);
   static bool isStore(quint8 opcode);
+  static quint8 operandBytes(Mnemonic mnemonic);
+  static quint8 operandBytes(quint8 opcode);
+  // SCALL and CALL
+  static bool isCall(Mnemonic mnemonic);
+  static bool isCall(quint8 opcode);
 
   static bool isUType(Mnemonic mnemonic);
   static bool isRType(Mnemonic mnemonic);
@@ -295,7 +281,7 @@ struct ISA_EXPORT Pep9 {
 
   static bool requiresAddressingMode(Mnemonic mnemonic);
   static bool canElideAddressingMode(Mnemonic mnemonic, AddressingMode addr);
-  constexpr static std::array<Opcode, 256> opcodeLUT = detail::pep9::initOpcodes();
+  constexpr static std::array<Opcode, 256> opcodeLUT = detail::pep10::initOpcodes();
   static QSet<QString> legalDirectives();
   static bool isLegalDirective(QString directive);
 };
