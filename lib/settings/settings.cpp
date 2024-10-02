143a41964cc0f2ce90f23ea70b30579d3a55b9e0
Matthew McRaven <matthew.mcraven@gmail.com>
2024-10-01 20:45:41 -0700

    chore(settings): add "Editor", "KeyMap" categories

--- lib/settings/settings.cpp
+++ lib/settings/settings.cpp
@@ -4,10 +4,14 @@
 pepp::settings::Category::Category(QObject *parent) : QObject(parent) {}
 pepp::settings::GeneralCategory::GeneralCategory(QObject *parent) : Category(parent) {}
 pepp::settings::ThemeCategory::ThemeCategory(QObject *parent) : Category(parent) {}
+pepp::settings::EditorCategory::EditorCategory(QObject *parent) : Category(parent) {}
+pepp::settings::KeyMapCategory::KeyMapCategory(QObject *parent) : Category(parent) {}
 
 pepp::settings::AppSettings::AppSettings(QObject *parent) : QObject{parent} {
   _categories.append(_general = new GeneralCategory(this));
   _categories.append(_theme = new ThemeCategory(this));
+  _categories.append(_editor = new EditorCategory(this));
+  _categories.append(_keymap = new KeyMapCategory(this));
 }
 
 void pepp::settings::AppSettings::sync() {
