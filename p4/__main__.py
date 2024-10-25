abfc5ed426c4b0b2c79e21c87b0d6a2b804fb574
Matthew McRaven <mkm302@georgetown.edu>
2024-10-25 12:39:23 -0700

    Move test script into package main

--- p4/__main__.py
+++ p4/__main__.py
@@ -1,22 +1,29 @@
 import click
 
 import p4.vocabs.boot, p4.dictionary, p4.vocabs.debug
-import test.utils
-words = p4.vocabs.boot.ALL + p4.vocabs.debug.ALL
+import test.utils as tutils
 
-@click.command()
+@click.group()
+@click.pass_context
+def cli(ctx):
+    ctx.ensure_object(dict)
+
+
+
+@cli.command
 @click.option("--vocabs", "-v", multiple=True)
 @click.option("--preloads", "-p", multiple=True)
 @click.option("--bootstrap/--no-bootstrap", default=True)
 @click.option("--debug/--no-debug", default=False)
-def cli(vocabs, preloads, bootstrap, debug):
+@click.pass_context
+def run(ctx, vocabs, preloads, bootstrap, debug):
     words = []
     for vocab in vocabs:
         match vocab.lower():
             case "boot": words.extend(p4.vocabs.boot.ALL)
             case "debug": words.extend(p4.vocabs.debug.ALL)
 
-    vm = test.utils.create_vm(words)
+    vm = tutils.create_vm(words)
     if bootstrap:
         #if "p4/bootstrap/control.f" not in preloads: preloads = ("p4/bootstrap/control.f", *preloads)
         if "p4/bootstrap/dummy.f" not in preloads: preloads = ("p4/bootstrap/dummy.f", *preloads)
@@ -25,11 +32,21 @@ def cli(vocabs, preloads, bootstrap, debug):
         click.echo(f"Preloading {preload}")
         with open(preload) as x: contents.extend(x.readlines())
     # Can only buffer input text once; old contents are cleared.
-    vm.io.buffer_text("\n".join(contents))
-    test.utils.next(vm, "INTERP")
+    vm.io.buffer_text("\n".join(contents)+"\n")
+    tutils.next(vm, "INTERP")
     vm.debug = debug
     # TODO: run until buffer text is empty, then enable debug mode.
     vm.run()
+
+@cli.command
+def test():
+    import itertools, unittest
+
+    loader = unittest.TestLoader()
+    tests = loader.discover("test", "test*.py", top_level_dir="../")
+    testRunner = unittest.runner.TextTestRunner()
+    testRunner.run(tests)
+
 
 if __name__ == "__main__":
     cli()
--- test.py
+++
@@ -1,7 +0,0 @@
-import itertools
-import unittest
-
-loader = unittest.TestLoader()
-tests = loader.discover("test", "test*.py", top_level_dir=".")
-testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
