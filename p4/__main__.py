4308f5c7293414602fdc4b4a6186f19dd5b1b4ea
Matthew McRaven <mkm302@georgetown.edu>
2024-10-25 14:47:45 -0700

    Localize imports

--- p4/__main__.py
+++ p4/__main__.py
@@ -1,8 +1,5 @@
 import click
 
-import p4.vocabs.boot, p4.dictionary, p4.vocabs.debug
-import test.utils as tutils
-
 @click.group()
 @click.pass_context
 def cli(ctx):
@@ -15,8 +12,10 @@ def cli(ctx):
 @click.option("--preloads", "-p", multiple=True)
 @click.option("--bootstrap/--no-bootstrap", default=True)
 @click.option("--debug/--no-debug", default=False)
-@click.pass_context
-def run(ctx, vocabs, preloads, bootstrap, debug):
+def run(vocabs, preloads, bootstrap, debug):
+    import p4.vocabs.boot, p4.dictionary, p4.vocabs.debug
+    import test.utils as tutils
+
     words = []
     for vocab in vocabs:
         match vocab.lower():
@@ -40,8 +39,7 @@ def run(ctx, vocabs, preloads, bootstrap, debug):
 
 @cli.command
 def test():
-    import itertools, unittest
-
+    import unittest
     loader = unittest.TestLoader()
     tests = loader.discover("test", "test*.py", top_level_dir="../")
     testRunner = unittest.runner.TextTestRunner()
