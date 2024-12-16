14ad5f51a040908d8d84fad5fffe9336bcc844a7
Matthew McRaven <matthew.mcraven@gmail.com>
2024-12-15 22:36:25 -0800

    feat(perf): add placeholders to iterate repo's commits

--- test/perf/__main__.py
+++ test/perf/__main__.py
@@ -1,6 +1,8 @@
 import click
-import statistics as stats
+import statistics as stats, pathlib
 from . import CMake, Term
+from pygit2 import Repository
+from pygit2.enums import SortMode
 
 @click.group()
 def cli(): pass
@@ -16,5 +18,12 @@ def run_once():
     term = Term("/Volumes/RAMDisk/Build/output/pepp-term")
     vals = [x for x in map(lambda _: term.mit(), range(10))]
     print(f"{stats.mean(vals)}±{round(stats.stdev(vals),0)}")
+
+@cli.command("do-commits")
+def run():
+    path = pathlib.Path(__file__).resolve().parent/"../../.git"
+    repo = Repository(path.resolve())
+    for commit in repo.walk(repo.head.target, SortMode.TOPOLOGICAL | SortMode.REVERSE):
+        print(commit.message.split("\n")[0])
 if __name__ == '__main__':
     cli()
