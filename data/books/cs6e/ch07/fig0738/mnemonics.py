e4654dc6fc86d8e560fa80564e7977eb3ccd1c80
Matthew McRaven <matthew.mcraven@gmail.com>
2025-02-03 22:53:04 -0800

    fix(bi): fix arity check for mnemonics

--- data/books/cs6e/ch07/fig0738/mnemonics.py
+++ data/books/cs6e/ch07/fig0738/mnemonics.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class Mnemonic:
-    def __init__(self, is_unary: bool | None):
+    def __init__(self, is_unary: bool):
         self.is_unary = is_unary
 
 
@@ -11,17 +11,17 @@ class Mnemonics(Enum):
     SUB = Mnemonic(False)
     MUL = Mnemonic(False)
     DIV = Mnemonic(False)
-    NEG = Mnemonic(True)
-    ABS = Mnemonic(True)
+    NEG = Mnemonic(False)
+    ABS = Mnemonic(False)
     SET = Mnemonic(False)
-    STOP = Mnemonic(None)
-    END = Mnemonic(None)
+    STOP = Mnemonic(True)
+    END = Mnemonic(True)
 
     def __str__(self):
         return f"{self.name.lower()}"
 
     def is_unary(self):
-        return self.value.is_unary == True
+        return self.value.is_unary
 
     def is_nonunary(self):
-        return self.value.is_unary == False
+        return not self.value.is_unary
--- data/books/cs6e/ch07/fig0738/test_mnemon.py
+++ data/books/cs6e/ch07/fig0738/test_mnemon.py
@@ -17,11 +17,11 @@ def test_is_unary():
     assert not Mnemonics.SUB.is_unary()
     assert not Mnemonics.MUL.is_unary()
     assert not Mnemonics.DIV.is_unary()
-    assert Mnemonics.NEG.is_unary()
-    assert Mnemonics.ABS.is_unary()
+    assert not Mnemonics.NEG.is_unary()
+    assert not Mnemonics.ABS.is_unary()
     assert not Mnemonics.SET.is_unary()
-    assert not Mnemonics.STOP.is_unary()
-    assert not Mnemonics.END.is_unary()
+    assert Mnemonics.STOP.is_unary()
+    assert Mnemonics.END.is_unary()
 
 
 def test_is_nonunary():
@@ -29,8 +29,8 @@ def test_is_nonunary():
     assert Mnemonics.SUB.is_nonunary()
     assert Mnemonics.MUL.is_nonunary()
     assert Mnemonics.DIV.is_nonunary()
-    assert not Mnemonics.NEG.is_nonunary()
-    assert not Mnemonics.ABS.is_nonunary()
+    assert Mnemonics.NEG.is_nonunary()
+    assert Mnemonics.ABS.is_nonunary()
     assert Mnemonics.SET.is_nonunary()
     assert not Mnemonics.STOP.is_nonunary()
     assert not Mnemonics.END.is_nonunary()
