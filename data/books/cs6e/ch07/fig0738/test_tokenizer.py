c293a1b6c553948f4722a53836ae70a66797777d
Matthew McRaven <matthew.mcraven@gmail.com>
2025-02-03 21:02:37 -0800

    feat(bi): add tokenizer
    I'm still not happy with how "pythonic" the tokenizer is.
    
    I feel like the tokenizer should be an iterator/generator implementing next() rather than having an explicit next_token().
    Also, I feel I need the ability to peek the stream, and some refactoring to indicate we've reached the end of iteration.

---
+++ data/books/cs6e/ch07/fig0738/test_tokenizer.py
@@ -0,0 +1,54 @@
+from io import StringIO
+
+from .tokenizer import Tokenizer
+from .tokens import Tokens
+
+
+def test_tokenizer_empty():
+    tk = Tokenizer(StringIO("   \n  "))
+    assert tk.next_token() == (Tokens.EMPTY, None)
+    assert tk.next_token() == (Tokens.EMPTY, None)
+
+
+def test_tokenizer_comma():
+    tk = Tokenizer(StringIO("   \n  ,  "))
+    # TODO: I think this is a bug, and should not be commented out
+    # assert tk.next_token() == (Tokens.EMPTY, None)
+    assert tk.next_token() == (Tokens.COMMA, None)
+    assert tk.next_token() == (Tokens.EMPTY, None)
+
+
+def test_tokenizer_identifier():
+    tk = Tokenizer(StringIO(" words are hard "))
+    assert tk.next_token() == (Tokens.IDENTIFIER, "words")
+    assert tk.next_token() == (Tokens.IDENTIFIER, "are")
+    assert tk.next_token() == (Tokens.IDENTIFIER, "hard")
+
+
+def test_tokenizer_integer():
+    tk = Tokenizer(StringIO("-10 10 +10 -0 +0 "))
+    assert tk.next_token() == (Tokens.INTEGER, -10)
+    assert tk.next_token() == (Tokens.INTEGER, 10)
+    assert tk.next_token() == (Tokens.INTEGER, 10)
+    assert tk.next_token() == (Tokens.INTEGER, 0)
+    assert tk.next_token() == (Tokens.INTEGER, 0)
+
+
+def test_tokenizer_invalid():
+    tk = Tokenizer(StringIO("-a ! ; "))
+    assert tk.next_token() == (Tokens.INVALID, None)
+    assert tk.next_token() == (Tokens.INVALID, None)
+    assert tk.next_token() == (Tokens.INVALID, None)
+    assert tk.next_token() == (Tokens.EMPTY, None)
+
+
+def test_tokenizer_left_paren():
+    tk = Tokenizer(StringIO("( "))
+    assert tk.next_token() == (Tokens.LEFT_PAREN, None)
+    assert tk.next_token() == (Tokens.EMPTY, None)
+
+
+def test_tokenizer_right_paren():
+    tk = Tokenizer(StringIO(") "))
+    assert tk.next_token() == (Tokens.RIGHT_PAREN, None)
+    assert tk.next_token() == (Tokens.EMPTY, None)
---
+++ data/books/cs6e/ch07/fig0738/tokenizer.py
@@ -0,0 +1,78 @@
+import os
+from enum import Enum
+from io import StringIO
+from typing import Tuple, Literal, Union, List
+
+from .tokens import Tokens
+
+
+class Tokenizer:
+    class States(Enum):
+        START = 1
+        IDENT = 2
+        SIGN = 3
+        INTEGER = 4
+        STOP = 5
+
+    def __init__(self, buffer: StringIO):
+        self.buffer = buffer
+        self.literals_to_tokens = {
+            ",": Tokens.COMMA,
+            "(": Tokens.LEFT_PAREN,
+            ")": Tokens.RIGHT_PAREN,
+            "\n": Tokens.EMPTY,
+            " ": Tokens.EMPTY,
+        }
+
+    def next_token(
+        self,
+    ) -> Union[
+        Tuple[Literal[Tokens.INTEGER], int]
+        | Tuple[Literal[Tokens.IDENTIFIER], str]
+        | Tuple[Tokens, None]
+    ]:
+
+        as_str_list: List[str] = []
+        as_int: int = 0
+        sign: Literal[-1, 1] = 1
+        state: Tokenizer.States = Tokenizer.States.START
+        token: Tuple[Tokens, int | str | None] = (Tokens.EMPTY, None)
+
+        while state != Tokenizer.States.STOP and token[0] != Tokens.INVALID:
+            prev_pos = self.buffer.tell()
+            ch: str = self.buffer.read(1)
+            match state:
+                case _ if len(ch) == 0:
+                    state = Tokenizer.States.STOP
+                case Tokenizer.States.START if ch.isspace():
+                    pass
+                case Tokenizer.States.START if ch.isalpha():
+                    as_str_list.append(ch)
+                    state = Tokenizer.States.IDENT
+                case Tokenizer.States.START if ch == "-" or ch == "+":
+                    sign = -1 if ch == "-" else 1
+                    state = Tokenizer.States.SIGN
+                case Tokenizer.States.START if ch.isdigit():
+                    as_int = ord(ch) - ord("0")
+                    state = Tokenizer.States.INTEGER
+                case Tokenizer.States.START if ch in self.literals_to_tokens:
+                    token = (self.literals_to_tokens[ch], None)
+                    state = Tokenizer.States.STOP
+                case Tokenizer.States.IDENT if ch.isalnum():
+                    as_str_list.append(ch)
+                case Tokenizer.States.IDENT:
+                    token = (Tokens.IDENTIFIER, "".join(as_str_list))
+                    self.buffer.seek(prev_pos, os.SEEK_SET)
+                    state = Tokenizer.States.STOP
+                case Tokenizer.States.SIGN if ch.isdigit():
+                    as_int = ord(ch) - ord("0")
+                    state = Tokenizer.States.INTEGER
+                case Tokenizer.States.INTEGER if ch.isdigit():
+                    as_int = 10 * as_int + (ord(ch) - ord("0"))
+                case Tokenizer.States.INTEGER:
+                    token = (Tokens.INTEGER, sign * as_int)
+                    self.buffer.seek(prev_pos, os.SEEK_SET)
+                    state = Tokenizer.States.STOP
+                case _:
+                    token = (Tokens.INVALID, None)
+        return token
