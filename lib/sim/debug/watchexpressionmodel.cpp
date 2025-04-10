f2064a1b840485174bd25c979d608a141e6819a7
Matthew McRaven <matthew.mcraven@gmail.com>
2025-04-10 09:09:46 -0700

    fix(sim): format displayed values using correct types
    I was always defaulting to uint64_t, which was definitely wrong for small, negative numbers.

--- lib/sim/debug/watchexpressionmodel.cpp
+++ lib/sim/debug/watchexpressionmodel.cpp
@@ -2,8 +2,8 @@
 
 pepp::debug::WatchExpressionModel::WatchExpressionModel(QObject *parent) : QObject(parent) {
   pepp::debug::Parser p(_c);
-  add_root(p.compile("x + 2"));
-  add_root(p.compile("3 * (x + 2)"));
+  add_root(p.compile("$x - 3"));
+  add_root(p.compile("3_u16 * (x + 2)"));
   add_root(p.compile("y + 1 ==  m * x + b"));
 }
 
@@ -40,6 +40,16 @@ int pepp::debug::WatchExpressionTableModel::columnCount(const QModelIndex &paren
   return 3;
 }
 
+QVariant variant_from_bits(const pepp::debug::TypedBits &bits) {
+  switch (bits.type) {
+  case pepp::debug::ExpressionType::i8: return QVariant::fromValue((int8_t)bits.bits);
+  case pepp::debug::ExpressionType::u8: return QVariant::fromValue((uint8_t)bits.bits);
+  case pepp::debug::ExpressionType::i16: return QVariant::fromValue((int16_t)bits.bits);
+  case pepp::debug::ExpressionType::u16: return QVariant::fromValue((uint16_t)bits.bits);
+  case pepp::debug::ExpressionType::i32: return QVariant::fromValue((int32_t)bits.bits);
+  case pepp::debug::ExpressionType::u32: return QVariant::fromValue((uint32_t)bits.bits);
+  }
+}
 QVariant pepp::debug::WatchExpressionTableModel::data(const QModelIndex &index, int role) const {
   if (!index.isValid() || _expressionModel == nullptr) return QVariant();
   int row = index.row(), col = index.column();
@@ -48,7 +58,7 @@ QVariant pepp::debug::WatchExpressionTableModel::data(const QModelIndex &index, 
   switch (role) {
   case Qt::DisplayRole:
     if (col == 0) return terms[row]->to_string();
-    else if (col == 1) return QVariant((qulonglong)terms[row]->evaluate(CachePolicy::UseAlways, env).bits);
+    else if (col == 1) return variant_from_bits(terms[row]->evaluate(CachePolicy::UseAlways, env));
     else {
       QMetaEnum metaEnum = QMetaEnum::fromType<ExpressionType>();
       return metaEnum.valueToKey((int)terms[row]->evaluate(CachePolicy::UseAlways, env).type);
