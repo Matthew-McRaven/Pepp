7d9b7a437fef16e0b159e7e77c624fc91d3cb7b7
Matthew McRaven <matthew.mcraven@gmail.com>
2025-04-16 19:15:46 -0700

    feat(sim): update breakpoint viewer on add / remove
    Change fundamental data structure of BPs from set to vector.
    It will be easier to iterate / assign rows to items in vector than set.
    The vector is required to be sorted (like the set), so lookups are still lg(n), at the cost of nlg(n) inserts/deletes.
    
    All values except address are wrong.
    however, it reacts to breakpoint inserts / deletes.

--- lib/sim/debug/debugger.cpp
+++ lib/sim/debug/debugger.cpp
@@ -4,34 +4,44 @@
 pepp::debug::BreakpointSet::BreakpointSet() : QObject(nullptr) { _bitmask.reset(); }
 
 void pepp::debug::BreakpointSet::addBP(quint16 address) {
-  _breakpoints.insert(address);
+  if (hasBP(address)) return;
+  _breakpoints.emplace_back(address);
+  std::sort(_breakpoints.begin(), _breakpoints.end());
   _bitmask.set(address / 8);
+  emit breakpointAdded(address);
 }
 
 void pepp::debug::BreakpointSet::removeBP(quint16 address) {
-  _breakpoints.erase(address);
+  if (!hasBP(address)) return;
+  _breakpoints.erase(std::remove(_breakpoints.begin(), _breakpoints.end(), address), _breakpoints.end());
   // Determine boundary addresses of 8-byte chunk.
   auto lower_address = address & 0xff'f8, upper_address = address | 0x8;
   // find first address >= lower_address
-  auto maybe_lower = _breakpoints.lower_bound(lower_address);
+  auto maybe_lower = std::lower_bound(_breakpoints.cbegin(), _breakpoints.cend(), lower_address);
   // If lower bound is above upper_address, we remove the last breakpoint for this chunk.
   if (maybe_lower == _breakpoints.end() || *maybe_lower > upper_address) _bitmask.reset(address / 8);
+  emit breakpointRemoved(address);
 }
 
 bool pepp::debug::BreakpointSet::hasBP(quint16 address) const {
   // Use _bitmask to determine if any nearby addresses has a breakpoint.
   // Traverse tree/set/map only if we need to refine the search.
   if (!_bitmask[address / 8]) return false;
-  return _breakpoints.contains(address);
+  return std::binary_search(_breakpoints.cbegin(), _breakpoints.cend(), address);
 }
 
-void pepp::debug::BreakpointSet::clearBPs() { _breakpoints.clear(); }
+void pepp::debug::BreakpointSet::clearBPs() {
+  emit breakpointsCleared();
+  _breakpoints.clear();
+}
 
 void pepp::debug::BreakpointSet::notifyPCChanged(quint16 newValue) {
   if (hasBP(newValue)) _hit = true;
 }
 
 std::size_t pepp::debug::BreakpointSet::count() const { return _breakpoints.size(); }
+
+std::span<quint16> pepp::debug::BreakpointSet::breakpoints() { return _breakpoints; }
 
 bool pepp::debug::BreakpointSet::hit() const { return _hit; }
 
@@ -40,8 +50,9 @@ void pepp::debug::BreakpointSet::clearHit() { _hit = false; }
 pepp::debug::BreakpointTableModel::BreakpointTableModel(QObject *parent) {}
 
 namespace {
-const char *col_names[] = {"Address", "File", "Line", "Condition", "Value"};
-}
+const int col_length = 5;
+const char *col_names[col_length] = {"Address", "File", "Line", "Condition", "Value"};
+} // namespace
 QVariant pepp::debug::BreakpointTableModel::headerData(int section, Qt::Orientation orientation, int role) const {
   switch (role) {
   case Qt::DisplayRole: return col_names[section];
@@ -54,23 +65,56 @@ int pepp::debug::BreakpointTableModel::rowCount(const QModelIndex &parent) const
   return _breakpoints->count();
 }
 
-int pepp::debug::BreakpointTableModel::columnCount(const QModelIndex &parent) const { return 5; }
+int pepp::debug::BreakpointTableModel::columnCount(const QModelIndex &parent) const { return col_length; }
 
 QVariant pepp::debug::BreakpointTableModel::data(const QModelIndex &index, int role) const {
   using namespace Qt::StringLiterals;
+  if (!index.isValid() || _breakpoints == nullptr) return {};
+  auto span = _breakpoints->breakpoints();
+  switch (role) {
+  case Qt::DisplayRole:
+    if (index.column() == 0) {
+      return QString::asprintf("0x%04x", span[index.row()]);
+    } else if (index.column() == 1) {
+      return "User";
+    } else if (index.column() == 2) {
+      return index.row();
+    } else if (index.column() == 3) {
+      return "Yelp";
+    } else if (index.column() == 4) {
+      return "0";
+    }
+    break;
+  }
+
   return u"%1"_s.arg(index.row());
 }
 
-pepp::debug::BreakpointSet *pepp::debug::BreakpointTableModel::breakpoints() {
+pepp::debug::BreakpointSet *pepp::debug::BreakpointTableModel::breakpointModel() {
   if (_breakpoints == nullptr) return nullptr;
   return _breakpoints;
 }
 
-void pepp::debug::BreakpointTableModel::setBreakpoints(BreakpointSet *breakpoints) {
+void pepp::debug::BreakpointTableModel::setBreakpointModel(BreakpointSet *breakpoints) {
   if (_breakpoints == breakpoints) return;
   beginResetModel();
+  if (_breakpoints) {
+    disconnect(_breakpoints, &BreakpointSet::breakpointAdded, this, &BreakpointTableModel::onBreakpointsChanged);
+    disconnect(_breakpoints, &BreakpointSet::breakpointRemoved, this, &BreakpointTableModel::onBreakpointsChanged);
+    disconnect(_breakpoints, &BreakpointSet::breakpointsCleared, this, &BreakpointTableModel::onBreakpointsChanged);
+  }
   _breakpoints = breakpoints;
-  emit breakpointsChanged();
+  if (_breakpoints) {
+    connect(_breakpoints, &BreakpointSet::breakpointAdded, this, &BreakpointTableModel::onBreakpointsChanged);
+    connect(_breakpoints, &BreakpointSet::breakpointRemoved, this, &BreakpointTableModel::onBreakpointsChanged);
+    connect(_breakpoints, &BreakpointSet::breakpointsCleared, this, &BreakpointTableModel::onBreakpointsChanged);
+  }
+  emit breakpointModelChanged();
+  endResetModel();
+}
+
+void pepp::debug::BreakpointTableModel::onBreakpointsChanged() {
+  beginResetModel();
   endResetModel();
 }
 
--- lib/sim/debug/debugger.hpp
+++ lib/sim/debug/debugger.hpp
@@ -5,6 +5,7 @@
 
 namespace pepp::debug {
 class BreakpointSet : public QObject {
+  Q_OBJECT
 public:
   explicit BreakpointSet();
   void addBP(quint16 address);
@@ -13,23 +14,29 @@ public:
   void clearBPs();
   void notifyPCChanged(quint16 newValue);
   std::size_t count() const;
+  std::span<quint16> breakpoints();
 
   bool hit() const;
   void clearHit();
+signals:
+  void breakpointAdded(quint16 address);
+  void breakpointRemoved(quint16 address);
+  void breakpointsCleared();
 
 private:
   // Used to enable constant-time detection of the common case (no breakpoints).
   // Packs an 8-byte interval into each bit, and a 64-byte per interval per byte.
   // Should consume ~1024 bytes of memory total for Pepp procesors.
   std::bitset<0x1'00'00 / 8> _bitmask;
-  std::set<quint16> _breakpoints;
+  std::vector<quint16> _breakpoints;
   bool _hit = false;
 };
 
 class BreakpointTableModel : public QAbstractTableModel {
   Q_OBJECT
   QML_ELEMENT
-  Q_PROPERTY(pepp::debug::BreakpointSet *breakpoints READ breakpoints WRITE setBreakpoints NOTIFY breakpointsChanged)
+  Q_PROPERTY(pepp::debug::BreakpointSet *breakpointModel READ breakpointModel WRITE setBreakpointModel NOTIFY
+                 breakpointModelChanged)
 public:
   explicit BreakpointTableModel(QObject *parent = nullptr);
   QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
@@ -37,11 +44,13 @@ public:
   int columnCount(const QModelIndex &parent) const override;
   QVariant data(const QModelIndex &index, int role) const override;
 
-  BreakpointSet *breakpoints();
-  void setBreakpoints(BreakpointSet *breakpoints);
+  BreakpointSet *breakpointModel();
+  void setBreakpointModel(BreakpointSet *breakpoints);
 
+public slots:
+  void onBreakpointsChanged();
 signals:
-  void breakpointsChanged();
+  void breakpointModelChanged();
 
 private:
   BreakpointSet *_breakpoints = nullptr;
