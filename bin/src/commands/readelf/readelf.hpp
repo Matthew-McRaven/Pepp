f3184b3a3c5485f8aa1c46abe69824d13255be69
Matthew McRaven <matthew.mcraven@gmail.com>
2025-04-19 15:12:53 -0700

    feat(bin): add ability to dump key ELF fields
    Need to play with some formatting & add pepp-specific info,
    Things like memory vectors, MMIO notes.

--- bin/src/commands/readelf/readelf.hpp
+++ bin/src/commands/readelf/readelf.hpp
@@ -15,19 +15,56 @@
  */
 
 #pragma once
+#include <CLI11.hpp>
 #include "../../shared.hpp"
 #include "../../task.hpp"
 
+namespace ELFIO {
+class elfio;
+}
+
 class ReadElfTask : public Task {
 public:
-  ReadElfTask(QObject *parent = nullptr);
+  struct Options {
+    bool file_header = false;
+    bool program_headers = false;
+    bool section_headers = false;
+    bool symbols = false;
+    bool notes = false;
+    std::string elffile;
+  };
+  ReadElfTask(Options &opts, QObject *parent = nullptr);
   void run() override;
+
+private:
+  void fileHeader(ELFIO::elfio &) const;
+  void programHeaders(ELFIO::elfio &) const;
+  void sectionHeaders(ELFIO::elfio &) const;
+  void symbols(ELFIO::elfio &) const;
+  void notes(ELFIO::elfio &) const;
+  Options &_opts;
 };
 
 void registerReadelf(auto &app, task_factory_t &task, detail::SharedFlags &flags) {
+  static ReadElfTask::Options opts;
   static auto readelf = app.add_subcommand("readelf", "Display information about ELF files");
+  readelf->set_help_flag("-H,--help");
+  static auto file_header = readelf->add_flag("-h,--file-header", opts.file_header, "Display the ELF file header");
+  static auto program_headers =
+      readelf->add_flag("-l,--program-headers", opts.program_headers, "Display the program headers");
+  static auto section_headers =
+      readelf->add_flag("-S,--section-headers", opts.section_headers, "Display the section headers");
+  static auto symbols = readelf->add_flag("-s,--symbols", opts.symbols, "Display the symbol tables");
+  static auto notes = readelf->add_flag("-n,--notes", opts.notes, "Display the notes");
+  static auto all = readelf->add_flag("-a,--all", " Equivalent to specifying --file-header, --program-headers,\
+--sections, --symbols, --relocs, --dynamic, --notes,\
+--version-info, --arch-specific, --unwind, --section-groups.");
+  static auto file = readelf->add_option("elffile", opts.elffile, "Elf file")->expected(1)->required(true);
   readelf->callback([&]() {
+    if (*all) {
+      opts.file_header = opts.program_headers = opts.section_headers = opts.symbols = opts.notes = true;
+    }
     flags.kind = detail::SharedFlags::Kind::TERM;
-    task = [&](QObject *parent) { return new ReadElfTask(parent); };
+    task = [&](QObject *parent) { return new ReadElfTask(opts, parent); };
   });
 }
