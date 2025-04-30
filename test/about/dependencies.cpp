2fb339b88cc7ee2477b4b179a632cc7b7c1747ef
Matthew McRaven <matthew.mcraven@gmail.com>
2025-04-30 08:07:02 -0700

    feat(bits): add LLVM's code for handling LEB128 values
    Copied (with attribution) into my bits library.
    Needed to encoding/decoding of values for the DWARF standard.

--- test/about/dependencies.cpp
+++ test/about/dependencies.cpp
@@ -19,6 +19,6 @@
 
 TEST_CASE("About Dependencies", "[scope:help.about][kind:unit][arch:*]") {
   auto deps = about::dependencies();
-  CHECK(deps.length() == 17);
+  CHECK(deps.length() == 18);
   for (const auto &dep : deps) CHECK(dep.licenseText.size() != 0);
 };
