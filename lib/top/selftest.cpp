46bfefd7a08a91f4fd38e4d017235938a6e0317d
Matthew McRaven <matthew.mcraven@gmail.com>
2025-05-21 21:18:43 -0700

    feat: enumerate test cases in self test
    Still need to format, add some run functionality, and add filtering.

--- lib/top/selftest.cpp
+++ lib/top/selftest.cpp
@@ -1,4 +1,5 @@
 #include "selftest.hpp"
+#include <catch.hpp>
 
 SelfTest::SelfTest(QObject *parent) : QAbstractListModel(parent) {}
 
@@ -11,14 +12,13 @@ int SelfTest::rowCount(const QModelIndex &parent) const {
   // For list models only the root node (an invalid parent) should return the list's size. For all
   // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
   if (parent.isValid()) return 0;
-
-  // FIXME: Implement me!
-  return 10;
+  const auto &hub = Catch::getRegistryHub();
+  return hub.getTestCaseRegistry().getAllTests().size();
 }
 
 QVariant SelfTest::data(const QModelIndex &index, int role) const {
   if (!index.isValid()) return QVariant();
-
-  // FIXME: Implement me!
-  return "Hello World!";
+  const auto &hub = Catch::getRegistryHub();
+  auto tc = hub.getTestCaseRegistry().getAllTests().at(index.row());
+  return QString::fromStdString(tc.getTestCaseInfo().name);
 }
