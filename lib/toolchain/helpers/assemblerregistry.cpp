086f7212b98cbc5323325e7944fdf6d59358e959
Matthew McRaven <matthew.mcraven@gmail.com>
2025-06-17 19:21:58 -0700

    refactor(bi): combine assembler/formatter instantiation
    Less duplicated code to debug!

--- lib/toolchain/helpers/assemblerregistry.cpp
+++ lib/toolchain/helpers/assemblerregistry.cpp
@@ -3,10 +3,17 @@
 #include "toolchain/helpers/asmb.hpp"
 #include "toolchain/pas/operations/pepp/string.hpp"
 
-struct Pep10Assembler : public builtins::Registry::Assembler {
-  Pep10Assembler(const builtins::Registry *registry) : _registry(registry) {}
+struct PepAssembler : public builtins::Registry::Assembler {
+  PepAssembler(const builtins::Registry *registry, pepp::Architecture arch) : _registry(registry), _arch(arch) {}
   QVariant operator()(const QString &os, const QString &user) override {
-    helpers::AsmHelper assembler(helpers::cs6e_macros(_registry), os, pepp::Architecture::PEP10);
+    QSharedPointer<macro::Registry> macros;
+    if (_arch == pepp::Architecture::PEP10) macros = helpers::cs6e_macros(_registry);
+    else if (_arch == pepp::Architecture::PEP9) macros = helpers::cs5e_macros(_registry);
+    else {
+      qWarning("Unsupported architecture for assembler");
+      return {};
+    }
+    helpers::AsmHelper assembler(macros, os, _arch);
     assembler.setUserText(user);
     if (!assembler.assemble()) {
       qWarning("Failed to assemble!!");
@@ -22,115 +29,72 @@ struct Pep10Assembler : public builtins::Registry::Assembler {
 
 private:
   const builtins::Registry *_registry;
+  pepp::Architecture _arch;
 };
-struct Pep10HFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto listing = pas::ops::pepp::formatHexListing<isa::Pep10>(*node);
-    return listing.join("\n");
+
+template <typename ISA> QString formatH(QVariant assembled) {
+  if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
+    qWarning("Unexpected variant type");
+    return "";
   }
+  auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
+  auto listing = pas::ops::pepp::formatHexListing<ISA>(*node);
+  return listing.join("\n");
+}
+template <typename ISA> QString formatB(QVariant assembled) {
+  if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
+    qWarning("Unexpected variant type");
+    return "";
+  }
+  auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
+  auto listing = pas::ops::pepp::formatBinListing<ISA>(*node);
+  return listing.join("\n");
+}
+template <typename ISA> QString formatL(QVariant assembled) {
+  if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
+    qWarning("Unexpected variant type");
+    return "";
+  }
+  auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
+  auto listing = pas::ops::pepp::formatListing<ISA>(*node);
+  return listing.join("\n");
+}
+
+template <typename ISA> QString formatO(QVariant assembled) {
+  if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
+    qWarning("Unexpected variant type");
+    return "";
+  }
+  auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
+  auto bytes = pas::ops::pepp::toBytes<ISA>(*node);
+  const bool isPep10 = std::is_same_v<ISA, isa::Pep10>;
+  return pas::ops::pepp::bytesToObject(bytes, 16, !isPep10);
+}
+
+struct Pep10HFormatter : public builtins::Registry::Formatter {
+  QString operator()(QVariant assembled) override { return formatH<isa::Pep10>(assembled); }
 };
 struct Pep10BFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto listing = pas::ops::pepp::formatBinListing<isa::Pep10>(*node);
-    return listing.join("\n");
-  }
+  QString operator()(QVariant assembled) override { return formatB<isa::Pep10>(assembled); }
 };
 struct Pep10LFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto listing = pas::ops::pepp::formatListing<isa::Pep10>(*node);
-    return listing.join("\n");
-  }
+  QString operator()(QVariant assembled) override { return formatL<isa::Pep10>(assembled); }
 };
 struct Pep10OFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto bytes = pas::ops::pepp::toBytes<isa::Pep10>(*node);
-    return pas::ops::pepp::bytesToObject(bytes, 16, false);
-  }
+  QString operator()(QVariant assembled) override { return formatO<isa::Pep10>(assembled); }
 };
 
-struct Pep9Assembler : public builtins::Registry::Assembler {
-  Pep9Assembler(const builtins::Registry *registry) : _registry(registry) {}
-  QVariant operator()(const QString &os, const QString &user) override {
-    helpers::AsmHelper assembler(helpers::cs6e_macros(_registry), os, pepp::Architecture::PEP9);
-    assembler.setUserText(user);
-    if (!assembler.assemble()) {
-      qWarning("Failed to assemble!!");
-      return {};
-    }
-    auto root = assembler.userRoot();
-    if (root.isNull()) {
-      qWarning("Assembler returned a null user root");
-      return {};
-    }
-    return QVariant::fromValue(root);
-  }
-
-private:
-  const builtins::Registry *_registry;
-};
 struct Pep9HFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto listing = pas::ops::pepp::formatHexListing<isa::Pep9>(*node);
-    return listing.join("\n");
-  }
+  QString operator()(QVariant assembled) override { return formatH<isa::Pep9>(assembled); }
 };
 struct Pep9BFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto listing = pas::ops::pepp::formatBinListing<isa::Pep9>(*node);
-    return listing.join("\n");
-  }
+  QString operator()(QVariant assembled) override { return formatB<isa::Pep9>(assembled); }
 };
 struct Pep9LFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto listing = pas::ops::pepp::formatListing<isa::Pep9>(*node);
-    return listing.join("\n");
-  }
+  QString operator()(QVariant assembled) override { return formatL<isa::Pep9>(assembled); }
 };
 struct Pep9OFormatter : public builtins::Registry::Formatter {
-  QString operator()(QVariant assembled) override {
-    if (!assembled.canConvert<QSharedPointer<const pas::ast::Node>>()) {
-      qWarning("Unexpected variant type");
-      return "";
-    }
-    auto node = assembled.value<QSharedPointer<const pas::ast::Node>>();
-    auto bytes = pas::ops::pepp::toBytes<isa::Pep9>(*node);
-    return pas::ops::pepp::bytesToObject(bytes, 16, true);
-  }
+  QString operator()(QVariant assembled) override { return formatO<isa::Pep9>(assembled); }
 };
 
 QSharedPointer<builtins::Registry> helpers::builtins_registry(bool use_app_settings, QString directory) {
@@ -141,12 +105,14 @@ QSharedPointer<builtins::Registry> helpers::builtins_registry(bool use_app_setti
 
 QSharedPointer<builtins::Registry> helpers::registry_with_assemblers(QString directory) {
   auto registry = builtins_registry(true, directory);
-  registry->addAssembler(pepp::Architecture::PEP10, std::make_unique<Pep10Assembler>(&*registry));
+  registry->addAssembler(pepp::Architecture::PEP10,
+                         std::make_unique<PepAssembler>(&*registry, pepp::Architecture::PEP10));
   registry->addFormatter(pepp::Architecture::PEP10, "peph", std::make_unique<Pep10HFormatter>());
   registry->addFormatter(pepp::Architecture::PEP10, "pepb", std::make_unique<Pep10BFormatter>());
   registry->addFormatter(pepp::Architecture::PEP10, "pepl", std::make_unique<Pep10LFormatter>());
   registry->addFormatter(pepp::Architecture::PEP10, "pepo", std::make_unique<Pep10OFormatter>());
-  registry->addAssembler(pepp::Architecture::PEP9, std::make_unique<Pep9Assembler>(&*registry));
+  registry->addAssembler(pepp::Architecture::PEP9,
+                         std::make_unique<PepAssembler>(&*registry, pepp::Architecture::PEP9));
   registry->addFormatter(pepp::Architecture::PEP9, "peph", std::make_unique<Pep9HFormatter>());
   registry->addFormatter(pepp::Architecture::PEP9, "pepb", std::make_unique<Pep9BFormatter>());
   registry->addFormatter(pepp::Architecture::PEP9, "pepl", std::make_unique<Pep9LFormatter>());
