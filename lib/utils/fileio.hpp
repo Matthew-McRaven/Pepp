8841eee6f8f2bf53a813d185b01f99a3cc990f05
Matthew McRaven <matthew.mcraven@gmail.com>
2025-07-01 09:04:37 -0700

    feat: add file open dialog for source code
    It tries to keep track of the level of abstraction for you using filters.
    Hopefully this will make creating and populating new projects easier.

--- lib/utils/fileio.hpp
+++ lib/utils/fileio.hpp
@@ -6,9 +6,11 @@
 class FileIO : public QObject {
   Q_OBJECT
   QML_NAMED_ELEMENT(FileIO)
-  QML_SINGLETON
 
 public:
   FileIO(QObject *parent = nullptr);
   Q_INVOKABLE void save(const QString &filename, const QString &data);
+  Q_INVOKABLE void load(const QString &filters);
+signals:
+  Q_INVOKABLE void fileLoaded(const QString &fileName, const QByteArray &fileContent, int arch, int abstraction);
 };
