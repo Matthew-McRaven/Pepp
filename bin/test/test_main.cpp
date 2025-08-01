d24b15cbc79d850eca7de099150bb1114f623df0
Matthew McRaven <matthew.mcraven@gmail.com>
2025-07-31 17:16:34 -0700

    test: re-enable CLI tests
    Should add some certainty that the CLI has not been broken.
    
    Also stop configuring the config file.
    I don't want CMake to be involved in detecting our terminal path.

--- bin/test/test_main.cpp
+++ bin/test/test_main.cpp
@@ -1,5 +1,6 @@
+#include <QCoreApplication>
 #include "config.hpp"
-int main(int argc, char *argv[])
-{
-    return Catch::Session().run(argc, argv);
+int main(int argc, char *argv[]) {
+  QCoreApplication ap(argc, argv);
+  return Catch::Session().run(argc, argv);
 }
