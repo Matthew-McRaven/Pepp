e8156cecef4b7dcfaacd93626f2f451965684117
Matthew McRaven <matthew.mcraven@gmail.com>
2025-09-21 14:32:49 -0700

    refactor(pas): reformat PeppLexerErrorListener

--- lib/toolchain/parse/PeppLexerErrorListener.h
+++ lib/toolchain/parse/PeppLexerErrorListener.h
@@ -9,12 +9,18 @@ namespace parse {
 
 class  PeppLexerErrorListener : public antlr4::ANTLRErrorListener {
 
-    bool _hadError = false;
+  bool _hadError = false;
+
 public:
-    bool hadError();
-    void syntaxError(antlr4::Recognizer *recognizer, antlr4::Token *offendingSymbol, size_t line, size_t charPositionInLine, const std::string &msg, std::exception_ptr e);
-    void reportAmbiguity(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex, size_t stopIndex, bool exact, const antlrcpp::BitSet &ambigAlts, antlr4::atn::ATNConfigSet *configs);
-    void reportAttemptingFullContext(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex, size_t stopIndex, const antlrcpp::BitSet &conflictingAlts, antlr4::atn::ATNConfigSet *configs);
-    void reportContextSensitivity(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex, size_t stopIndex, size_t prediction, antlr4::atn::ATNConfigSet *configs);
+  bool hadError();
+  void syntaxError(antlr4::Recognizer *recognizer, antlr4::Token *offendingSymbol, size_t line,
+                   size_t charPositionInLine, const std::string &msg, std::exception_ptr e);
+  void reportAmbiguity(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex, size_t stopIndex,
+                       bool exact, const antlrcpp::BitSet &ambigAlts, antlr4::atn::ATNConfigSet *configs);
+  void reportAttemptingFullContext(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex,
+                                   size_t stopIndex, const antlrcpp::BitSet &conflictingAlts,
+                                   antlr4::atn::ATNConfigSet *configs);
+  void reportContextSensitivity(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa, size_t startIndex,
+                                size_t stopIndex, size_t prediction, antlr4::atn::ATNConfigSet *configs);
 };
 }
