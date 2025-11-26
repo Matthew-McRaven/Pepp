457bf258da6005df8ab55763f2763e7c62c9e5cc
Matthew McRaven <matthew.mcraven@gmail.com>
2025-11-25 22:12:21 -0800

    build: do not list changelog.db as both output and byproduct
    Otherwise ninja thinks it has multiple rules which build it.

--- config/cmake/GenerateChangelog.cmake
+++ config/cmake/GenerateChangelog.cmake
@@ -1,24 +1,20 @@
-
 if(Python_FOUND)
   # Ensure that changelog exists at configure time, otherwise qt_add_resources
   # will fail on some Mac platforms.
   execute_process(
     OUTPUT_FILE ${CMAKE_BINARY_DIR}/changelog.db
-    COMMAND
-      ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/scripts/changelog_tools.py
-      to_sql ${CMAKE_BINARY_DIR}/changelog.db
+    COMMAND ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/scripts/changelog_tools.py
+            to_sql ${CMAKE_BINARY_DIR}/changelog.db
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
   # Ensure that target only rebuils iff the CSV files change using depends.
   add_custom_command(
     OUTPUT ${CMAKE_BINARY_DIR}/changelog.db
-    COMMAND
-      ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/scripts/changelog_tools.py
-      to_sql ${CMAKE_BINARY_DIR}/changelog.db
+    COMMAND ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/scripts/changelog_tools.py
+            to_sql ${CMAKE_BINARY_DIR}/changelog.db
     DEPENDS ${CMAKE_SOURCE_DIR}/data/changelog/changes.csv
             ${CMAKE_SOURCE_DIR}/data/changelog/versions.csv
             ${CMAKE_SOURCE_DIR}/scripts/changelog_tools.py
     COMMENT "Generating changelog SQL"
-    BYPRODUCTS ${CMAKE_BINARY_DIR}/changelog.db
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
   # Must use a dummy static library target. Attempting to use this directly in a
   # shared library fails due to missing rules for object code files.
