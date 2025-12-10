9926f591202230b1915f462085fbb42cd1eb6bd7
Matthew McRaven <matthew.mcraven@gmail.com>
2025-12-09 17:13:04 -0800

    test(pas): add additional address assignment test
    I'm worried that statements before a .ORG will misbehave within a section.

--- test/toolchain2/asm/pep_codegen_org.cpp
+++ test/toolchain2/asm/pep_codegen_org.cpp
@@ -35,6 +35,13 @@ static const auto ex3 = R"(.WORD 5
 .ORG 0xfeed
 .WORD 20
 )";
+static const auto ex4 = R"(.WORD 5
+.WORD 10
+.SECTION ".text2", "rwx"
+.BLOCK 15
+.ORG 0xfeed
+.WORD 20
+)";
 } // namespace
 
 TEST_CASE("Pepp ASM codegen .ORG address assignment", "[scope:asm][kind:unit][arch:*][tc2]") {
@@ -101,5 +108,30 @@ TEST_CASE("Pepp ASM codegen .ORG address assignment", "[scope:asm][kind:unit][ar
     CHECK(addresses.find(&*s1[0]) == addresses.end());
     CHECK(addresses.find(&*s1[1]) == addresses.end());
     CHECK(addresses.at(&*s1[2]).address == 0xfeed);
+  }
+  SECTION("Two sections, .ORG in middle of second section") {
+    pepp::tc::DiagnosticTable diag;
+    auto p = Parser(data(ex4));
+    auto results = p.parse(diag);
+    CHECK(diag.count() == 0);
+    REQUIRE(results.size() == 6);
+    auto result = pepp::tc::split_to_sections(results);
+    auto &sections = result.grouped_ir;
+    auto addresses = pepp::tc::assign_addresses(sections);
+    CHECK(sections.size() == 2);
+
+    CHECK(sections[0].first.name == ".text");
+    CHECK(sections[0].second.size() == 2);
+    auto s0 = sections[0].second;
+    CHECK(addresses.at(&*s0[0]).address == 0xfeed - 19);
+    CHECK(addresses.at(&*s0[1]).address == 0xfeed - 17);
+
+    CHECK(sections[1].first.name == ".text2");
+    CHECK(sections[1].second.size() == 4);
+    auto s1 = sections[1].second;
+    CHECK(addresses.find(&*s1[0]) == addresses.end());
+    CHECK(addresses.at(&*s1[1]).address == 0xfeed - 15);
+    CHECK(addresses.find(&*s1[2]) == addresses.end());
+    CHECK(addresses.at(&*s1[3]).address == 0xfeed);
   }
 }
