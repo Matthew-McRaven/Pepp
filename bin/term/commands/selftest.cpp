d5eb24795a6200734f5b898d9374b5765449c0ac
Matthew McRaven <matthew.mcraven@gmail.com>
2025-12-23 10:56:41 -0800

    docs: update copyrights in `bin`

--- bin/term/commands/selftest.cpp
+++ bin/term/commands/selftest.cpp
@@ -1,6 +1,5 @@
-
 /*
- * Copyright (c) 2024 J. Stanley Warford, Matthew McRaven
+ * Copyright (c) 2024-2026 J. Stanley Warford, Matthew McRaven
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
