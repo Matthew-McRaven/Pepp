70c2c53f362ceb582421b25ad6a0ac3fa7a7ce4c
Matthew McRaven <matthew.mcraven@gmail.com>
2025-12-23 11:06:19 -0800

    docs: update non-simulator, non-assembler copyrights

--- lib/enums/isa/pep_shared.hpp
+++ lib/enums/isa/pep_shared.hpp
@@ -1,6 +1,6 @@
 #pragma once
 /*
- * Copyright (c) 2023 J. Stanley Warford, Matthew McRaven
+ * Copyright (c) 2023-2026 J. Stanley Warford, Matthew McRaven
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
