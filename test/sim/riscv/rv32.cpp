0a4f0a7e3c17a0915496eb52fcc63c21fba8bbe0
Matthew McRaven <matthew.mcraven@gmail.com>
2025-12-27 15:44:32 -0800

    test(sim): add instruction unit tests for RISC-V simulator

---
+++ test/sim/riscv/rv32.cpp
@@ -0,0 +1,136 @@
+#include <catch.hpp>
+#include <cstdint>
+#include "enums/isa/rvc.hpp"
+#include "sim3/systems/notraced_riscv_isa3_system.hpp"
+
+#include "testable_instruction.hpp"
+
+TEST_CASE("RV32I", "[scope:sim][kind:int][arch:RV]") {
+
+  static const std::array<uint32_t, 4> instructions = {
+      0x00065637, // lui     a2,0x65
+      0x000655b7, // lui     a1,0x65
+      0x11612023, // sw      s6,256(sp)
+      0x0b410b13, // addi    s6,sp,180
+  };
+
+  riscv::Machine<riscv::RISCV32> m{std::string_view{}, {.memory_max = 65536}};
+  // install instructions
+  m.cpu.init_execute_area(instructions.data(), 0x1000, 4 * instructions.size());
+  m.cpu.jump(0x1000);
+
+  // stack frame
+  m.cpu.reg(riscv::REG_SP) = 0x120000 - 288;
+  const uint32_t current_sp = m.cpu.reg(riscv::REG_SP);
+
+  // execute LUI a2, 0x65000
+  m.cpu.step_one();
+  CHECK(m.cpu.reg(riscv::REG_ARG2) == 0x65000);
+  // execute LUI a1, 0x65000
+  m.cpu.step_one();
+  CHECK(m.cpu.reg(riscv::REG_ARG1) == 0x65000);
+  // execute SW  s6, [SP + 256]
+  m.cpu.reg(22) = 0x12345678;
+  m.cpu.step_one();
+  CHECK(m.memory.template read<uint32_t>(current_sp + 256) == m.cpu.reg(22));
+  // execute ADDI s6, [SP + 180]
+  m.cpu.reg(22) = 0x0;
+  m.cpu.step_one();
+  CHECK(m.cpu.reg(22) == current_sp + 180);
+}
+
+TEST_CASE("RV32C", "[scope:sim][kind:int][arch:RV]") {
+  riscv::Machine<uint32_t> machine{std::string_view{}, {.memory_max = 65536}};
+  SECTION("C.SRLI") { // C.SRLI imm = [0, 31] CI_CODE(0b100, 0b01):
+    for (int i = 0; i < 32; i++) {
+      riscv::rv32c_instruction ci;
+      ci.CA.opcode = 0b01;     // Quadrant 1
+      ci.CA.funct6 = 0b100000; // ALU OP: SRLI
+      ci.CAB.srd = 0x2;        // A0
+      ci.CAB.imm04 = i;
+      ci.CAB.imm5 = 0;
+
+      const riscv::testable_insn<uint32_t> insn{
+          .name = "C.SRLI", .bits = ci.whole, .reg = riscv::REG_ARG0, .index = i, .initial_value = 0xFFFFFFFF};
+      bool b = validate<uint32_t>(machine, insn, [](auto &cpu, const auto &insn) -> bool {
+        return cpu.reg(insn.reg) == (insn.initial_value >> insn.index);
+      });
+      CHECK(b);
+    }
+  }
+  SECTION("C.SRAI") { // C.SRAI imm = [0, 31] CI_CODE(0b100, 0b01):
+
+    for (int i = 0; i < 32; i++) {
+      riscv::rv32c_instruction ci;
+      ci.CA.opcode = 0b01;     // Quadrant 1
+      ci.CA.funct6 = 0b100001; // ALU OP: SRAI
+      ci.CAB.srd = 0x2;        // A0
+      ci.CAB.imm04 = i;
+      ci.CAB.imm5 = 0;
+
+      const riscv::testable_insn<uint32_t> insn{
+          .name = "C.SRAI", .bits = ci.whole, .reg = riscv::REG_ARG0, .index = i, .initial_value = 0xFFFFFFFF};
+      bool b = validate<uint32_t>(
+          machine, insn, [](auto &cpu, const auto &insn) -> bool { return cpu.reg(insn.reg) == insn.initial_value; });
+      CHECK(b);
+    }
+  };
+  SECTION("C.ANDI") { // C.ANDI imm = [-32, 31] CI_CODE(0b100, 0b01):
+    for (int i = 0; i < 64; i++) {
+      riscv::rv32c_instruction ci;
+      ci.CA.opcode = 0b01;     // Quadrant 1
+      ci.CA.funct6 = 0b100010; // ALU OP: ANDI
+      ci.CAB.srd = 0x2;        // A0
+      ci.CAB.imm04 = i & 31;
+      ci.CAB.imm5 = i >> 5;
+
+      const riscv::testable_insn<uint32_t> insn{
+          .name = "C.ANDI", .bits = ci.whole, .reg = riscv::REG_ARG0, .index = i, .initial_value = 0xFFFFFFFF};
+      bool b = validate<uint32_t>(machine, insn, [](auto &cpu, const auto &insn) -> bool {
+        if (insn.index < 32) {
+          return cpu.reg(insn.reg) == (insn.initial_value & insn.index);
+        }
+        return cpu.reg(insn.reg) == (insn.initial_value & (insn.index - 64));
+      });
+      CHECK(b);
+    }
+  }
+  SECTION("C.SLLI") { // C.SLLI imm = [0, 31] CI_CODE(0b011, 0b10):
+    for (int i = 0; i < 32; i++) {
+      riscv::rv32c_instruction ci;
+      ci.CI.opcode = 0b10; // Quadrant 1
+      ci.CI.funct3 = 0x0;  // OP: SLLI
+      ci.CI.rd = 0xA;      // A0
+      ci.CI.imm1 = i;
+      ci.CI.imm2 = 0;
+
+      const riscv::testable_insn<uint32_t> insn{
+          .name = "C.SLLI", .bits = ci.whole, .reg = riscv::REG_ARG0, .index = i, .initial_value = 0xA};
+      bool b = validate<uint32_t>(machine, insn, [](auto &cpu, const auto &insn) -> bool {
+        return cpu.reg(insn.reg) == (insn.initial_value << insn.index);
+      });
+      CHECK(b);
+    }
+  }
+}
+
+TEST_CASE("RV32 Custom Machine", "[scope:sim][kind:int][arch:RV][!throws]") {
+  // this is a custom machine with very little virtual memory
+  riscv::Machine<uint32_t> m2{std::string_view{}, {.memory_max = 65536}};
+
+  // free the zero-page to reclaim 4k
+  m2.memory.free_pages(0x0, riscv::Page::size());
+
+  // fake a start at 0x1068
+  const uint32_t entry_point = 0x1068;
+  try {
+    m2.cpu.jump(entry_point);
+  } catch (...) {
+  }
+  try {
+    m2.simulate();
+  } catch (...) {
+  }
+
+  CHECK(m2.instruction_counter() == 0);
+}
---
+++ test/sim/riscv/testable_instruction.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <cstdio>
+#include "enums/isa/rv_types.hpp"
+#include "enums/isa/rvi.hpp"
+#include "sim3/systems/notraced_riscv_isa3_system.hpp"
+
+namespace riscv
+{
+	template<AddressType address_t>
+	struct testable_insn {
+		const char* name;     // test name
+		address_t bits; // the instruction bits
+		const int reg;        // which register this insn affects
+		const int index;      // test loop index
+		address_t initial_value; // start value of register
+	};
+
+	template <AddressType address_t>
+	static bool
+	validate(Machine<address_t>& machine, const testable_insn<address_t>& insn,
+			std::function<bool(CPU<address_t>&, const testable_insn<address_t>&)> callback)
+	{
+		static const address_t MEMBASE = 0x1000;
+
+		const std::array<uint32_t, 1> instr_page = {
+			insn.bits
+		};
+
+		DecodedExecuteSegment<address_t> &des = machine.cpu.init_execute_area(&instr_page[0], MEMBASE, sizeof(instr_page));
+		// jump to page containing instruction
+		machine.cpu.jump(MEMBASE);
+		// execute instruction
+		machine.cpu.reg(insn.reg) = insn.initial_value;
+		machine.cpu.step_one();
+		// There is a max number of execute segments. Evict the latest to avoid the max limit check
+		machine.cpu.memory().evict_execute_segment(des);
+		// call instruction validation callback
+		if ( callback(machine.cpu, insn) ) return true;
+		fprintf(stderr, "Failed test: %s on iteration %d\n", insn.name, insn.index);
+		fprintf(stderr, "Register value: 0x%X\n", machine.cpu.reg(insn.reg));
+		return false;
+	}
+}
