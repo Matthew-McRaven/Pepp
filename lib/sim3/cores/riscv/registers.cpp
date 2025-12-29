3649ad32f02b1b55b0ec77e7473cf77891909ee7
Matthew McRaven <matthew.mcraven@gmail.com>
2025-12-28 17:19:12 -0800

    fix: avoid potentially unsafe `snprintf`
    In practice, this will never happen.
    The buffer has been sized for the number of items being written at compile time.
    
    The number of registers will never changed, since it's fixed by the ISA.
    Still, this should appease CodeQL

--- lib/sim3/cores/riscv/registers.cpp
+++ lib/sim3/cores/riscv/registers.cpp
@@ -32,42 +32,52 @@
  * <https://opensource.org/license/bsd-3-clause>
  */
 #include "./registers.hpp"
+#include <stdexcept>
 #include "enums/isa/rv_base.hpp"
 
 namespace riscv {
 
 template <AddressType address_t> PEPP_COLD_PATH() std::string Registers<address_t>::to_string() const {
   char buffer[600];
-  int len = 0;
+  unsigned len = 0;
   for (int i = 1; i < 32; i++) {
+    if (UNLIKELY(len >= sizeof(buffer))) throw std::logic_error("Not possible, buffer sized at compile time");
     len += snprintf(buffer + len, sizeof(buffer) - len, "[%s\t%08X] ", RISCV::regname(i), this->get(i));
-    if (i % 5 == 4) len += snprintf(buffer + len, sizeof(buffer) - len, "\n");
+    if (i % 5 == 4) {
+      if (UNLIKELY(len >= sizeof(buffer))) throw std::logic_error("Not possible, buffer sized at compile time");
+      len += snprintf(buffer + len, sizeof(buffer) - len, "\n");
+    }
   }
   return std::string(buffer, len);
 }
 
 template <AddressType address_t> PEPP_COLD_PATH() std::string Registers<address_t>::flp_to_string() const {
   char buffer[800];
-  int len = 0;
+  unsigned len = 0;
   for (int i = 0; i < 32; i++) {
     auto &src = this->getfl(i);
     const char T = (src.i32[1] == 0) ? 'S' : 'D';
     if constexpr (true) {
       double val = (src.i32[1] == 0) ? src.f32[0] : src.f64;
+      if (UNLIKELY(len >= sizeof(buffer))) throw std::logic_error("Not possible, buffer sized at compile time");
       len += snprintf(buffer + len, sizeof(buffer) - len, "[%s\t%c%+.2f] ", RISCV::flpname(i), T, val);
     } else {
       if (src.i32[1] == 0) {
         double val = src.f64;
+        if (UNLIKELY(len >= sizeof(buffer))) throw std::logic_error("Not possible, buffer sized at compile time");
         len += snprintf(buffer + len, sizeof(buffer) - len, "[%s\t%c0x%lX] ", RISCV::flpname(i), T, *(int64_t *)&val);
       } else {
         float val = src.f32[0];
+        if (UNLIKELY(len >= sizeof(buffer))) throw std::logic_error("Not possible, buffer sized at compile time");
         len += snprintf(buffer + len, sizeof(buffer) - len, "[%s\t%c0x%X] ", RISCV::flpname(i), T, *(int32_t *)&val);
       }
     }
     if (i % 5 == 4) {
+      if (UNLIKELY(len >= sizeof(buffer))) throw std::logic_error("Not possible, buffer sized at compile time");
       len += snprintf(buffer + len, sizeof(buffer) - len, "\n");
     }
   }
+  if (UNLIKELY(len >= sizeof(buffer))) throw std::logic_error("Not possible, buffer sized at compile time");
   len += snprintf(buffer + len, sizeof(buffer) - len, "[FFLAGS\t0x%X] ", m_fcsr.fflags);
   return std::string(buffer, len);
 }
