01e9488789de41d9deb15d8ba1f7a3192429d6bc
Matthew McRaven <matthew.mcraven@gmail.com>
2026-01-19 22:14:24 -0800

    fix: address various typos relating to pypepp
    Also update uv lock file

--- pypepp/__init__.py
+++ pypepp/__init__.py
@@ -1,4 +1,4 @@
-#  /Copyright (c) 2026. Stanley Warford, Matthew McRaven
+#   Copyright (c) 2026. Stanley Warford, Matthew McRaven
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
@@ -11,21 +11,6 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 from .pypepp_native import add
