521496decb22a7e9434e2fd91949b055faa5bae2
David McRaven <david.mcraven@sabrixtax.com>
2026-01-27 07:41:09 -0800

    set application icon

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QGuiApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
@@ -13,6 +14,8 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.loadFromModule("CircuitDesign", "Main");
+
+    app.setWindowIcon(QIcon{"qrc:/icon"});
 
     return app.exec();
 }
