9cbbc1dac6f42b8ba2e2ba6b4e42c7e1fdf487f0
Matthew McRaven <matthew.mcraven@gmail.com>
2026-02-04 08:18:11 -0800

    feat(bin): draw static rectangles
    Draw a collection of compile-time defined rectangles + a background.

--- bin/logisim/ui/renderer.cpp
+++ bin/logisim/ui/renderer.cpp
@@ -1,10 +1,47 @@
 #include "renderer.hpp"
 #include <QPainter>
 
-CursedCanvas::CursedCanvas(QQuickItem *parent) : QQuickPaintedItem(parent) {}
+CursedCanvas::CursedCanvas(QQuickItem *parent) : QQuickPaintedItem(parent) {
+  using Size = pepp::core::Size<i16>;
+  using Pt = pepp::core::Point<i16>;
+  auto eight = Size{8, 8};
+  _rects.emplace_back(Rectangle{Pt{10, 20}, eight});
+  _rects.emplace_back(Rectangle{Pt{30, 20}, eight});
+  _rects.emplace_back(Rectangle{Pt{40, 20}, eight});
+  _rects.emplace_back(Rectangle{Pt{50, 20}, eight});
+
+  _rects.emplace_back(Rectangle{Pt{10, 30}, eight});
+  _rects.emplace_back(Rectangle{Pt{30, 50}, eight});
+  _rects.emplace_back(Rectangle{Pt{40, 80}, eight});
+  _rects.emplace_back(Rectangle{Pt{50, 90}, eight});
+  _top_left = Pt{0, 0};
+}
 
 void CursedCanvas::paint(QPainter *painter) {
-  QBrush brush(QColor("#007430"));
+  QBrush brush(QColor(0x00, 0x74, 0x20));
   painter->setBrush(brush);
   painter->drawRect(0, 0, size().width(), size().height());
+  painter->setBrush(QBrush(QColor(255, 0, 0)));
+
+  for (const auto &rect : _rects) {
+    const auto screen_rect = grid_to_screen(rect);
+    painter->save();
+    painter->setClipRect(screen_rect);
+    paint_one(painter, rect, nullptr);
+    painter->restore();
+  }
+}
+
+void CursedCanvas::paint_one(QPainter *painter, Rectangle rect, void *props) {
+  auto screen_rect = grid_to_screen(rect);
+  painter->drawRect(screen_rect);
+}
+
+QRectF CursedCanvas::grid_to_screen(Rectangle rect) {
+  const auto offset_x = rect.top_left().x() - _top_left.x();
+  const auto offset_y = rect.top_left().y() - _top_left.y();
+  const auto width = rect.width();
+  const auto height = rect.height();
+  const float grid_to_px = 4.0f;
+  return QRectF(offset_x * grid_to_px, offset_y * grid_to_px, width * grid_to_px, height * grid_to_px);
 }
--- bin/logisim/ui/renderer.hpp
+++ bin/logisim/ui/renderer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <QQuickPaintedItem>
+#include "core/math/geom/rectangle.hpp"
 class CursedCanvas : public QQuickPaintedItem {
   Q_OBJECT
   QML_NAMED_ELEMENT(CursedCanvas)
@@ -7,4 +8,11 @@ class CursedCanvas : public QQuickPaintedItem {
 public:
   CursedCanvas(QQuickItem *parent = nullptr);
   void paint(QPainter *painter) override;
+
+private:
+  using Rectangle = pepp::core::Rectangle<i16>;
+  void paint_one(QPainter *painter, Rectangle rect, void *props);
+  QRectF grid_to_screen(Rectangle rect);
+  std::vector<Rectangle> _rects;
+  pepp::core::Point<i16> _top_left;
 };
