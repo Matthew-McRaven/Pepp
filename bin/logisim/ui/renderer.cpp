3cd041e5ff6d768a5abc3e92fcbad5aa7e9bae24
Matthew McRaven <matthew.mcraven@gmail.com>
2026-02-04 17:30:59 -0800

    docs(bin): add additional comments to QQuickPaintedItem

--- bin/logisim/ui/renderer.cpp
+++ bin/logisim/ui/renderer.cpp
@@ -4,6 +4,7 @@
 CursedCanvas::CursedCanvas(QQuickItem *parent) : QQuickPaintedItem(parent) {
   using Size = pepp::core::Size<i16>;
   using Pt = pepp::core::Point<i16>;
+  // Create a semi-random assortment of same-sized rectangles to render.
   auto eight = Size{8, 8};
   _rects.emplace_back(Rectangle{Pt{10, 20}, eight});
   _rects.emplace_back(Rectangle{Pt{30, 20}, eight});
@@ -16,37 +17,43 @@ CursedCanvas::CursedCanvas(QQuickItem *parent) : QQuickPaintedItem(parent) {
   _rects.emplace_back(Rectangle{Pt{50, 90}, eight});
   _rects.emplace_back(Rectangle{Pt{150, 90}, eight});
   _rects.emplace_back(Rectangle{Pt{250, 90}, eight});
+
+  // Initalize viewport to 0,0
   _top_left = Pt{0, 0};
 }
 
 void CursedCanvas::paint(QPainter *painter) {
+  // Paint the background.
   QBrush brush(QColor(0x00, 0x74, 0x20));
   painter->setBrush(brush);
   painter->drawRect(0, 0, size().width(), size().height());
-  painter->setBrush(QBrush(QColor(255, 0, 0)));
 
+  // Determine the size of the viewport in grid coordinates.
   auto screen_viewport = QRectF(0, 0, size().width(), size().height());
   auto grid_viewport = screen_to_grid(screen_viewport);
 
+  // Make rectangles a different color.
+  painter->setBrush(QBrush(QColor(255, 0, 0)));
   for (const auto &rect : _rects) {
+    // Skip paiting rectangles that are outside the viewport.
     if (!pepp::core::intersects(grid_viewport, rect)) continue;
-
-    painter->save();
     paint_one(painter, rect, nullptr);
-    painter->restore();
   }
 }
 
 void CursedCanvas::paint_one(QPainter *painter, Rectangle rect, void *props) {
+  // Convert our absolute grid coordinates to screen coordinates.
   auto screen_rect = grid_to_screen(rect);
+  // If we actually had props, we would use them to make decisions about how to paint.
+  // e.g., do I copy one of the NAND/NOR images into this rectangle, or do I draw a solid color?
   painter->drawRect(screen_rect);
 }
 
 QRectF CursedCanvas::grid_to_screen(Rectangle rect) {
-  const auto offset_x = rect.top_left().x() - _top_left.x();
-  const auto offset_y = rect.top_left().y() - _top_left.y();
-  const auto width = rect.width();
-  const auto height = rect.height();
+  const float offset_x = rect.top_left().x() - _top_left.x();
+  const float offset_y = rect.top_left().y() - _top_left.y();
+  const float width = rect.width();
+  const float height = rect.height();
 
   return QRectF(offset_x * grid_to_px, offset_y * grid_to_px, width * grid_to_px, height * grid_to_px);
 }
--- bin/logisim/ui/renderer.hpp
+++ bin/logisim/ui/renderer.hpp
@@ -2,22 +2,29 @@
 #include <QQuickPaintedItem>
 #include "core/math/geom/rectangle.hpp"
 
+// "screen" coordinates are pixels, in a range specified by our containing Flickable.
+// "grid" coordinates are integer values. Currently, 1 grid unit = 4 screen pixels, but this should
+// be programmable to enable zoom.
 class CursedCanvas : public QQuickPaintedItem {
   Q_OBJECT
   QML_NAMED_ELEMENT(CursedCanvas)
+  // Sizes in "screen" coordinates
   Q_PROPERTY(float contentWidth READ contentWidth NOTIFY xBoundsChanged FINAL)
   Q_PROPERTY(float contentHeight READ contentHeight NOTIFY yBoundsChanged FINAL)
+  // In "screen" coordinates (e.g., pixels according to our containing Flickable)
   Q_PROPERTY(float originX READ originX WRITE setOriginX NOTIFY originChanged FINAL)
   Q_PROPERTY(float originY READ originY WRITE setOriginY NOTIFY originChanged FINAL)
 public:
   CursedCanvas(QQuickItem *parent = nullptr);
   void paint(QPainter *painter) override;
-  float maxX() const { return 1200; }
-  float maxY() const { return 2000; }
-  float contentWidth() const { return maxX(); }
-  float contentHeight() const { return maxY(); }
+  // TODO: determine min/max bounds based on contained rectangles, then add padding around the edges.
+  float contentWidth() const { return 1200; }
+  float contentHeight() const { return 2000; }
+
+  // The top-left corner, as measured in "screen" coordinates
   float originX() const { return _top_left.x() * grid_to_px; }
   float originY() const { return _top_left.y() * grid_to_px; }
+  // Compute grid coordinates from screen coordinates
   void setOriginX(float x) {
     _top_left = {static_cast<i16>(x / grid_to_px), _top_left.y()};
     emit originChanged();
@@ -34,11 +41,17 @@ signals:
   void originChanged();
 
 private:
+  // Magic constant to convert from grid coordinates to screen coordinates
   const float grid_to_px = 4.0f;
+  // One of the classes from my geometry library. See core/math/geom
   using Rectangle = pepp::core::Rectangle<i16>;
+  // Helepr for painting a single rect that has already "passed" the clipping test.
   void paint_one(QPainter *painter, Rectangle rect, void *props);
   QRectF grid_to_screen(Rectangle rect);
   Rectangle screen_to_grid(QRectF rect);
+
+  // The things we want to render
   std::vector<Rectangle> _rects;
+  // Top-left corner of the viewport in grid coordinates
   pepp::core::Point<i16> _top_left;
 };
