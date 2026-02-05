e2b7836fbd9b72df681624be1aeed70441bc99d6
Matthew McRaven <matthew.mcraven@gmail.com>
2026-02-04 17:12:18 -0800

    feat(bin): use canvas contentX/Y to set bounding rectangle of canvas
    Putting the canvas inside the flickable seems like a bad idea.
    A QQuickPaintedItem is backed by a QImage that has the same dimensions as the control.
    
    When placing the canvas inside a flickable, the dimensions of the canvas will be grown to contain all of the content, not just the visible portions.

--- bin/logisim/ui/renderer.cpp
+++ bin/logisim/ui/renderer.cpp
@@ -25,10 +25,13 @@ void CursedCanvas::paint(QPainter *painter) {
   painter->drawRect(0, 0, size().width(), size().height());
   painter->setBrush(QBrush(QColor(255, 0, 0)));
 
+  auto screen_viewport = QRectF(0, 0, size().width(), size().height());
+  auto grid_viewport = screen_to_grid(screen_viewport);
+
   for (const auto &rect : _rects) {
-    const auto screen_rect = grid_to_screen(rect);
+    if (!pepp::core::intersects(grid_viewport, rect)) continue;
+
     painter->save();
-    painter->setClipRect(screen_rect);
     paint_one(painter, rect, nullptr);
     painter->restore();
   }
@@ -46,4 +49,12 @@ QRectF CursedCanvas::grid_to_screen(Rectangle rect) {
   const auto height = rect.height();
 
   return QRectF(offset_x * grid_to_px, offset_y * grid_to_px, width * grid_to_px, height * grid_to_px);
+}
+
+CursedCanvas::Rectangle CursedCanvas::screen_to_grid(QRectF rect) {
+  const i16 x = static_cast<i16>(rect.x() / grid_to_px) + _top_left.x();
+  const i16 y = static_cast<i16>(rect.y() / grid_to_px) + _top_left.y();
+  const i16 width = static_cast<i16>(rect.width() / grid_to_px);
+  const i16 height = static_cast<i16>(rect.height() / grid_to_px);
+  return Rectangle{pepp::core::Point<i16>{x, y}, pepp::core::Size<i16>{width, height}};
 }
--- bin/logisim/ui/renderer.hpp
+++ bin/logisim/ui/renderer.hpp
@@ -1,21 +1,37 @@
 #pragma once
 #include <QQuickPaintedItem>
 #include "core/math/geom/rectangle.hpp"
+
 class CursedCanvas : public QQuickPaintedItem {
   Q_OBJECT
   QML_NAMED_ELEMENT(CursedCanvas)
   Q_PROPERTY(float contentWidth READ contentWidth NOTIFY xBoundsChanged FINAL)
   Q_PROPERTY(float contentHeight READ contentHeight NOTIFY yBoundsChanged FINAL)
+  Q_PROPERTY(float originX READ originX WRITE setOriginX NOTIFY originChanged FINAL)
+  Q_PROPERTY(float originY READ originY WRITE setOriginY NOTIFY originChanged FINAL)
 public:
   CursedCanvas(QQuickItem *parent = nullptr);
   void paint(QPainter *painter) override;
-  float maxX() const { return 600; }
+  float maxX() const { return 1200; }
   float maxY() const { return 2000; }
   float contentWidth() const { return maxX(); }
   float contentHeight() const { return maxY(); }
+  float originX() const { return _top_left.x() * grid_to_px; }
+  float originY() const { return _top_left.y() * grid_to_px; }
+  void setOriginX(float x) {
+    _top_left = {static_cast<i16>(x / grid_to_px), _top_left.y()};
+    emit originChanged();
+    update();
+  }
+  void setOriginY(float y) {
+    _top_left = {_top_left.x(), static_cast<i16>(y / grid_to_px)};
+    emit originChanged();
+    update();
+  }
 signals:
   void xBoundsChanged();
   void yBoundsChanged();
+  void originChanged();
 
 private:
   const float grid_to_px = 4.0f;
