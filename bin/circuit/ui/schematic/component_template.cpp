2b3e2cfe12311544c7b85aa3d6862001a97ffdac
Matthew McRaven <matthew.mcraven@gmail.com>
2026-04-04 18:30:39 -0700

    feat: add alignment constraints to ComponentTemplate
    Can be used to implement grid alignment constraints fairly easily.

--- bin/circuit/ui/schematic/component_template.cpp
+++ bin/circuit/ui/schematic/component_template.cpp
@@ -20,3 +20,29 @@ u16 ComponentTemplate::clock_pins() const {
     if (pin.type == PinType::Clock) it++;
   return it;
 }
+
+// Round value to the nearest value v such that v % modulus == offset. Modulus is required to be >0.
+// Compute k = round((value - offset) / modulus), then v = offset + k * modulus.
+static i16 align_component(i16 value, i16 modulus, i16 offset) {
+  // Do arithmetic in i32 to avoid overflow.
+  const i32 v = i32(value) - i32(offset);
+  // Round-to-nearest aligned value by adding 1/2 the value of the modulus with a floor-div. Sign must match v, because
+  const i32 half = i32(modulus) / 2;
+  const i32 shifted = v >= 0 ? v + half : v - half;
+  // Truncating division rounds toward zero, which is acceptable since we already shifted the value by half.
+  const i32 k = shifted / i32(modulus);
+  const i32 aligned = i32(offset) + k * i32(modulus);
+  return static_cast<i16>(std::clamp<i32>(aligned, std::numeric_limits<i16>::min(), std::numeric_limits<i16>::max()));
+};
+
+pepp::core::Point<i16> AlignmentConstraint::nearest_aligned_point(const pepp::core::Point<i16> &pt) const noexcept {
+  return pepp::core::Point<i16>{
+      align_component(pt.x(), x_modulus, x_offset),
+      align_component(pt.y(), y_modulus, y_offset),
+  };
+}
+
+bool AlignmentConstraint::is_aligned(const pepp::core::Point<i16> &pt) const noexcept {
+  return align_component(pt.x(), x_modulus, x_offset) == pt.x() &&
+         align_component(pt.y(), y_modulus, y_offset) == pt.y();
+}
--- bin/circuit/ui/schematic/component_template.hpp
+++ bin/circuit/ui/schematic/component_template.hpp
@@ -10,6 +10,19 @@ enum class PinType : u8 {
   Clock,
 };
 
+struct AlignmentConstraint {
+  // Only allow the component to be placed at pos.x%x_modulus == x_offset, and similarly for y.
+  // While a component can also be aligned to any (1,1) in instructor mode, this is used to make snap-to alignment.
+  // easier in practice.
+  // Modulus must always be >0 and offset+1 <modules.
+  // A modulus of 1 effectively disables alignment constraints.
+  i16 x_modulus = 1, x_offset = 0;
+  i16 y_modulus = 1, y_offset = 0;
+
+  pepp::core::Point<i16> nearest_aligned_point(const pepp::core::Point<i16> &pt) const noexcept;
+  bool is_aligned(const pepp::core::Point<i16> &pt) const noexcept;
+};
+
 struct ComponentTemplate {
   struct Pin {
     // Position is rleative to the geometry of the containing component.
@@ -19,6 +32,7 @@ struct ComponentTemplate {
 
   pepp::core::Rectangle<i16> geometry;
   std::vector<Pin> pins;
+  AlignmentConstraint alignmentConstraint;
 
   u16 input_pins() const;
   u16 output_pins() const;
