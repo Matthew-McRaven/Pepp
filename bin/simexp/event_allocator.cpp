f473610e5641ec7eb5a1aceff5f4a1ccaf0dd449
Matthew McRaven <matthew.mcraven@gmail.com>
2026-06-10 18:24:52 -0700

    refactor(bin): remove guard conditions from allocator::free
    This data type is fairly trivial, so a double-free doesn't really matter.
    
    Saves ~100ms

--- bin/simexp/event_allocator.cpp
+++ bin/simexp/event_allocator.cpp
@@ -4,16 +4,14 @@ void EventAllocator::free(Event *ev) {
   // Ensure that the event actually belongs to us.
   if (const void *address = ev; address == nullptr) [[unlikely]]
     return;
-  size_t slot_index = (reinterpret_cast<std::byte *>(ev) - _slots[0].data) / sizeof(EventSlot);
-  free(slot_index);
+  else free((reinterpret_cast<std::byte *>(ev) - _slots[0].data) / sizeof(EventSlot));
 }
 
 void EventAllocator::free(u8 idx) {
-  if (idx >= MAX_EVENTS) [[unlikely]]
+  /*if (idx >= MAX_EVENTS) [[unlikely]]
     throw std::runtime_error("Index too high");
-  else if (!_slots_used.test(idx)) [[unlikely]] throw std::runtime_error("Event slot is not currently allocated");
+  else if (!_slots_used.test(idx)) [[unlikely]] throw std::runtime_error("Event slot is not currently allocated");*/
   std::destroy_at((Event *)_slots[idx].data);
-
   _counters.freed++;
   _slots_used.clear_bit(idx);
 }
