a3643312cfd23f492afeb98b13eac5ebcc0ea305
Matthew McRaven <matthew.mcraven@gmail.com>
2026-06-26 12:19:09 -0700

    feat(core): add WIP concrete simulator type
    Going to make some intrusive changes to the new API; want to preserve this work.

---
+++ core/core/sim/system.cpp
@@ -0,0 +1,14 @@
+#include "system.hpp"
+#include "core/math/bitmanip/enums.hpp"
+using namespace bits;
+consteval void allow_opaque_handle_increment(Device::ID);
+
+System::System()
+    : Device(_root_desc), _gen_next_ID([this]() { return next_ID(); }),
+      _root(std::make_unique<DeviceTree>(this, nullptr)) {}
+
+Device::ID System::next_ID() { return _next_ID++; }
+
+Device::IDGenerator System::gen_next_ID() { return _gen_next_ID; }
+
+void System::set_buffer(trace::Buffer *buffer) { throw std::logic_error("Unimplemented"); }
---
+++ core/core/sim/system.hpp
@@ -0,0 +1,89 @@
+/*
+ * /Copyright (c) 2024-2025. Stanley Warford, Matthew McRaven
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <map>
+#include <memory>
+#include <vector>
+#include "core/sim/adevice.hpp"
+
+/*struct Scheduler {
+  virtual tick::Recipient *next(tick::Type current) = 0;
+  virtual void schedule(tick::Recipient *listener, tick::Type startingOn) = 0;
+  virtual void reschedule(device::ID device, tick::Type startingOn) = 0;
+};*/
+
+namespace trace {
+class Buffer;
+}
+
+class System : public Device {
+public:
+  System();
+  ~System() = default;
+  System(const System &) = delete;
+  System(System &&) = delete;
+  System &operator=(const System &) = delete;
+  System &operator=(System &&) = delete;
+
+  Device::ID next_ID();
+  Device::IDGenerator gen_next_ID();
+
+  void set_buffer(trace::Buffer *buffer);
+
+  // Create a device that is a child of the root (this system)
+  template <typename ConcreteDevice, typename... Args>
+  ConcreteDevice *make_device(std::string_view self_name, Args &&...args);
+  // Create children under a given device.
+  template <typename ConcreteDevice, typename... Args>
+  ConcreteDevice *make_device(Device::ID parent, std::string_view self_name, Args &&...args);
+  template <typename ConcreteDevice, typename... Args>
+  ConcreteDevice *make_device(Device *parent, std::string_view self_name, Args &&...args);
+
+private:
+  Device::ID _next_ID = Device::ID(1);
+  Device::IDGenerator _gen_next_ID = [] { return Device::ID(0); };
+  static inline Device::Descriptor _root_desc{.id = Device::ID(0), .basename = "/", .fullname = "/"};
+  std::unique_ptr<DeviceTree> _root = nullptr;
+  std::map<Device::ID, DeviceTree *> _id_to_device;
+};
+
+template <typename ConcreteDevice, typename... Args>
+ConcreteDevice *System::make_device(Device *parent, std::string_view self_name, Args &&...args) {
+  const auto id = parent->descriptor().id;
+  if (auto it = _id_to_device.find(id); it != _id_to_device.end())
+    return make_device<ConcreteDevice>(id, self_name, std::forward<Args>(args)...);
+  else throw std::runtime_error("Parent device not found");
+}
+
+template <typename ConcreteDevice, typename... Args>
+ConcreteDevice *System::make_device(Device::ID parent_id, std::string_view self_name, Args &&...args) {
+  auto device_tree = _id_to_device.find(parent_id);
+  if (device_tree == _id_to_device.end()) throw std::runtime_error("Parent device not found");
+  auto &parent = device_tree->second->device;
+  static_assert(std::is_base_of_v<Device, ConcreteDevice>, "ConcreteDevice must be derived from Device");
+  const auto descriptor = Device::Descriptor{
+      .id = next_ID(), .basename = std::string(self_name), .fullname = parent->descriptor().child_name(self_name)};
+  auto device = std::make_unique<ConcreteDevice>(descriptor, std::forward<Args>(args)...);
+  auto ptr = device.get();
+  device_tree->second->children.push_back(std::make_unique<DeviceTree>(std::move(device, device_tree->second)));
+  return ptr;
+}
+
+template <typename ConcreteDevice, typename... Args>
+ConcreteDevice *System::make_device(std::string_view self_name, Args &&...args) {
+  static_assert(std::is_base_of_v<Device, ConcreteDevice>, "Device must be derived from Device");
+  return make_device<ConcreteDevice>(this, self_name, std::forward<Args>(args)...);
+}
