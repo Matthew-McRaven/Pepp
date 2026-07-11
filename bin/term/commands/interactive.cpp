b9a8e16a82fd34d0554f6eb8969d5f12dd38c111
Matthew McRaven <matthew.mcraven@gmail.com>
2026-07-11 12:44:45 -0700

    refactor(bin): remove unused code from interactive.cpp

--- bin/term/commands/interactive.cpp
+++ bin/term/commands/interactive.cpp
@@ -17,60 +17,11 @@
 #include "./interactive.hpp"
 #include <catch.hpp>
 #include <iostream>
-#include "core/interactive_test/dict.hpp"
 #include "core/interactive_test/hostobjs/vocab.hpp"
 #include "core/interactive_test/interp.hpp"
 #include "core/interactive_test/vocab/core_words.hpp"
-#include "core/sim/system.hpp"
-#include "fmt/format.h"
-#include "fmt/ranges.h"
 
 InteractiveTask::InteractiveTask(QObject *parent) : Task(parent) {}
-
-std::vector<std::string_view> tokenize(std::string_view input) {
-  constexpr std::string_view ws = " \t\r\n";
-  std::vector<std::string_view> out;
-  size_t start = input.find_first_not_of(ws);
-  while (start != std::string_view::npos) {
-    size_t end = input.find_first_of(ws, start);
-    if (end == std::string_view::npos) end = input.size();
-    out.push_back(input.substr(start, end - start));
-    start = input.find_first_not_of(ws, end);
-  }
-  return out;
-}
-
-struct Context {
-  std::stack<std::unique_ptr<System>> _systems;
-};
-
-using Handler = std::function<bool(Context &, std::span<std::string_view>)>;
-
-bool hnd_sys_dev_count(Context &ctx, std::span<std::string_view> args) {
-  if (ctx._systems.empty()) {
-    std::cerr << "  No system created yet." << std::endl;
-    return false;
-  }
-  auto &sys = ctx._systems.top();
-  auto dt = sys->root();
-  auto count = std::distance(dt->begin(), dt->end());
-  std::cout << "  System has " << count << " devices." << std::endl;
-  return true;
-}
-// Many name=values
-bool hnd_create_dense(Context &ctx, std::span<std::string_view> args) {
-  if (ctx._systems.empty()) {
-    std::cerr << "  No system created yet." << std::endl;
-    return false;
-  }
-  std::cout << fmt::format("  {}", fmt::join(args.begin(), args.end(), ",")) << std::endl;
-
-  return true;
-}
-
-static const inline NativeOpcode make_machine{.name = "sys.create", .h = [](Interpreter *interp) {
-                                                // interp->push_object((void)nullptr);
-                                              }};
 
 void InteractiveTask::run() {
   std::cout << "Interactive mode. Type something and press enter (Ctrl+D to exit):" << std::endl;
