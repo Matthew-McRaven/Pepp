ae81df040bca6753bff5f64fa7954a48ebda03a8
Matthew McRaven <matthew.mcraven@gmail.com>
2026-07-25 10:40:05 -0700

    fix(core): remove constrained partially specialization of std::hash
    This is undefined behavior! Stop!

--- core/core/math/bitmanip/enums.hpp
+++ core/core/math/bitmanip/enums.hpp
@@ -74,13 +74,13 @@ constexpr bool none(T lhs) noexcept {
   return to_underlying(lhs) == 0;
 }
 
-} // namespace bits
-
 template <class T>
   requires(std::is_enum_v<T> && requires(T e) { is_bitflags(e); })
-struct std::hash<T> {
+struct bitflags_hash {
   std::size_t operator()(const T &v) const noexcept {
     using U = std::underlying_type_t<T>;
     return std::hash<U>{}(static_cast<U>(v));
   }
 };
+
+} // namespace bits
