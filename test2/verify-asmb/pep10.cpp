8abe98e90d09e61ae281b2f3ee1c01323d11d953
Matthew McRaven <matthew.mcraven@gmail.com>
2026-09-01 18:19:28 -0700

    test: split longest test case

--- test2/verify-asmb/pep10.cpp
+++ test2/verify-asmb/pep10.cpp
@@ -61,11 +61,11 @@ void injectFakeSCallMacros(QSharedPointer<macro::Registry> registry) {
 }
 } // namespace
 
-TEST_CASE("CS6E figure assembly", "[scope:asm][kind:e2e][arch:pep10]") {
+TEST_CASE("CS6E figure assembly, Standalone", "[scope:asm][kind:e2e][arch:pep10]") {
   auto fs = builtins::QtFilesystemProvider::create();
   auto bookReg = pepp::BuiltinRegistry(std::move(fs));
   auto book = bookReg.find_book("Computer Systems, 6th Edition");
-  SECTION("Standalone") {
+
     // Load macros on each iteration to prevent macros from migrating between
     // tests.
     auto registry = QSharedPointer<macro::Registry>::create();
@@ -96,7 +96,6 @@ TEST_CASE("CS6E figure assembly", "[scope:asm][kind:e2e][arch:pep10]") {
         REQUIRE(result);
       }
     }
-  }
   SECTION("Unified") {
     // Load macros on each iteration to prevent macros from migrating between tests.
     auto registry = QSharedPointer<macro::Registry>::create();
@@ -185,6 +184,100 @@ TEST_CASE("CS6E figure assembly", "[scope:asm][kind:e2e][arch:pep10]") {
         QVector<quint8> dump(0x1'00'00);
         sys->bus()->dump({dump.data(), std::size_t(dump.size())});
       }
+    }
+  }
+}
+
+TEST_CASE("CS6E figure assembly, Unified", "[scope:asm][kind:e2e][arch:pep10]") {
+  auto fs = builtins::QtFilesystemProvider::create();
+  auto bookReg = pepp::BuiltinRegistry(std::move(fs));
+  auto book = bookReg.find_book("Computer Systems, 6th Edition");
+
+  // Load macros on each iteration to prevent macros from migrating between tests.
+  auto registry = QSharedPointer<macro::Registry>::create();
+  REQUIRE(book != nullptr);
+  loadBookMacros(book, registry);
+  for (auto &fig : book->figures()) {
+    auto defaultOS = fig->default_os();
+    if (!fig->has_fragment("pep")) continue;
+    else if (fig->is_os()) continue;
+    else if (defaultOS == nullptr) continue;
+    else if (!defaultOS->has_fragment("pep")) continue;
+    QString chapter = QString::fromStdString(fig->name_chapter());
+    QString figName = QString::fromStdString(fig->name_figure());
+    QString osBody = QString::fromStdString(defaultOS->find_fragment("pep")->contents());
+    QString userBody = QString::fromStdString(fig->find_fragment("pep")->contents());
+    bool isFullOS = bool(defaultOS->name_figure() == "full");
+
+    DYNAMIC_SECTION(chapter.toStdString() << "." << figName.toStdString()) {
+      auto pipeline = pas::driver::pep10::pipeline<pas::driver::ANTLRParserTag>(
+          {{osBody, {.isOS = true, .ignoreUndefinedSymbols = false}},
+           {userBody, {.isOS = false, .ignoreUndefinedSymbols = false}}},
+          registry);
+      auto result = pipeline->assemble(pas::driver::pep10::Stage::End);
+
+      CHECK(pipeline->pipelines.size() == 2);
+
+      auto osTarget = pipeline->pipelines[0].first;
+      REQUIRE(osTarget->bodies.contains(pas::driver::repr::Nodes::name));
+      auto osRoot = osTarget->bodies[pas::driver::repr::Nodes::name].value<pas::driver::repr::Nodes>().value;
+      if (pipeline->pipelines[0].first->stage != pas::driver::pep10::Stage::End) {
+        for (auto &error : pas::ops::generic::collectErrors(*osRoot)) qCritical() << "OS:   " << error.second.message;
+        REQUIRE(false);
+      }
+      auto userTarget = pipeline->pipelines[1].first;
+      REQUIRE(userTarget->bodies.contains(pas::driver::repr::Nodes::name));
+      auto userRoot = userTarget->bodies[pas::driver::repr::Nodes::name].value<pas::driver::repr::Nodes>().value;
+      // print out error messages before failing -- enables debugging broken
+      // tests.
+      if (!result) {
+        auto lines = pas::ops::pepp::formatListing<isa::Pep10>(*userRoot);
+        for (auto &line : lines) qCritical() << line;
+        for (auto &error : pas::ops::generic::collectErrors(*osRoot)) qCritical() << "OS:   " << error.second.message;
+        for (auto &error : pas::ops::generic::collectErrors(*userRoot)) qCritical() << "USER: " << error.second.message;
+      }
+      auto elf = pas::obj::pep10::createElf();
+      pas::obj::pep10::combineSections(*osRoot);
+      pas::obj::pep10::writeOS(*elf, *osRoot);
+      pas::obj::pep10::combineSections(*userRoot);
+      pas::obj::pep10::writeUser(*elf, *userRoot);
+      // Segments are not layed out correctly until saving.
+      {
+        std::stringstream s;
+        elf->save(s);
+        s.seekg(0, std::ios::beg);
+        elf->load(s);
+      }
+      REQUIRE(result);
+
+      // Verify MMIO information.
+      auto decs = ::obj::getMMIODeclarations(*elf);
+      CHECK(decs.size() == 3);
+      CHECK(std::find_if(decs.cbegin(), decs.cend(), is_charIn) != decs.cend());
+      CHECK(std::find_if(decs.cbegin(), decs.cend(), is_charOut) != decs.cend());
+      CHECK(std::find_if(decs.cbegin(), decs.cend(), is_pwrOff) != decs.cend());
+
+      auto memMap = obj::getLoadableSegments(*elf);
+      auto mergeMap = obj::mergeSegmentRegions(memMap);
+      if (isFullOS) {
+        CHECK(mergeMap.size() == 3);
+        // user memory + system stack
+        mergeMap[0].segs = {};
+        auto uMem = obj::MemoryRegion{.r = 1, .w = 1, .minOffset = 0, .maxOffset = 0xfaf9};
+        CHECK(mergeMap[0] == uMem);
+        // OS text
+        mergeMap[1].segs = {};
+        auto txt = obj::MemoryRegion{.r = 1, .w = 0, .minOffset = 0xfafa, .maxOffset = 0xfff9};
+        CHECK(mergeMap[1] == txt);
+        // Carveout for MMIO
+        mergeMap[2].segs = {};
+        auto mmio = obj::MemoryRegion{.r = 1, .w = 1, .minOffset = 0xfffa, .maxOffset = 0xffff};
+        CHECK(mergeMap[2] == mmio);
+      }
+      QSharedPointer<targets::isa::System> sys;
+      REQUIRE_NOTHROW([&sys, &elf]() { sys = targets::isa::systemFromElf(*elf, true); }());
+      QVector<quint8> dump(0x1'00'00);
+      sys->bus()->dump({dump.data(), std::size_t(dump.size())});
     }
   }
 }
